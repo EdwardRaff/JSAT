08a589751a54e8c68d5283160c136c65f8f346b5
Alex Bartol <alexb2539@gmail.com>
2011-03-08 22:27:34 +0000

    Added ability to maximize by switching from JDialog to JFrame
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@11 c8979423-a768-6f17-17f2-fb57d64bf5d2

--- JSAT/src/jsat/GraphDialog.java
+++ JSAT/src/jsat/GraphDialog.java
@@ -4,21 +4,22 @@ package jsat;
 
 import java.awt.Frame;
 import java.awt.GridLayout;
-import javax.swing.JDialog;
+import javax.swing.JFrame;
 import jsat.graphing.Graph2D;
 
 /**
  *
  * @author Edward Raff
  */
-public class GraphDialog extends JDialog
+public class GraphDialog extends JFrame
 {
     Graph2D graph;
 
     public GraphDialog(Frame parent, String title, Graph2D graph)
     {
-        super(parent, title, false);
+        super(title);
         this.graph = graph;
+
 
         getContentPane().setLayout(new GridLayout(1, 1));
         getContentPane().add(graph);
