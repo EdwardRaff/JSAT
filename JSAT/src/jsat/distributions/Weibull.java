cc8849acbde0aca87d6a25586e2c5bb395d84c58
Edward Raff <Raff.Edward@gmail.com>
2011-05-09 23:09:54 +0000

    Implemented Weibull distribution 	Missing 1 part, I couldent find or deduce a way to estimate the parameters of the distribution from a data set
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@25 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/distributions/Weibull.java
@@ -0,0 +1,120 @@
+
+package jsat.distributions;
+
+import jsat.linear.Vec;
+import static java.lang.Math.*;
+import static jsat.math.SpecialMath.*;
+/**
+ *
+ * @author Edward Raff
+ */
+public class Weibull extends ContinousDistribution
+{
+    /**
+     * Shape parameter
+     */
+    double k;
+    /**
+     * Scale parameter
+     */
+    double gam;
+
+    public Weibull(double k, double gam)
+    {
+        if(k <= 0)
+            throw new ArithmeticException("k must be > 0 not " + k );
+        if(gam <= 0)
+            throw new ArithmeticException("k must be > 0 not " + gam );
+        this.k = k;
+        this.gam = gam;
+    }
+
+
+    
+    @Override
+    public double pdf(double x)
+    {
+        if(x < 0)
+            return 0;
+        
+        return k/gam * pow(x/gam, k-1)*exp(-pow(x/gam, k));
+        
+    }
+
+    @Override
+    public double invPdf(double p)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public double cdf(double x)
+    {
+        return 1 - exp(-pow(x/gam, k));
+    }
+
+    @Override
+    public double invCdf(double p)
+    {
+        return gam*pow(-log(1-p),1/k);
+    }
+
+    @Override
+    public double min()
+    {
+        return 0;
+    }
+
+    @Override
+    public double max()
+    {
+        return Double.POSITIVE_INFINITY;
+    }
+
+    @Override
+    public String getDistributionName()
+    {
+        return "Weibull";
+    }
+
+    @Override
+    public String[] getVariables()
+    {
+        return new String[]{"k", "gam"};
+    }
+
+    @Override
+    public double[] getCurrentVariableValues()
+    {
+        return new double[]{k, gam};
+    }
+
+    @Override
+    public void setVariable(String var, double value)
+    {
+        if (var.equals("k"))
+            if (value > 0)
+                k = value;
+            else
+                throw new ArithmeticException("k must be > 0 not " + value);
+        else if (var.equals("gam"))
+            if (value > 0)
+                gam = value;
+            else
+                throw new ArithmeticException("gam must be > 0 not " + value );
+    }
+
+    @Override
+    public ContinousDistribution copy()
+    {
+        return new Weibull(k, gam);
+    }
+
+    @Override
+    public void setUsingData(Vec data)
+    {
+        //TODO how do you solve for this sucker? I dont know. 
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
