7cbf0f82e155259ab0f7b098a3070e7a7252ca89
Edward Raff <Raff.Edward@gmail.com>
2011-05-14 21:39:30 +0000

    Added Beta distribution
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@48 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/distributions/Beta.java
@@ -0,0 +1,140 @@
+
+package jsat.distributions;
+
+import jsat.linear.Vec;
+import static java.lang.Math.*;
+import static jsat.math.SpecialMath.*;
+/**
+ *
+ * @author Edward Raff
+ */
+public class Beta extends ContinousDistribution
+{
+    double alpha;
+    double beta;
+
+    public Beta(double alpha, double beta)
+    {
+        if(alpha <= 0)
+            throw new ArithmeticException("Alpha must be > 0, not " + alpha);
+        else if(beta <= 0)
+            throw new ArithmeticException("Beta must be > 0, not " + beta);
+        this.alpha = alpha;
+        this.beta = beta;
+    }
+
+    @Override
+    public double pdf(double x)
+    {
+        return exp((alpha-1)*log(x)+(beta-1)*log(1-x)-lnBeta(alpha, beta));
+    }
+
+    @Override
+    public double cdf(double x)
+    {
+        return betaIncReg(x, alpha, beta);
+    }
+
+    @Override
+    public double invCdf(double p)
+    {
+        return invBetaIncReg(p, alpha, beta);
+    }
+
+    @Override
+    public double min()
+    {
+        return 0;
+    }
+
+    @Override
+    public double max()
+    {
+        return 1;
+    }
+
+    @Override
+    public String getDistributionName()
+    {
+        return "Beta";
+    }
+
+    @Override
+    public String[] getVariables()
+    {
+        return new String[]{"alpha", "beta"};
+    }
+
+    @Override
+    public double[] getCurrentVariableValues()
+    {
+        return new double[]{alpha, beta};
+    }
+
+    @Override
+    public void setVariable(String var, double value)
+    {
+        if (var.equals("alpha"))
+            if (value > 0)
+                alpha = value;
+            else
+                throw new RuntimeException("Alpha must be > 0, not " + value);
+        else if (var.equals("beta"))
+            if (value > 0)
+                beta = value;
+            else
+                throw new RuntimeException("Beta must be > 0, not " + value);
+    }
+
+    @Override
+    public ContinousDistribution copy()
+    {
+        return new Beta(alpha, beta);
+    }
+
+    @Override
+    public void setUsingData(Vec data)
+    {
+        double mean = data.mean();
+        double var = data.variance();
+        
+        //alpha = (mean^2 - mean^3 - mean * var) / var
+        alpha = (mean*mean-mean*mean*mean-mean*var)/var;
+        beta = (alpha-alpha*mean)/mean;
+    }
+
+    @Override
+    public double mean()
+    {
+        return alpha/(alpha+beta);
+    }
+
+    @Override
+    public double median()
+    {
+        return invBetaIncReg(0.5, alpha, beta);
+    }
+
+    @Override
+    public double mode()
+    {
+        if(alpha > 1 && beta > 1)
+            return (alpha-1)/(alpha+beta-2);
+        else
+            return Double.NaN;
+    }
+
+    @Override
+    public double variance()
+    {
+        return alpha*beta / (pow(alpha+beta, 2)*(alpha+beta+1));
+    }
+
+    @Override
+    public double skewness()
+    {
+        return 2*(beta-alpha)*sqrt(alpha+beta+1)/((alpha+beta+2)*sqrt(alpha*beta));
+    }
+    
+    
+}
