1ac8353f82f525102ac06cdeaec61fd9ba30cb93
Edward Raff <Raff.Edward@gmail.com>
2011-09-21 15:19:23 +0000

    Updated IndexTable to be much more robost, utalizing Java's built in sort method. Also made it work for Lists and compariable Object types
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@124 c8979423-a768-6f17-17f2-fb57d64bf5d2

--- JSAT/src/jsat/utils/IndexTable.java
+++ JSAT/src/jsat/utils/IndexTable.java
@@ -2,6 +2,9 @@
 package jsat.utils;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  *
@@ -9,18 +12,75 @@ import java.util.Arrays;
  */
 public class IndexTable<T extends Comparable<T>>
 {
-    int n;
-    int[] index;
+    Integer[] index;
 
     public IndexTable(double[] array)
     {
-        n = array.length;
-        index = new int[n];
-        for(int i = 0; i < n; i++)
+        index = new Integer[array.length];
+        for(int i = 0; i < index.length; i++)
             index[i] = i;
-        int start = zerosUpFront(array);
-        quickSort(array, start, array.length-1);
+        Arrays.sort(index, new IndexViewCompD(array));
+    }
+    
+    public IndexTable(T[] array)
+    {
+        index = new Integer[array.length];
+        for(int i = 0; i < index.length; i++)
+            index[i] = i;
+        Arrays.sort(index, new IndexViewCompG(array));
+    }
+    
+    public IndexTable(List<T> list)
+    {
+        index = new Integer[list.size()];
+        for(int i = 0; i < index.length; i++)
+            index[i] = i;
+        Arrays.sort(index, new IndexViewCompList(list));
+    }
+    
+    private class IndexViewCompD implements Comparator<Integer> 
+    {
+        double[] base;
+
+        public IndexViewCompD(double[] base)
+        {
+            this.base = base;
+        }
         
+        public int compare(Integer t, Integer t1)
+        {
+            return Double.compare(base[t], base[t1]);
+        }        
+    }
+    
+    private class IndexViewCompG implements Comparator<Integer> 
+    {
+        T[] base;
+
+        public IndexViewCompG(T[] base)
+        {
+            this.base = base;
+        }
+
+        public int compare(Integer t, Integer t1)
+        {
+            return base[t].compareTo(base[t1]);
+        }        
+    }
+    
+    private class IndexViewCompList implements Comparator<Integer> 
+    {
+        List<T> base;
+
+        public IndexViewCompList(List<T> base)
+        {
+            this.base = base;
+        }
+
+        public int compare(Integer t, Integer t1)
+        {
+            return base.get(t).compareTo(base.get(t1));
+        }        
     }
     
     
@@ -36,68 +96,9 @@ public class IndexTable<T extends Comparable<T>>
         return index[i];
     }
     
-    /**
-     * Moves all the zeros to the front of the array, and then returns the index
-     * of the first non zero item. Assumes all values are >= 0
-     * @param numbers
-     * @return the index of the first non zero
-     */
-    private int zerosUpFront(double[] numbers)
+    public int length()
     {
-        int i = 0;
-        for(int k = i; k < numbers.length; k++)
-            if(numbers[index(k)] == 0)
-                swap(k, i++);
-        
-        return i;
-    }
-    
-    private void quickSort(double[] numbers, int low, int high)
-    {
-        int len = high-low+1;
-//        if(len  == numbers.length)
-//            System.out.println("Hat?");
-//        else if(len  == 14)
-//            System.out.println("\tBad? 14");
-//        else
-//            System.out.println("\t" + len);
-        
-        if(len < 7)
-        {
-            for(int i = low; i <= high; i++)
-            {
-                for(int j = i+1; j <= high; j++)
-                    if(numbers[index(i)] > numbers[index(j)])
-                    {
-                        swap(i, j);
-                    }
-            }
-            return;
-        }
-        
-        int i = low, j = high;
-        // Get the pivot element from the middle of the list
-        double pivot = numbers[index(low + (high - low)/2)]; 
-
-        while (i <= j)
-        {
-            while (numbers[index(i)] < pivot)
-                i++;
-
-            while (numbers[index(j)] > pivot)
-                j--;
-
-            if (i <= j)
-            {
-                swap(i, j);
-                i++;
-                j--;
-            }
-        }
-        if (low < j)
-            quickSort(numbers, low, j);
-        if (i < high)
-            quickSort(numbers, i, high);
+        return index.length;
     }
     
 }
---
+++ JSAT/test/jsat/utils/IndexTableTest.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsat.utils;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public class IndexTableTest
+{
+    static final double[] array = new double[] {9.0, 4.0, 3.0, 2.0, 1.0, 10.0, 11.0 };
+    static final Double[] arrayD = new Double[] {9.0, 4.0, 3.0, 2.0, 1.0, 10.0, 11.0 };
+    static final List<Double> list = Arrays.asList(arrayD);
+    
+    public IndexTableTest()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    
+    @Test
+    public void testSortD()
+    {
+        IndexTable idt = new IndexTable(array);
+        for(int i = 0; i < idt.length()-1; i++)
+            assertTrue(array[idt.index(i)] <= array[idt.index(i+1)]);
+    }
+    
+    @Test
+    public void testSortG()
+    {
+        IndexTable idt = new IndexTable(arrayD);
+        for(int i = 0; i < idt.length()-1; i++)
+            assertTrue(arrayD[idt.index(i)].compareTo(arrayD[idt.index(i+1)]) <= 0);
+    }
+    
+    @Test
+    public void testSortList()
+    {
+        IndexTable idt = new IndexTable(list);
+        for(int i = 0; i < idt.length()-1; i++)
+            assertTrue(list.get(idt.index(i)).compareTo(list.get(idt.index(i+1))) <= 0);
+    }
+    
+    @Test
+    public void testSwap()
+    {
+        System.out.println("swap");
+        int i = 0;
+        int j = 1;
+        IndexTable idx = new IndexTable(array);
+        double di = array[idx.index(i)];
+        double dj = array[idx.index(j)];
+        idx.swap(i, j);
+        assertTrue(di == array[idx.index(j)]);
+        assertTrue(dj == array[idx.index(i)]);
+        assertTrue(di != dj);
+        idx.swap(j, i);
+        assertTrue(di != array[idx.index(j)]);
+        assertTrue(dj != array[idx.index(i)]);
+        assertTrue(di == array[idx.index(i)]);
+        assertTrue(dj == array[idx.index(j)]);
+        
+    }
+}
