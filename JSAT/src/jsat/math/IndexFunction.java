0e27b6ce29fe160e26c951f169e98ce9c75f27e7
Edward Raff <Raff.Edward@gmail.com>
2011-11-08 22:09:44 +0000

    Forgot to add the IndexFunction, needed for loading text documents as vectors
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@157 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/math/IndexFunction.java
@@ -0,0 +1,35 @@
+
+package jsat.math;
+
+import jsat.linear.Vec;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public abstract class IndexFunction implements Function
+{
+    /**
+     * An index function, meant to be applied to vectors where the 
+     * value to be computed may vary based on the position in the 
+     * vector of the value. 
+     * 
+     * @param value the value at the specified index
+     * @param index the index the value is from
+     * @return the computed result. If a negative index was given, 
+     * the function should return 0.0 if indexFunc(0,indx) would 
+     * return zero for all valid indices. If this is not the case, any non zero value should be returned. 
+     * 
+     */
+    abstract public double indexFunc(double value, int index);
+
+    public double f(double... x)
+    {
+        return indexFunc(x[0], (int)x[1]);
+    }
+
+    public double f(Vec x)
+    {
+        return indexFunc(x.get(0), (int)x.get(1));
+    }    
+}
