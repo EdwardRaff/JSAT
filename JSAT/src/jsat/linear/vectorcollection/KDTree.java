cc8f0835e00a32afdaae41eac55bd2e044c33017
Edward Raff <Raff.Edward@gmail.com>
2011-12-16 22:31:16 +0000

    Added code to throw an error if an invalid distance metric is used Fixed bug where the range search did not return points in sorted order. Added 2nd pivot selection method
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@171 c8979423-a768-6f17-17f2-fb57d64bf5d2

--- JSAT/src/jsat/linear/vectorcollection/KDTree.java
+++ JSAT/src/jsat/linear/vectorcollection/KDTree.java
@@ -8,7 +8,12 @@ import java.util.List;
 import jsat.classifiers.DataPointPair;
 import jsat.linear.Vec;
 import jsat.linear.VecPaired;
+import jsat.linear.distancemetrics.ChebyshevDistance;
 import jsat.linear.distancemetrics.DistanceMetric;
+import jsat.linear.distancemetrics.EuclideanDistance;
+import jsat.linear.distancemetrics.ManhattanDistance;
+import jsat.linear.distancemetrics.MinkowskiDistance;
+import jsat.math.OnLineStatistics;
 import jsat.utils.BoundedSortedList;
 import jsat.utils.BoundedSortedSet;
 import jsat.utils.PairedReturn;
@@ -16,18 +21,45 @@ import jsat.utils.ProbailityMatch;
 import static jsat.linear.VecPaired.*;
 
 /**
- *
+ * Note: KD trees are only usable with Distance Metrics based off of the pNorm between two vectors. The valid distance metrics are 
+ * {@link EuclideanDistance}, {@link ChebyshevDistance}, {@link ManhattanDistance}, {@link MinkowskiDistance}
+ * 
  * @author Edward Raff
  */
 public class KDTree<V extends Vec> implements VectorCollection<V>
 {
     private DistanceMetric distanceMetric;
     private KDNode root;
+    private PivotSelection pvSelection;
+    
+    public enum PivotSelection
+    {
+        /**
+         * The next pivot will be selected by iteratively going through each possible pivot. 
+         * This method has no additional overhead. 
+         */
+        Incremental, 
+        /**
+         * The next pivot will be selected by determining which pivot index contains the most variance. 
+         * This method requires an additional O(n d) work per step. Where n is the number of data points
+         * being split, and d is the dimension of the data set. 
+         */
+        Variance
+    }
 
+    public KDTree(List<V> vecs, DistanceMetric distanceMetric, PivotSelection pvSelection)
+    {
+        if(!( distanceMetric instanceof EuclideanDistance || distanceMetric instanceof ChebyshevDistance || 
+              distanceMetric instanceof ManhattanDistance || distanceMetric instanceof MinkowskiDistance) )
+            throw new ArithmeticException("KD Trees are not compatible with the given distance metric.");
+        this.distanceMetric = distanceMetric;
+        this.pvSelection = pvSelection;
+        this.root = buildTree(vecs, 0);
+    }
+    
     public KDTree(List<V> vecs, DistanceMetric distanceMetric)
     {
-        this.distanceMetric = distanceMetric;
-        this.root = buildTree(vecs, 0);
+        this(vecs, distanceMetric, PivotSelection.Variance);
     }
     
     private class KDNode 
@@ -108,14 +140,43 @@ public class KDTree<V extends Vec> implements VectorCollection<V>
             return null;
         int mod = data.get(0).length();
         
-        int axi = depth % mod;
+        if(data.size() == 1)
+            return new KDNode(data.get(0), depth % mod);
         
-        Collections.sort(data, new VecIndexComparator(axi));
+        int pivot = -1;
+        if(pvSelection == PivotSelection.Incremental)
+            pivot = depth % mod;
+        else//Variance 
+        {
+            OnLineStatistics[] allStats = new OnLineStatistics[data.get(0).length()];
+            for(int j = 0; j < allStats.length; j++)
+                allStats[j] = new OnLineStatistics();
+            
+            for(int i = 0; i < data.size(); i++)//For each data point
+            {
+                V vec = data.get(i);
+                for(int j = 0; j < allStats.length; j++)//For each dimension 
+                    allStats[j].add(vec.get(j));
+            }
+            
+            double maxVariance = -1;
+            for(int j = 0; j < allStats.length; j++)
+            {
+                if(allStats[j].getVarance() > maxVariance)
+                {
+                    
+                    maxVariance = allStats[j].getVarance();
+                    pivot = j;
+                }
+            }
+        }
+        
+        Collections.sort(data, new VecIndexComparator(pivot));
         
         int medianIndex = data.size()/2;
         V median = data.get(medianIndex);
         
-        KDNode node = new KDNode(median, axi);
+        KDNode node = new KDNode(median, pivot);
         
         node.setLeft(buildTree(data.subList(0, medianIndex), depth+1));
         node.setRight(buildTree(data.subList(medianIndex+1, data.size()), depth+1));
@@ -198,15 +259,35 @@ public class KDTree<V extends Vec> implements VectorCollection<V>
         
         distanceSearch(query, root, vecs, range);
         
+        Collections.sort(vecs, new Comparator<VecPaired<Double, V>>() {
+
+            public int compare(VecPaired<Double, V> o1, VecPaired<Double, V> o2)
+            {
+                return Double.compare(o1.getPair(), o2.getPair());
+            }
+        });
+        
         return vecs;
         
     }
     
     public static class KDTreeFactory<V extends Vec> implements VectorCollectionFactory<V>
     {
+        private PivotSelection pvSelectionMethod;
+
+        public KDTreeFactory(PivotSelection pvSelectionMethod)
+        {
+            this.pvSelectionMethod = pvSelectionMethod;
+        }
+
+        public KDTreeFactory()
+        {
+            this(PivotSelection.Variance);
+        }
+        
         public VectorCollection<V> getVectorCollection(List<V> source, DistanceMetric distanceMetric)
         {
-            return new KDTree<V>(source, distanceMetric);
+            return new KDTree<V>(source, distanceMetric, pvSelectionMethod);
         }
     }
 }
