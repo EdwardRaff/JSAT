5ac0c534892a8f8faca9497aaac2d3ecaa17e123
Edward Raff <Raff.Edward@gmail.com>
2012-01-20 18:51:00 +0000

    Added a toString method
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@188 c8979423-a768-6f17-17f2-fb57d64bf5d2

--- JSAT/src/jsat/utils/PairedReturn.java
+++ JSAT/src/jsat/utils/PairedReturn.java
@@ -18,13 +18,27 @@ public class PairedReturn<T, V>
         this.secondItem = v;
     }
 
+    /**
+     * Returns the first object stored.
+     * @return the first object stored
+     */
     public T getFirstItem()
     {
         return firstItem;
     }
 
+    /**
+     * Returns the second object stored
+     * @return the second object stored
+     */
     public V getSecondItem()
     {
         return secondItem;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "(" + getFirstItem() + ", " + getSecondItem() + ")";
     }
 }
