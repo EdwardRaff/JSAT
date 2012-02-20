fac8edb04c6a73cb63ff3f4b23b386d7e085ad69
Edward Raff <Raff.Edward@gmail.com>
2012-02-20 23:59:43 +0000

    Added an implementation of LogitBoost based on the original paper
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@261 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/classifiers/boosting/LogitBoost.java
@@ -0,0 +1,224 @@
+
+package jsat.classifiers.boosting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import jsat.classifiers.CategoricalResults;
+import jsat.classifiers.ClassificationDataSet;
+import jsat.classifiers.Classifier;
+import jsat.classifiers.DataPoint;
+import jsat.classifiers.DataPointPair;
+import jsat.classifiers.OneVSAll;
+import jsat.exceptions.FailedToFitException;
+import jsat.exceptions.UntrainedModelException;
+import jsat.linear.DenseVector;
+import jsat.linear.Vec;
+import jsat.math.TrigMath;
+import jsat.regression.MultipleLinearRegression;
+import jsat.regression.RegressionDataSet;
+import jsat.regression.Regressor;
+
+/**
+ * An implementation of the original 2 class LogitBoost algorithm. While there is a 
+ * multi-class description in the original paper, its implementation is congruent 
+ * with the result of using LogitBoost with {@link OneVSAll} classifier. <br>
+ * LogitBoost differs from its predecessors in that it boosts 
+ * {@link Regressor regression} models to create a powerful classifier. 
+ * <br><br>
+ * Paper: <b>Special Invited Paper Additive Logistic Regression: A Statistical
+ * View of Boosting</b>, By Jerome Friedman, Trevor Hastie and Robert Tibshirani.
+ * <i>The Annals of Statistics</i> 2000, Vol. 28, No. 2, 337–407
+ * 
+ * @author Edward Raff
+ */
+public class LogitBoost implements Classifier
+{
+    /**
+     * Weak learners
+     */
+    private List<Regressor> baseLearners;
+    /**
+     * Weak learner to use, 'the oracle' 
+     */
+    private Regressor baseLearner;
+    private int maxIterations;
+    /**
+     * Constant for stability and controls the maximum penalty  
+     */
+    private double zMax = 3;
+
+    /**
+     * Creates a new LogitBoost using the standard {@link MultipleLinearRegression} .
+     * @param M the maximum number of iterations. 
+     */
+    public LogitBoost(int M)
+    {
+        this(new MultipleLinearRegression(true), M);
+    }
+    
+    /**
+     * Creates a new LogitBoost using the given base learner. 
+     * @param baseLearner the weak learner to build an ensemble out of. 
+     * @param M the maximum number of iterations. 
+     */
+    public LogitBoost(Regressor baseLearner, int M)
+    {
+        if(!baseLearner.supportsWeightedData())
+            throw new RuntimeException("Base Learner must support weighted data points to be boosted");
+        this.baseLearner = baseLearner;
+        this.maxIterations = M;
+    }
+
+    /**
+     * Sets the maximum number of iterations of boosting that can occur, giving 
+     * the maximum number of base learners that may be trained
+     * @param maxIterations the maximum number of iterations
+     */
+    public void setMaxIterations(int maxIterations)
+    {
+        this.maxIterations = maxIterations;
+    }
+
+    /**
+     * The maximum number of iterations of boosting that may occur.
+     * @return maximum number of iterations of boosting that may occur.
+     */
+    public int getMaxIterations()
+    {
+        return maxIterations;
+    }
+
+    /**
+     * Sets the penalty bound for miss-classification of results. This also provides
+     * numerical stability to the algorithm. The results are not sensitive to this 
+     * value. The recommended value range is in [2, 4]
+     * 
+     * @param zMax the penalty bound
+     * @throws ArithmeticException if the value is not in (0, {@link Double#MAX_VALUE}]
+     */
+    public void setzMax(double zMax)
+    {
+        if(Double.isInfinite(zMax) || Double.isNaN(zMax) || zMax <= 0)
+            throw new ArithmeticException("Invalid penalty given: " + zMax);
+        this.zMax = zMax;
+    }
+
+    /**
+     * Returns the maximum miss-classification penalty used by the algorithm. 
+     * @return the maximum miss-classification
+     */
+    public double getzMax()
+    {
+        return zMax;
+    }
+    
+    public CategoricalResults classify(DataPoint data)
+    {
+        if(baseLearner == null)
+            throw new UntrainedModelException("Model has not yet been trained");
+        double p = P(data);
+        
+        CategoricalResults cr  = new CategoricalResults(2);
+        
+        cr.setProb(1, p);
+        cr.setProb(0, 1.0-p);
+        
+        return cr;
+    }
+
+    public void trainC(ClassificationDataSet dataSet, ExecutorService threadPool)
+    {
+        trainC(dataSet);
+    }
+
+    public void trainC(ClassificationDataSet dataSet)
+    {
+        if(dataSet.getPredicting().getNumOfCategories() != 2)
+            throw new FailedToFitException("LogitBoost only supports binary decision tasks, not " + dataSet.getPredicting().getNumOfCategories() + " class problems");
+        /**
+         * The data points paired with what we will use to store the target regression values. 
+         */
+        List<DataPointPair<Double>> dataPoints = dataSet.getAsFloatDPPList();
+        
+        baseLearners = new ArrayList<Regressor>(maxIterations);
+        int N = dataSet.getSampleSize();
+        
+        for(int m = 0; m < maxIterations; m++)
+        {
+            for(int i = 0; i < N; i++)
+            {
+                DataPoint dp = dataPoints.get(i).getDataPoint();
+                double pi = P(dp);
+                double zi;
+                if(dataSet.getDataPointCategory(i) == 1)
+                    zi = Math.min(zMax, 1.0/pi);
+                else
+                    zi = Math.max(-zMax, -1.0/(1.0-pi));
+                double wi = Math.max(pi*(1-pi), 2*1e-15);
+
+                dp.setWeight(wi);
+                dataPoints.get(i).setPair(zi);
+            }
+            
+            Regressor f = baseLearner.clone();
+            f.train(new RegressionDataSet(dataPoints));
+            baseLearners.add(f);
+        }
+        
+    }
+    
+    private double F(DataPoint x)
+    {
+        double fx = 0.0;//0 so when we are uninitalized P will return 0.5
+        
+        for(Regressor fm : baseLearners)
+            fx += fm.regress(x);
+        return fx*0.5;
+    }
+    
+    /**
+     * Returns the probability that a given data point belongs to class 1 
+     * @param x the data point in question
+     * @return P(y = 1 | x)
+     */
+    private double P(DataPoint x)
+    {
+        /**
+         *              F(x)
+         *             e
+         * p(x) = ---------------
+         *         F(x)    - F(x)
+         *        e     + e
+         */
+        double fx = F(x);
+        double efx = Math.exp(fx);
+        double enfx = Math.exp(-fx);
+        if(Double.isInfinite(efx) && efx > 0 && enfx < 1e-15)//Well classified point could return a Infinity which turns into NaN
+            return 1.0;
+        return efx/(efx + enfx);
+    }
+
+    public boolean supportsWeightedData()
+    {
+        return false;
+    }
+
+    @Override
+    public Classifier clone()
+    {
+        LogitBoost clone = new LogitBoost(maxIterations);
+        clone.zMax = this.zMax;
+        if(this.baseLearner != null) 
+            clone.baseLearner = this.baseLearner.clone();
+        if(this.baseLearners != null)
+        {
+            clone.baseLearners = new ArrayList<Regressor>(this.baseLearners.size());
+            for(Regressor r :  baseLearners)
+                clone.baseLearners.add(r.clone());
+        }
+        return clone;
+    }
+    
+}
