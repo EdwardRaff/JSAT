e74f4d01546af83351e0ce7fdc79f7a3c0d061f1
Edward Raff <Raff.Edward@gmail.com>
2012-02-26 04:30:01 +0000

    Added SquaredEuclideanDistance for those who want it
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@294 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/linear/distancemetrics/SquaredEuclideanDistance.java
@@ -0,0 +1,60 @@
+
+package jsat.linear.distancemetrics;
+
+import jsat.linear.SparceVector;
+import jsat.linear.Vec;
+
+/**
+ * In many applications, the squared {@link EuclideanDistance} is used because it avoids an expensive {@link Math#sqrt(double) } operation. 
+ * However, the Squared Euclidean Distance is not a truly valid metric, as it does not obey the {@link #isSubadditive() triangle inequality}.
+ * 
+ * @author Edward Raff
+ */
+public class SquaredEuclideanDistance implements DistanceMetric
+{
+
+    public double dist(Vec a, Vec b)
+    {
+        if(a.length() != b.length())
+            throw new ArithmeticException("Length miss match, vectors must have the same length");
+        double d = 0;
+        
+        if( a instanceof SparceVector && b instanceof SparceVector)
+        {
+            //Just square the pNorm for now... not easy code to write, and the sparceness is more important
+            return Math.pow(a.pNormDist(2, b), 2);
+        }
+        else
+        {
+            double tmp;
+            for(int i = 0; i < a.length(); i++)
+            {
+                tmp = a.get(i) - b.get(i);
+                d += tmp*tmp;
+            }
+        }
+        
+        return d;
+    }
+
+    public boolean isSymmetric()
+    {
+        return true;
+    }
+
+    public boolean isSubadditive()
+    {
+        return false;
+    }
+
+    public boolean isIndiscemible()
+    {
+        return true;
+    }
+
+    public double metricBound()
+    {
+        return Double.POSITIVE_INFINITY;
+    }
+    
+}
