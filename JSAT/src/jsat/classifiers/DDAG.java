c1cfd783b0408982885a270140b824e5d90b31c5
Edward Raff <Raff.Edward@gmail.com>
2012-08-19 01:09:49 +0000

    Added a new OneVsOne meta classifier, and an extension DDAG.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@353 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/classifiers/DDAG.java
@@ -0,0 +1,91 @@
+package jsat.classifiers;
+
+import java.util.PriorityQueue;
+
+/**
+ * Decision Directed Acyclic Graph (DDAG) classifier. DDAG extends
+ * binary decision classifiers into multi-class decision classifiers. Unlike 
+ * {@link OneVSOne}, DDAG results are hard classification decisions, and will 
+ * not give probabilistic estimates. Accuracy is often very similar, but DDAG
+ * classification speed can be significantly faster, as it does not evaluate all
+ * possible combinations of classifiers. 
+ * 
+ * @author Edward Raff
+ */
+public class DDAG extends OneVSOne
+{
+
+    /**
+     * Creates a new DDAG classifier to extend a binary classifier to handle multi-class problems. 
+     * @param baseClassifier the binary classifier to extend
+     * @param concurrentTrain <tt>true</tt> to have training of individual 
+     * classifiers occur in parallel, <tt>false</tt> to have them use their 
+     * native parallel training method.  
+     */
+    public DDAG(Classifier baseClassifier, boolean concurrentTrain)
+    {
+        super(baseClassifier, concurrentTrain);
+    }
+
+    /**
+     * Creates a new DDAG classifier to extend a binary classifier to handle multi-class problems. 
+     * @param baseClassifier the binary classifier to extend
+     */
+    public DDAG(Classifier baseClassifier)
+    {
+        super(baseClassifier);
+    }
+
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        CategoricalResults cr = new CategoricalResults(predicting.getNumOfCategories());
+        
+        //Use a priority que so that we always pick the two lowest value class labels, makes indexing into the oneVsOne array simple
+        PriorityQueue<Integer> options = new PriorityQueue<Integer>(predicting.getNumOfCategories());
+        for(int i = 0; i < cr.size(); i++)
+            options.add(i);
+        
+        
+        CategoricalResults subRes;
+        int c1, c2;
+        //We will now loop through and repeatedly pick two combinations, and eliminate the loser, until there is one winer
+        while(options.size() > 1)
+        {
+            c1 = options.poll();
+            c2 = options.poll();
+            
+            subRes = oneVone[c1][c2-c1-1].classify(data);
+            
+            if(subRes.mostLikely() == 0)//c1 wins, c2 no longer a candidate
+                options.add(c1);
+            else//c2 wins, c1 no onger a candidate
+                options.add(c2);
+        }
+        
+        cr.setProb(options.peek(), 1.0);
+        
+        
+        return cr;
+    }
+
+    @Override
+    public Classifier clone()
+    {
+        DDAG clone = new DDAG(baseClassifier.clone(), isConcurrentTraining());
+        if (oneVone != null)
+        {
+            clone.oneVone = new Classifier[oneVone.length][];
+            for (int i = 0; i < oneVone.length; i++)
+            {
+                clone.oneVone[i] = new Classifier[oneVone[i].length];
+                for (int j = 0; j < oneVone[i].length; j++)
+                    clone.oneVone[i][j] = oneVone[i][j].clone();
+            }
+        }
+        if(predicting != null)
+            clone.predicting = predicting.clone();
+
+        return clone;
+    }
+}
---
+++ JSAT/src/jsat/classifiers/OneVSOne.java
@@ -0,0 +1,201 @@
+package jsat.classifiers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jsat.utils.FakeExecutor;
+
+/**
+ * A One VS One classifier extends binary decision classifiers into multi-class 
+ * decision classifiers. This is done by creating a binary-classification 
+ * problem for every possible pair of classes, and then classifying by taking 
+ * the result of all possible combinations and choosing the class that got the 
+ * most results. This allows for a soft decision result, however, it often 
+ * proves to be a meaningless soft boundary. 
+ * 
+ * @author Edward Raff
+ */
+public class OneVSOne implements Classifier
+{
+    /**
+     * Main binary classifier 
+     */
+    protected Classifier baseClassifier;
+    /**
+     * Uper-diagonal matrix of classifiers sans the first index since a 
+     * classifier vs itself is useless. First index is the first dimensions is 
+     * the first class, 2nd index + the value of the first + 1 is the opponent 
+     * class
+     */
+    protected Classifier[][] oneVone;
+    private boolean concurrentTrain;
+    protected CategoricalData predicting;
+
+    /**
+     * Creates a new One-vs-One classifier
+     * @param baseClassifier the binary classifier to extend
+     */
+    public OneVSOne(Classifier baseClassifier)
+    {
+        this(baseClassifier, false);
+    }
+    
+    /**
+     * Creates a new One-vs-One classifier 
+     * @param baseClassifier the binary classifier to extend
+     * @param concurrentTrain <tt>true</tt> to have training of individual 
+     * classifiers occur in parallel, <tt>false</tt> to have them use their 
+     * native parallel training method. 
+     */
+    public OneVSOne(Classifier baseClassifier, boolean concurrentTrain)
+    {
+        this.baseClassifier = baseClassifier;
+        this.concurrentTrain = concurrentTrain;
+    }
+
+    /**
+     * Controls whether or not training of the several classifiers occurs concurrently or sequentually. 
+     * @param concurrentTrain <tt>true</tt> to have training of individual 
+     * classifiers occur in parallel, <tt>false</tt> to have them use their 
+     * native parallel training method. 
+     */
+    public void setConcurrentTraining(boolean concurrentTrain)
+    {
+        this.concurrentTrain = concurrentTrain;
+    }
+
+    /**
+     * 
+     * @return <tt>true</tt> if training of individual 
+     * classifiers occur in parallel, <tt>false</tt> they use their 
+     * native parallel training method.
+     */
+    public boolean isConcurrentTraining()
+    {
+        return concurrentTrain;
+    }
+    
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        CategoricalResults cr = new CategoricalResults(predicting.getNumOfCategories());
+        for (int i = 0; i < oneVone.length; i++)
+        {
+            for (int j = 0; j < oneVone[i].length; j++)
+            {
+                CategoricalResults subRes = oneVone[i][j].classify(data);
+                int mostLikely = subRes.mostLikely();
+                if(mostLikely == 0)
+                    cr.incProb(i, 1.0);
+                else
+                    cr.incProb(i+j+1, 1.0);
+            }
+        }
+        
+        cr.normalize();
+        return cr;
+    }
+
+    @Override
+    public void trainC(final ClassificationDataSet dataSet, final ExecutorService threadPool)
+    {
+        oneVone = new Classifier[dataSet.getClassSize()][];
+        
+        List<List<DataPoint>> dataByCategory = new ArrayList<List<DataPoint>>(dataSet.getClassSize());
+        for(int i = 0; i < dataSet.getClassSize(); i++)
+            dataByCategory.add(dataSet.getSamples(i));
+        
+        final CountDownLatch latch = new CountDownLatch(oneVone.length*(oneVone.length-1)/2);
+        
+        for(int i = 0; i < oneVone.length; i++)
+        {
+            oneVone[i] = new Classifier[oneVone.length-i-1];
+            
+            for(int j = 0; j < oneVone.length-i-1; j++)
+            {
+                final Classifier curClassifier = baseClassifier.clone();
+                
+                oneVone[i][j] = curClassifier;
+                final int otherClass = j+i+1;
+                CategoricalData subPred = new CategoricalData(2);
+                subPred.setOptionName(dataSet.getPredicting().getOptionName(i), 0);
+                subPred.setOptionName(dataSet.getPredicting().getOptionName(otherClass), 1);
+                
+                final ClassificationDataSet subDataSet = new ClassificationDataSet(dataSet.getNumNumericalVars(), dataSet.getCategories(), subPred); 
+                
+                //Fill sub data set with the two classes
+                for(DataPoint dp : dataByCategory.get(i))
+                    subDataSet.addDataPoint(dp.getNumericalValues(), dp.getCategoricalValues(), 0);
+                for(DataPoint dp : dataByCategory.get(otherClass))
+                    subDataSet.addDataPoint(dp.getNumericalValues(), dp.getCategoricalValues(), 1);
+
+                if(!concurrentTrain)
+                {
+                    if(threadPool != null && !(threadPool instanceof FakeExecutor))
+                        curClassifier.trainC(subDataSet, threadPool);
+                    else
+                        curClassifier.trainC(subDataSet);
+                    continue;
+                }
+                //Else, concurrent
+                threadPool.submit(new Runnable() {
+
+                    @Override
+                    public void run()
+                    {
+                        curClassifier.trainC(subDataSet);
+                        latch.countDown();
+                    }
+                });
+            }
+        }
+        
+        if(concurrentTrain)
+            try
+        {
+            latch.await();
+        }
+        catch (InterruptedException ex)
+        {
+            Logger.getLogger(OneVSOne.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        predicting = dataSet.getPredicting();
+        
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet)
+    {
+        trainC(dataSet, new FakeExecutor());
+    }
+
+    @Override
+    public boolean supportsWeightedData()
+    {
+        return baseClassifier.supportsWeightedData();
+    }
+
+    @Override
+    public Classifier clone()
+    {
+        OneVSOne clone = new OneVSOne(baseClassifier.clone(), concurrentTrain);
+        if (oneVone != null)
+        {
+            clone.oneVone = new Classifier[oneVone.length][];
+            for (int i = 0; i < oneVone.length; i++)
+            {
+                clone.oneVone[i] = new Classifier[oneVone[i].length];
+                for (int j = 0; j < oneVone[i].length; j++)
+                    clone.oneVone[i][j] = oneVone[i][j].clone();
+            }
+        }
+        if(predicting != null)
+            clone.predicting = predicting.clone();
+
+        return clone;
+    }
+}
