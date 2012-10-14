81f62c531679bcd26d624072a2f7766a789db26f
Edward Raff <Raff.Edward@gmail.com>
2012-10-14 07:25:20 +0000

    New utility vector implementation added to reduce the use of a pattern I've been using many times now in code.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@430 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/linear/VecPairedComparable.java
@@ -0,0 +1,24 @@
+package jsat.linear;
+
+/**
+ * Utility class for using {@link VecPaired} when the paired value is comparable
+ * , and the vectors need to be sorted based on their paired value. This class 
+ * performs exactly the same, and its only modification is that it is comparable
+ * based on the paired object type. 
+ * 
+ * @author Edward Raff
+ */
+public class VecPairedComparable<V extends Vec, P extends Comparable<P>> extends VecPaired<V, P> implements Comparable<VecPairedComparable<V, P>>
+{
+
+    public VecPairedComparable(V v, P p)
+    {
+        super(v, p);
+    }
+
+    @Override
+    public int compareTo(VecPairedComparable<V, P> o)
+    {
+        return this.getPair().compareTo(o.getPair());
+    }
+}
