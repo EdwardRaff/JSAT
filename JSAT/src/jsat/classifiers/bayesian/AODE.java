63850ea7135fc0c8956b2b986203a293fb0c19ad
Edward Raff <Raff.Edward@gmail.com>
2012-11-05 14:21:59 +0000

    Adding the Averaged One Dependence Estimator as an available classifier. It is an extension of Naive Bayes. The base of its implementation, the One Dependence Estimator, is broken out into its own class as well.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@468 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/classifiers/bayesian/AODE.java
@@ -0,0 +1,164 @@
+package jsat.classifiers.bayesian;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import jsat.classifiers.*;
+import jsat.exceptions.FailedToFitException;
+
+/**
+ * Averaged One-Dependence Estimators (AODE) is an extension of Naive Bayes that
+ * attempts to be more accurate by reducing the independence assumption. For 
+ * <i>n</i> data points with <i>d</i> categorical features, <i>d</i> 
+ * {@link ODE} classifier are created, each with a dependence on a different 
+ * attribute. The results of these classifiers is averaged to produce a final
+ * result.  The construction time is <i>O(n d<sup>2</sup>)</i>. Because of this 
+ * extra dependence requirement, the implementation only allows for categorical 
+ * features. <br>
+ * 
+ * @author Edward Raff
+ */
+public class AODE extends BaseUpdateableClassifier
+{
+    protected CategoricalData predicting;
+    protected ODE[] odes;
+
+    /**
+     * The minimum value to use a probability
+     */
+    private double m = 20;
+    
+    /**
+     * Creates a new AODE classifier. 
+     */
+    public AODE()
+    {
+    }
+    
+    /**
+     * Creates a copy of an AODE classifier
+     * @param toClone the classifier to clone
+     */
+    protected AODE(AODE toClone)
+    {
+        if(toClone.odes != null)
+        {
+            this.odes = new ODE[toClone.odes.length];
+            for(int i = 0; i < this.odes.length; i++)
+                this.odes[i] = toClone.odes[i].clone();
+            this.predicting = toClone.predicting.clone();
+        }
+        this.m = toClone.m;
+    }
+
+    @Override
+    public Classifier clone()
+    {
+        return new AODE(this);
+    }
+
+    @Override
+    public void setUp(CategoricalData[] categoricalAttributes, int numericAttributes, CategoricalData predicting)
+    {
+        if(categoricalAttributes.length < 1)
+            throw new FailedToFitException("At least 2 categorical varaibles are needed for AODE");
+        this.predicting = predicting;
+        odes = new ODE[categoricalAttributes.length];
+        
+        for(int i = 0; i < odes.length; i++)
+        {
+            odes[i] = new ODE(i);
+            odes[i].setUp(categoricalAttributes, numericAttributes, predicting);
+        }
+    }
+    
+    @Override
+    public void trainC(final ClassificationDataSet dataSet, ExecutorService threadPool)
+    {
+        setUp(dataSet.getCategories(), dataSet.getNumNumericalVars(), 
+                dataSet.getPredicting());
+        
+        final CountDownLatch latch = new CountDownLatch(odes.length);
+
+        for (int i = 0; i < odes.length; i++)
+        {
+            final ODE ode = odes[i];
+            threadPool.submit(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    for (int i = 0; i < dataSet.getSampleSize(); i++)
+                        ode.update(dataSet.getDataPoint(i), dataSet.getDataPointCategory(i));
+                    latch.countDown();
+                }
+            });
+        }
+        try
+        {
+            latch.await();
+        }
+        catch (InterruptedException ex)
+        {
+            trainC(dataSet);
+        }
+    }
+
+    @Override
+    public void update(DataPoint dataPoint, int targetClass)
+    {
+        for(ODE ode : odes)
+            ode.update(dataPoint, targetClass);
+    }
+
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        CategoricalResults cr = new CategoricalResults(predicting.getNumOfCategories());
+        
+        int[] catVals = data.getCategoricalValues();
+        for(int c = 0; c < cr.size(); c++)
+        {
+            double prob = 0.0;
+            for (ODE ode : odes)
+                if (ode.priors[c][catVals[ode.dependent]] < m)
+                    continue;
+                else
+                    prob += Math.exp(ode.getLogPrb(catVals, c));
+            cr.setProb(c, prob);
+        }
+        cr.normalize();
+        
+        return cr;
+    }
+
+    @Override
+    public boolean supportsWeightedData()
+    {
+        return true;
+    }
+
+    /**
+     * Sets the minimum prior observation value needed for an attribute 
+     * combination to have enough support to be included in the final estimate. 
+     * 
+     * @param m the minimum needed score
+     */
+    public void setM(double m)
+    {
+        if(m < 0 || Double.isInfinite(m) || Double.isNaN(m))
+            throw new ArithmeticException("The minimum count must be a non negative number");
+        this.m = m;
+    }
+
+    /**
+     * Returns 
+     * @return 
+     */
+    public double getM()
+    {
+        return m;
+    }
+    
+    
+    
+}
---
+++ JSAT/src/jsat/classifiers/bayesian/ODE.java
@@ -0,0 +1,178 @@
+package jsat.classifiers.bayesian;
+
+import java.util.Arrays;
+import jsat.classifiers.*;
+import jsat.exceptions.FailedToFitException;
+
+/**
+ * One-Dependence Estimators (ODE) is an extension of Naive Bayes that, instead 
+ * of assuming all features are independent, assumes all features are dependent 
+ * on one other feature besides the target class. Because of this extra 
+ * dependence requirement, the implementation only allows for categorical 
+ * features. 
+ * 
+ * @author Edward Raff
+ */
+public class ODE extends BaseUpdateableClassifier 
+{
+    /**
+     * The attribute we will be dependent on
+     */
+    protected int dependent;
+    /**
+     * The number of possible values in the target class
+     */
+    protected int predTargets;
+    /**
+     * The number of possible values for the dependent variable
+     */
+    protected int depTargets;
+    /**
+     * First index is the number of target values <br>
+     * 2nd index is the number of values for the dependent variable <br>
+     * 3rd is the number of categorical variables, including the dependent one <br>
+     * 4th is the count for the variable value <br>
+     */
+    protected double[][][][] counts;
+    /**
+     * The prior probability of each combination of target and dependent variable
+     */
+    protected double[][] priors;
+    protected double priorSum;
+
+    /**
+     * Creates a new ODE classifier 
+     * @param dependent the categorical feature to be dependent of
+     */
+    public ODE(int dependent)
+    {
+        this.dependent = dependent;
+    }
+    
+    /**
+     * Copy constructor
+     * @param toClone the ODE to copy
+     */
+    protected ODE(ODE toClone)
+    {
+        this(toClone.dependent);
+        this.predTargets = toClone.predTargets;
+        this.depTargets = toClone.depTargets;
+        if (toClone.counts != null)
+        {
+            this.counts = new double[toClone.counts.length][][][];
+            for (int i = 0; i < this.counts.length; i++)
+            {
+                this.counts[i] = new double[i][][];
+                for (int j = 0; j < this.counts[i].length; j++)
+                {
+                    this.counts[i][j] = new double[toClone.counts[i][j].length][];
+                    for (int z = 0; z < this.counts[i][j].length; z++)
+                    {
+                        this.counts[i][j][z] =
+                                Arrays.copyOf(toClone.counts[i][j][z],
+                                toClone.counts[i][j][z].length);
+                    }
+                }
+            }
+            
+            this.priors = new double[toClone.priors.length][];
+            for(int i = 0; i < this.priors.length; i++)
+                this.priors[i] = Arrays.copyOf(toClone.priors[i], toClone.priors[i].length);
+            this.priorSum = toClone.priorSum;
+        }
+    }
+
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        CategoricalResults cr = new CategoricalResults(predTargets);
+        
+        int[] catVals = data.getCategoricalValues();
+        for (int c = 0; c < predTargets; c++)
+        {
+            double logProb = getLogPrb(catVals, c);
+            
+            cr.setProb(c, Math.exp(logProb)); 
+        }
+        cr.normalize();
+
+        return cr;
+    }
+
+    @Override
+    public boolean supportsWeightedData()
+    {
+        return true;
+    }
+
+    @Override
+    public ODE clone()
+    {
+        return new ODE(this);
+    }
+
+    @Override
+    public void setUp(CategoricalData[] categoricalAttributes, int numericAttributes, CategoricalData predicting)
+    {
+        if(categoricalAttributes.length < 1)
+            throw new FailedToFitException("At least 2 categorical varaibles are needed for ODE");
+        CategoricalData[] catData = categoricalAttributes;
+        predTargets = predicting.getNumOfCategories();
+        depTargets = catData[dependent].getNumOfCategories();
+        counts = new double[predTargets][depTargets][catData.length][];
+        for(int i = 0; i < counts.length; i++)
+            for(int j = 0; j < counts[i].length; j++)
+                for(int z = 0; z < counts[i][j].length; z++)
+                {
+                    counts[i][j][z] = new double[catData[z].getNumOfCategories()];
+                    Arrays.fill(counts[i][j][z], 1.0);//Fill will laplace
+                }
+        
+        priors = new double[predTargets][depTargets];
+        for(int i = 0; i < priors.length; i++)
+        {
+            Arrays.fill(priors[i], 1.0);
+            priorSum += priors[i].length;
+        }
+    }
+
+    @Override
+    public void update(DataPoint dataPoint, int targetClass)
+    {
+        int[] catVals = dataPoint.getCategoricalValues();
+        double weight = dataPoint.getWeight();
+        for (int j = 0; j < catVals.length; j++)
+            if (j == dependent)
+                continue;
+            else
+                counts[targetClass][catVals[dependent]][j][catVals[j]] += weight;
+        priors[targetClass][catVals[dependent]] += weight;
+        priorSum += weight;
+    }
+
+    /**
+     * 
+     * @param catVals the catigorical values for a data point
+     * @param c the target value to get the probability of
+     * @param xi the value of the dependent variable in the data point
+     * @return the non normalized log probability of the data point belonging to
+     * the target class <tt>c</tt>
+     */
+    protected double getLogPrb(int[] catVals, int c)
+    {
+        double logProb = 0.0;
+        int xi = catVals[dependent];
+        for (int j = 0; j < catVals.length; j++)
+        {
+            if(j == dependent)
+                continue;
+            double sum = 0;
+            for(int z = 0; z < counts[c][xi][j].length; z++)
+                sum += counts[c][xi][j][z];
+            logProb += Math.log(counts[c][xi][j][catVals[j]]/sum);
+        }
+        
+        return logProb + Math.log(priors[c][xi]/priorSum);
+    }
+}
