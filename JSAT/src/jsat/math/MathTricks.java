3c8b38201438b5a69af1c3b65fdfeabf20cc8444
Edward Raff <Raff.Edward@gmail.com>
2012-12-03 00:10:42 +0000

    Adding a class for static methods that can be used to compute common math patterns in a numerically stable way
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@493 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/math/MathTricks.java
@@ -0,0 +1,71 @@
+package jsat.math;
+
+import jsat.linear.DenseVector;
+import jsat.linear.Vec;
+
+/**
+ * This class provides utilities for performing specific arithmetic patterns in 
+ * numerically stable / efficient ways. 
+ * 
+ * @author Edward Raff
+ */
+public class MathTricks
+{
+
+    private MathTricks()
+    {
+    }
+    
+    
+    /**
+     * Provides a numerically table way to perform the log of a sum of 
+     * exponentiations. The computed result is <br>
+     * <pre>
+     *    /  n            \
+     *    |=====          |
+     *    |\              |
+     * log| >    exp/val \|
+     *    |/        \   i/|
+     *    |=====          |
+     *    \i = 1          /
+     * </pre>
+     *
+     * @param vals the array of values to exponentiate and add
+     * @param maxValue the maximum value in the array 
+     * @return the log of the sum of the exponentiated values
+     */
+    public static double logSumExp(Vec vals, double maxValue)
+    {
+        double expSum = 0.0;
+        for(int i = 0; i < vals.length(); i++)
+            expSum += Math.exp(vals.get(i)-maxValue);
+        
+        return maxValue + expSum;
+    }
+    
+    /**
+     * Provides a numerically table way to perform the log of a sum of 
+     * exponentiations. The computed result is <br>
+     * <pre>
+     *    /  n            \
+     *    |=====          |
+     *    |\              |
+     * log| >    exp/val \|
+     *    |/        \   i/|
+     *    |=====          |
+     *    \i = 1          /
+     * </pre>
+     * 
+     * @param vals the array of values to exponentiate and add
+     * @param maxValue the maximum value in the array 
+     * @return the log of the sum of the exponentiated values
+     */
+    public static double logSumExp(double[] vals, double maxValue)
+    {
+        double expSum = 0.0;
+        for(int i = 0; i < vals.length; i++)
+            expSum += Math.exp(vals[i]-maxValue);
+        
+        return maxValue + expSum;
+    }
+}
