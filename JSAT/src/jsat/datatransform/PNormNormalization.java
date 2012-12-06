9686dc95eae3ca65d343ffbaedcf043c50b4ac2f
Edward Raff <Raff.Edward@gmail.com>
2012-12-06 05:35:37 +0000

    Adding pNormNormalization transform.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@510 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/datatransform/PNormNormalization.java
@@ -0,0 +1,64 @@
+package jsat.datatransform;
+
+import jsat.DataSet;
+import jsat.classifiers.DataPoint;
+import jsat.linear.IndexValue;
+import jsat.linear.Vec;
+
+/**
+ * PNormNormalization transformation performs normalizations of a vector x by 
+ * one its p-norms where p is in (0, Infinity)
+ * 
+ * @author Edward Raff
+ */
+public class PNormNormalization implements DataTransform
+{
+    private double p;
+
+    /**
+     * Creates a new p norm
+     * @param p the norm to use
+     */
+    public PNormNormalization(double p)
+    {
+        this.p = p;
+    }
+    
+    @Override
+    public DataPoint transform(DataPoint dp)
+    {
+        DataPoint dpNew = dp.clone();
+        
+        Vec vec = dpNew.getNumericalValues();
+        vec.mutableDivide(vec.pNorm(p));
+        
+        return dpNew;
+    }
+
+    @Override
+    public DataTransform clone()
+    {
+        return new PNormNormalization(p);
+    }
+    
+    static public class PNormNormalizationFactory implements DataTransformFactory
+    {
+        private double p;
+
+        /**
+         * Creates a new p norm factory
+         * @param p the norm to use
+         */
+        public PNormNormalizationFactory(double p)
+        {
+            this.p = p;
+        }
+
+        @Override
+        public DataTransform getTransform(DataSet dataset)
+        {
+            return new PNormNormalization(p);
+        }
+        
+    }
+}
