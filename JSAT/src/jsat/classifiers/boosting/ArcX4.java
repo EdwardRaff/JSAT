12a5219863ef2a0dae3530022ef6ca52b911d611
Edward Raff <Raff.Edward@gmail.com>
2012-12-23 20:55:11 +0000

    Implementation of the Arc-x4 arcing classifier by Breiman.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@534 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/classifiers/boosting/ArcX4.java
@@ -0,0 +1,225 @@
+package jsat.classifiers.boosting;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import jsat.classifiers.CategoricalData;
+import jsat.classifiers.CategoricalResults;
+import jsat.classifiers.ClassificationDataSet;
+import jsat.classifiers.Classifier;
+import jsat.classifiers.DataPoint;
+import jsat.parameters.Parameter;
+import jsat.parameters.Parameterized;
+import jsat.utils.FakeExecutor;
+
+/**
+ * Arc-x4 is a ensemble-classifier that performs re-weighting of the data points 
+ * based on the total number of errors that have occurred for the data point.
+ * <br><br>
+ * See: Breiman, L. (1998). <i>Arcing Classifiers</i>. The Annals of Statistics,
+ * 26(3), 801–824.
+ * 
+ * @author Edward Raff
+ */
+public class ArcX4 implements Classifier, Parameterized
+{
+    private Classifier weakLearner;
+    private int iterations;
+    
+    private double coef = 1;
+    private double expo = 4;
+    
+    private CategoricalData predicing;
+    private Classifier[] hypoths;
+    
+    private List<Parameter> params = Collections.unmodifiableList(Parameter.getParamsFromMethods(this));
+    private Map<String, Parameter> paramMap = Parameter.toParameterMap(params);
+    
+    /**
+     * Creates a new Arc-X4 classifier 
+     * 
+     * @param weakLearner the weak learner to use
+     * @param iterations the number of iterations to perform
+     */
+    public ArcX4(Classifier weakLearner, int iterations)
+    {
+        setWeakLearner(weakLearner);
+        setIterations(iterations);
+    }
+
+    /**
+     * Sets the weak learner used at each iteration of learning
+     * @param weakLearner the weak learner to use
+     */
+    public void setWeakLearner(Classifier weakLearner)
+    {
+        if(!weakLearner.supportsWeightedData())
+            throw new RuntimeException("Weak learners must support weighted data samples");
+        this.weakLearner = weakLearner;
+    }
+
+    /**
+     * Returns the weak learner used
+     * @return the weak learner used
+     */
+    public Classifier getWeakLearner()
+    {
+        return weakLearner;
+    }
+
+    /**
+     * Sets the number of iterations to perform
+     * @param iterations the number of iterations to do
+     */
+    public void setIterations(int iterations)
+    {
+        this.iterations = iterations;
+    }
+
+    /**
+     * Returns the number of iterations to learn
+     * @return the number of iterations to learn
+     */
+    public int getIterations()
+    {
+        return iterations;
+    }
+
+    /**
+     * Weights are updated as 1+coef*errors<sup>expo</sup>. This sets the 
+     * coefficient used to update the errors
+     * 
+     * @param coef the multiplicative factor on the errors in weight construction
+     */
+    public void setCoefficient(double coef)
+    {
+        if(coef <= 0 || Double.isInfinite(coef) || Double.isNaN(coef))
+            throw new ArithmeticException("The coefficient must be a positive constant");
+        this.coef = coef;
+    }
+
+    /**
+     * Returns the coefficient use when re-weighting
+     * @return the coefficient use when re-weighting
+     */
+    public double getCoefficient()
+    {
+        return coef;
+    }
+
+    /**
+     * Weights are updated as 1+coef*errors<sup>expo</sup>. This sets the 
+     * exponent used to update the errors
+     * @param expo the exponent to use
+     */
+    public void setExponent(double expo)
+    {
+        if(expo <= 0 || Double.isInfinite(expo) || Double.isNaN(expo))
+            throw new ArithmeticException("The exponent must be a positive constant");
+        this.expo = expo;
+    }
+
+    /**
+     * Returns the exponent used when re-weighting 
+     * @return the exponent used when re-weighting 
+     */
+    public double getExponent()
+    {
+        return expo;
+    }
+    
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        CategoricalResults cr = new CategoricalResults(predicing.getNumOfCategories());
+        
+        for(Classifier hypoth : hypoths)
+            cr.incProb(hypoth.classify(data).mostLikely(), 1.0);
+        
+        cr.normalize();
+        
+        return cr;
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet, ExecutorService threadPool)
+    {
+        //Create a low memory clone that only has different dataPoint Objects to save space
+        ClassificationDataSet cds = dataSet.shallowClone();
+        for(int i = 0; i < cds.getSampleSize(); i++)
+        {
+            DataPoint dp = cds.getDataPoint(i);
+            cds.setDataPoint(i, new DataPoint(dp.getNumericalValues(), dp.getCategoricalValues(), dp.getCategoricalData()));
+        }
+        
+        //Everyone starts with no errors
+        int[] errors = new int[cds.getSampleSize()];
+        hypoths = new Classifier[iterations];
+        for(int t = 0; t < hypoths.length; t++)
+        {
+            for(int i = 0; i < cds.getSampleSize(); i++)
+                cds.getDataPoint(i).setWeight(1+coef*Math.pow(errors[i], expo));
+            
+            Classifier hypoth = weakLearner.clone();
+            
+            if(threadPool == null || threadPool instanceof FakeExecutor)
+                hypoth.trainC(cds);
+            else
+                hypoth.trainC(cds, threadPool);
+            
+            hypoths[t] = hypoth;
+            
+            for(int i = 0; i < errors.length; i++)
+                if(hypoth.classify(cds.getDataPoint(i)).mostLikely() != cds.getDataPointCategory(i))
+                    errors[i]++;
+        }
+        
+        this.predicing = cds.getPredicting();
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet)
+    {
+        trainC(dataSet, null);
+    }
+
+    @Override
+    public boolean supportsWeightedData()
+    {
+        return false;
+    }
+
+    @Override
+    public ArcX4 clone()
+    {
+        ArcX4 clone = new ArcX4(weakLearner.clone(), iterations);
+        
+        clone.coef = this.coef;
+        clone.expo = this.expo;
+        
+        if(this.predicing != null)
+            clone.predicing = this.predicing.clone();
+        if(this.hypoths != null)
+        {
+            clone.hypoths = new Classifier[this.hypoths.length];
+            for(int i = 0; i < clone.hypoths.length; i++)
+                clone.hypoths[i] = this.hypoths[i].clone();
+        }
+        
+        return clone;
+    }
+
+    @Override
+    public List<Parameter> getParameters()
+    {
+        return params;
+    }
+
+    @Override
+    public Parameter getParameter(String paramName)
+    {
+        return paramMap.get(paramName);
+    }
+    
+}
