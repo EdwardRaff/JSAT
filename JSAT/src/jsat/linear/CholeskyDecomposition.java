ee3f014af3bcfc1be3876ee2431d4c8a2bd019a7
Edward Raff <Raff.Edward@gmail.com>
2013-02-25 05:31:17 +0000

    Adding initial parallel version of Cholesky decomposition. Performance scaling is not as good as I would like. Needs an efficient block version
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@598 c8979423-a768-6f17-17f2-fb57d64bf5d2

--- JSAT/src/jsat/linear/CholeskyDecomposition.java
+++ JSAT/src/jsat/linear/CholeskyDecomposition.java
@@ -4,7 +4,11 @@ package jsat.linear;
 import java.io.Serializable;
 import java.util.concurrent.ExecutorService;
 import static java.lang.Math.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static jsat.linear.LUPDecomposition.*;
+import jsat.utils.SystemInfo;
 
 /**
  * The Cholesky Decomposition factors a symmetric positive definite matrix A 
@@ -15,6 +19,8 @@ import static jsat.linear.LUPDecomposition.*;
  */
 public class CholeskyDecomposition implements Serializable
 {
+    //TODO add block decomposition for efficency 
+    
     /**
      * Contains the matrix 'L', but instead of just keeping the lower triangular, we keep it
      * in a symmetric  copy so {@link LUPDecomposition#forwardSub(jsat.linear.Matrix, jsat.linear.Vec) }
@@ -33,54 +39,76 @@ public class CholeskyDecomposition implements Serializable
      * 
      * @param A the matrix to create the Cholesky Decomposition of
      */
-    public CholeskyDecomposition(Matrix A)
+    public CholeskyDecomposition(final Matrix A)
     {
         if(!A.isSquare())
             throw new ArithmeticException("Input matrix must be symmetric positive definite");
         
         L = A;
-       
-        for(int j = 0; j < A.rows(); j++)
+        final int ROWS = A.rows();
+
+        for (int j = 0; j < ROWS; j++)
         {
-            /**
-             *                _________________
-             *               /       j - 1
-             *              /        =====
-             *             /         \      2
-             * L    =     /   A    -  >    L
-             *  j j      /     j j   /      j k
-             *          /            =====
-             *        \/             k = 1
-             */
-            double L_jj = A.get(j, j);
-            for(int k = 0; k < j; k++)
-                L_jj -= pow(L.get(j, k), 2);
-            L.set(j, j, sqrt(L_jj));
-            
-            /*
-             * 
-             *             /       j - 1          \
-             *             |       =====          |
-             *          1  |       \              |
-             * L    = ---- |A    -  >    L    L   |
-             *  i j   L    | i j   /      i k  j k|
-             *         j j |       =====          |
-             *             \       k = 1          /
-             */
-            for(int i = j+1; i < L.rows(); i++)
-            {
-                double L_ij = A.get(i, j);
-                for(int k = 0; k < j; k++)
-                    L_ij -= L.get(i, k)*L.get(j, k);
-                L.set(i, j, L_ij/L_jj);
-            }
-            
+            double L_jj = computeLJJ(A, j);
+            L.set(j, j, L_jj);
+            updateRows(j, j + 1, ROWS, 1, A, L_jj);
         }
+        copyUpperToLower(ROWS);
+    }
+
+    /**
+     * Computes the Cholesky Decomposition of the matrix A. The matrix 
+     * <tt>A</tt> will be altered to form the decomposition <tt>L</tt>. If A is 
+     * still needed after this computation a {@link Matrix#clone() clone} of the
+     * matrix should be given instead. <br>
+     * NOTE: No check for the symmetric positive definite property will occur. 
+     * The results of passing a matrix that does not meet this properties is 
+     * undefined. 
+     * 
+     * @param A the matrix to create the Cholesky Decomposition of
+     * @param threadpool the source of threads for computation
+     */
+    public CholeskyDecomposition(final Matrix A, ExecutorService threadpool)
+    {
+        if(!A.isSquare())
+            throw new ArithmeticException("Input matrix must be symmetric positive definite");
         
-        //Now copy so that All of L is filled 
-        for(int i = 0; i < L.rows(); i++)
-            for(int j = 0; j < i; j++)
-                L.set(j, i, L.get(i, j));
+        L = A;
+        final int ROWS = A.rows();
+        double nextLJJ = computeLJJ(A, 0);
+        for (int j = 0; j < ROWS; j++)
+        {
+            final int J = j;
+            final double L_jj = nextLJJ;//computeLJJ(A, j);
+            L.set(j, j, L_jj);
+            final CountDownLatch latch = new CountDownLatch(SystemInfo.LogicalCores-1);
+            for (int i = 1; i < SystemInfo.LogicalCores; i++)
+            {
+                final int ID = i;
+                threadpool.submit(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        updateRows(J, J + 1+ID, ROWS, SystemInfo.LogicalCores, A, L_jj);
+                        latch.countDown();
+                    }
+                });
+            }
+            try
+            {
+                updateRows(J, J+1, ROWS, SystemInfo.LogicalCores, A, L_jj);
+                if(j+1 < ROWS)
+                    nextLJJ = computeLJJ(A, j+1);
+                latch.await();
+            }
+            catch (InterruptedException ex)
+            {
+                Logger.getLogger(CholeskyDecomposition.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        copyUpperToLower(ROWS);
     }
 
     /**
@@ -160,5 +188,52 @@ public class CholeskyDecomposition implements Serializable
         for(int i = 0; i < L.rows(); i++)
             det *= L.get(i, i);
         return det;
+    }
+
+    private double computeLJJ(final Matrix A, final int j)
+    {
+        /**
+         *                _________________
+         *               /       j - 1
+         *              /        =====
+         *             /         \      2
+         * L    =     /   A    -  >    L
+         *  j j      /     j j   /      j k
+         *          /            =====
+         *        \/             k = 1
+         */
+        double L_jj = A.get(j, j);
+        for(int k = 0; k < j; k++)
+            L_jj -= pow(L.get(j, k), 2);
+        return sqrt(L_jj);
+    }
+
+    private void updateRows(final int j, final int start, final int end, final int skip, final Matrix A, final double L_jj)
+    {
+        /*
+         * 
+         *             /       j - 1          \
+         *             |       =====          |
+         *          1  |       \              |
+         * L    = ---- |A    -  >    L    L   |
+         *  i j   L    | i j   /      i k  j k|
+         *         j j |       =====          |
+         *             \       k = 1          /
+         */
+        for(int i = start; i < end; i+=skip)
+        {
+            double L_ij = A.get(i, j);
+            for(int k = 0; k < j; k++)
+                L_ij -= L.get(i, k)*L.get(j, k);
+            L.set(i, j, L_ij/L_jj);
+        }
+    }
+
+    private void copyUpperToLower(final int ROWS)
+    {
+        //Now copy so that All of L is filled 
+        for (int i = 0; i < ROWS; i++)
+            for (int j = 0; j < i; j++)
+                L.set(j, i, L.get(i, j));
     }
 }
