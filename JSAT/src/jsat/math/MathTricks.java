4af45bde9b0c512988d2133acb810e5fdeb749cd
Edward Raff <Raff.Edward@gmail.com>
2013-03-06 20:23:06 +0000

    Added softmax normalizatoin. Added a few default functions for common methods that one might want to apply
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@623 c8979423-a768-6f17-17f2-fb57d64bf5d2

--- JSAT/src/jsat/math/MathTricks.java
+++ JSAT/src/jsat/math/MathTricks.java
@@ -1,6 +1,6 @@
 package jsat.math;
 
-import jsat.linear.DenseVector;
+import static java.lang.Math.exp;
 import jsat.linear.Vec;
 
 /**
@@ -52,4 +52,83 @@ public class MathTricks
         
         return maxValue + expSum;
     }
+    
+    /**
+     * Applies the softmax function to the given array of values, normalizing 
+     * them so that each value is equal to<br><br>
+     * exp(x<sub>j</sub>) / &Sigma;<sub>&forall; i</sub> exp(x<sub>i</sub>)
+     * 
+     * @param x the array of values
+     * @param implicitExtra {@code true} if the softmax will assume there is 
+     * an extra implicit value not included in the array with a value of 1.0 
+     */
+    public static void softmax(double[] x, boolean implicitExtra)
+    {
+        double max = implicitExtra ? 1 : Double.NEGATIVE_INFINITY;
+        for(int i = 0; i < x.length; i++)
+            max = Math.max(max, x[i]);
+        
+        //exp is exp(0 - max), b/c exp(0) gives the implicit 1 value
+        double z =implicitExtra ? exp(-max) : 0;
+        for (int c = 0; c < x.length; c++)
+            z += (x[c] = exp(x[c] - max));
+        for (int c = 0; c < x.length; c++)
+            x[c] /= z;
+    }
+    
+    /**
+     * Convenience object for taking the {@link Math#sqrt(double) square root} 
+     * of the first index
+     */
+    public static final Function sqrtFunc = new FunctionBase() 
+    {
+        @Override
+        public double f(Vec x)
+        {
+            return Math.sqrt(x.get(0));
+        }
+    };
+    
+    /**
+     * Convenience object for taking the squared value
+     * of the first index
+     */
+    public static final Function sqrdFunc = new FunctionBase() {
+
+        @Override
+        public double f(Vec x)
+        {
+            double xx = x.get(0);
+            return xx*xx;
+        }
+    };
+    
+    /**
+     * Convenience object for taking the {@link Math#log(double) log } of the 
+     * first index
+     */
+    public static final Function logFunc = new FunctionBase() 
+    {
+        @Override
+        public double f(Vec x)
+        {
+            return Math.log(x.get(0));
+        }
+    };
+    
+    /**
+     * Convenience object for taking the {@link Math#exp(double) exp } of the 
+     * first index
+     */
+    public static final Function expFunc = new FunctionBase() 
+    {
+        @Override
+        public double f(Vec x)
+        {
+            return Math.exp(x.get(0));
+        }
+    };
+    
+    
+    
 }
