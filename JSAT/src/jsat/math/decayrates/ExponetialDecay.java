abe64a3078e154ac97a07e34128ac5f5552e3bbd
Edward Raff <Raff.Edward@gmail.com>
2013-03-07 05:29:13 +0000

    Added a minimum value to the decay so that you can make the last round a non zero result. Updated documentation.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@627 c8979423-a768-6f17-17f2-fb57d64bf5d2

--- JSAT/src/jsat/math/decayrates/ExponetialDecay.java
+++ JSAT/src/jsat/math/decayrates/ExponetialDecay.java
@@ -1,20 +1,44 @@
 package jsat.math.decayrates;
 
 /**
- * A possible value for a decaying learning rate. The initial value will be 
+ * An Exponential decay that drives the initial learning rate down to a fixed 
+ * value after a fixed number of iterations. The initial value will be 
  * decayed at an exponential rate. 
  * 
  * @author Edward Raff
  */
 public class ExponetialDecay implements DecayRate
 {
+    private double min;
+
+    /**
+     * Sets the minimum amount of learning rate that can occur. The decay will 
+     * decay down to and stop at the minimum. 
+     * 
+     * @param min a value less than the learning rate that, that will be the 
+     * minimum returned value 
+     */
+    public ExponetialDecay(double min)
+    {
+        this.min = min;
+    }
+
+    /**
+     * Creates a new decay rate that decays down to zero
+     */
+    public ExponetialDecay()
+    {
+        this(0.0);
+    }
+    
+    
 
     @Override
     public double rate(double time, double maxTime, double initial)
     {
         if(time < 0)
             throw new ArithmeticException("Negative time value given");
-        return initial*Math.exp(  -time / (maxTime / Math.log(maxTime)));
+        return (initial-min)*Math.exp(  -time / (maxTime / Math.log(maxTime)))+min;
     }
 
     @Override
