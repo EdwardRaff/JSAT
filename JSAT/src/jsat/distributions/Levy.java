d017c0b29160f209f39996a20a8d6ebf98e686b6
Edward Raff <Raff.Edward@gmail.com>
2013-03-09 20:08:48 +0000

    Adding implementation of the Levy Distribution
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@636 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/distributions/Levy.java
@@ -0,0 +1,185 @@
+
+package jsat.distributions;
+
+import static java.lang.Math.*;
+import jsat.linear.Vec;
+import jsat.math.SpecialMath;
+
+/**
+ * Implementation of the 
+ * <a href="http://en.wikipedia.org/wiki/L%C3%A9vy_distribution">Levy 
+ * Distribution <a/>
+ * 
+ * @author Edward Raff
+ */
+public class Levy extends Distribution
+{
+    private double location;
+    private double scale;
+    private double logScale;
+
+    public Levy(double scale, double location)
+    {
+        setScale(scale);
+        setLocation(location);
+    }
+
+    /**
+     * Sets the scale of the Levy distribution
+     * @param scale the new scale value, must be positive
+     */
+    public void setScale(double scale)
+    {
+        if(scale <= 0 || Double.isNaN(scale) || Double.isInfinite(scale))
+            throw new ArithmeticException("Scale must be a positive value, not " + scale);
+        this.scale = scale;
+        this.logScale = log(scale);
+    }
+
+    /**
+     * Returns the scale parameter used by this distribution
+     * @return the scale parameter
+     */
+    public double getScale()
+    {
+        return scale;
+    }
+
+    /**
+     * Sets location of the Levy distribution. 
+     * @param location the new location 
+     */
+    public void setLocation(double location)
+    {
+        if(Double.isNaN(location) || Double.isInfinite(location))
+            throw new ArithmeticException("location must be a real number");
+        this.location = location;
+    }
+
+    /**
+     * Returns the location parameter used by this distribution. 
+     * @return distribution
+     */
+    public double getLocation()
+    {
+        return location;
+    }
+
+    @Override
+    public double pdf(double x)
+    {
+        if(x < location)
+            return 0;
+        return exp(logPdf(x));
+    }
+
+    @Override
+    public double logPdf(double x)
+    {
+        if(x < location)
+            return Double.NEGATIVE_INFINITY;
+        final double mu = x-location;
+        return -(-mu*logScale+scale+3*mu*log(mu)+mu*log(PI)+mu*log(2))/(2*mu);
+    }
+
+    @Override
+    public double cdf(double x)
+    {
+        if(x < location)
+            return 0;
+        return SpecialMath.erfc(sqrt(scale/(2*(x-location))));
+    }
+    
+    @Override
+    public double invCdf(double p)
+    {
+        if(p < 0 || p > 1)
+            throw new ArithmeticException("Invalid probability " + p);
+        return scale/(2*pow(SpecialMath.invErfc(p), 2))+location;
+    }
+
+    @Override
+    public double min()
+    {
+        return location;
+    }
+
+    @Override
+    public double max()
+    {
+        return Double.POSITIVE_INFINITY;
+    }
+
+    @Override
+    public String getDistributionName()
+    {
+        return "Levy";
+    }
+
+    @Override
+    public String[] getVariables()
+    {
+        return new String[]{"Scale", "Location"};
+    }
+
+    @Override
+    public double[] getCurrentVariableValues()
+    {
+        return new double[] {scale, location};
+    }
+
+    @Override
+    public void setVariable(String var, double value)
+    {
+        if(var.equals(getVariables()[0]))
+            setScale(value);
+        else if(var.equals(getVariables()[1]))
+            setLocation(value);
+    }
+
+    @Override
+    public Levy clone()
+    {
+        return new Levy(scale, location);
+    }
+
+    @Override
+    public void setUsingData(Vec data)
+    {
+        setLocation(data.min());
+        
+        setScale(2*pow(SpecialMath.invErfc(0.5), 2)*(data.median()-location));
+        
+    }
+
+    @Override
+    public double mean()
+    {
+        return Double.POSITIVE_INFINITY;
+    }
+
+    @Override
+    public double mode()
+    {
+        return scale/3+location;
+    }
+
+    @Override
+    public double standardDeviation()
+    {
+        return Double.POSITIVE_INFINITY;
+    }
+    
+    @Override
+    public double variance()
+    {
+        return Double.POSITIVE_INFINITY;
+    }
+
+    @Override
+    public double skewness()
+    {
+        return Double.NaN;
+    }
+    
+}
