4bef1aeec5c7fa58c8121ef963f63e558959e4a3
Edward Raff <Raff.Edward@gmail.com>
2013-03-20 03:30:05 +0000

    Added the Passive Aggressive linear classifier. Also included simple test case
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@653 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/classifiers/linear/PassiveAggressive.java
@@ -0,0 +1,303 @@
+package jsat.classifiers.linear;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import jsat.classifiers.*;
+import jsat.exceptions.FailedToFitException;
+import jsat.linear.DenseVector;
+import jsat.linear.Vec;
+import jsat.parameters.Parameter;
+import jsat.parameters.Parameterized;
+import jsat.regression.BaseUpdateableRegressor;
+import jsat.regression.RegressionDataSet;
+import jsat.regression.UpdateableRegressor;
+
+/**
+ * An implementations of the 3 versions of the Passive Aggressive algorithm for 
+ * binary classification and regression. Its a type of online algorithm that 
+ * performs the minimal update necessary to correct for a mistake. 
+ * <br><br>
+ * See:<br>
+ * Crammer, K., Dekel, O., Keshet, J., Shalev-Shwartz, S., & Singer, Y. (2006). 
+ * <a href="http://dl.acm.org/citation.cfm?id=1248566">
+ * <i>Online passive-aggressive algorithms</i></a>. Journal of Machine Learning 
+ * Research, 7, 551–585. 
+ * 
+ * @author Edward Raff
+ */
+public class PassiveAggressive implements UpdateableClassifier, UpdateableRegressor, Parameterized
+{
+    private int epochs;
+    private double C = 0.01;
+    private double eps = 0.001;
+    private Vec w;
+    private Mode mode;
+    
+    private List<Parameter> params = Parameter.getParamsFromMethods(this);
+    private Map<String, Parameter> paramMap = Parameter.toParameterMap(params);
+
+    /**
+     * Creates a new Passive Aggressive learner that does 10 epochs and uses
+     * {@link Mode#PA1}
+     */
+    public PassiveAggressive()
+    {
+        this(10, Mode.PA1);
+    }
+    
+    /**
+     * Creates a new Passive Aggressive learner
+     * 
+     * @param epochs the number of training epochs to use during batch training
+     * @param mode which version of the update to perform 
+     */
+    public PassiveAggressive(int epochs, Mode mode)
+    {
+        this.epochs = epochs;
+        this.mode = mode;
+    }
+
+    /**
+     * Controls which version of the Passive Aggressive update is used
+     */
+    public static enum Mode 
+    {
+        /**
+         * The default Passive Aggressive algorithm, it performs correction 
+         * updates that make the minimal change necessary to correct the output 
+         * for a single input
+         */
+        PA,
+        /**
+         * Limits the aggressiveness by reducing the maximum correction to the 
+         * {@link #setC(double) aggressiveness parameter}
+         */
+        PA1, 
+        /**
+         * Limits the aggressiveness by adding a constant factor to the 
+         * denominator of the correction. 
+         */
+        PA2
+    }
+    
+    /**
+     * Set the aggressiveness parameter. Increasing the value of this parameter 
+     * increases the aggressiveness of the algorithm. It must be a positive 
+     * value. This parameter essentially performs a type of regularization on 
+     * the updates
+     * <br>
+     * An infinitely large value is equivalent to being completely aggressive, 
+     * and is performed when the mode is set to {@link Mode#PA}. 
+     * 
+     * @param C the positive aggressiveness parameter
+     */
+    public void setC(double C)
+    {
+        if(Double.isNaN(C) || Double.isInfinite(C) || C <= 0)
+            throw new ArithmeticException("Aggressiveness must be a positive constant");
+        this.C = C;
+    }
+    
+    /**
+     * Returns the aggressiveness parameter 
+     * @return the aggressiveness parameter 
+     */
+    public double getC()
+    {
+        return C;
+    }
+
+    /**
+     * Sets which version of the PA update is used. 
+     * @param mode which PA update style to perform
+     */
+    public void setMode(Mode mode)
+    {
+        this.mode = mode;
+    }
+
+    /**
+     * Returns which version of the PA update is used
+     * @return which PA update style is used
+     */
+    public Mode getMode()
+    {
+        return mode;
+    }
+
+    /**
+     * Sets the range for numerical prediction. If it is within range of the 
+     * given value, no error will be incurred. 
+     * @param eps the maximum acceptable difference in prediction and truth 
+     */
+    public void setEps(double eps)
+    {
+        this.eps = eps;
+    }
+
+    /**
+     * Returns the maximum acceptable difference in prediction and truth
+     * @return the maximum acceptable difference in prediction and truth
+     */
+    public double getEps()
+    {
+        return eps;
+    }
+
+    /**
+     * Sets the number of whole iterations through the training set that will be
+     * performed for training
+     * @param epochs the number of whole iterations through the data set
+     */
+    public void setEpochs(int epochs)
+    {
+        if(epochs < 1)
+            throw new IllegalArgumentException("epochs must be a positive value");
+        this.epochs = epochs;
+    }
+
+    /**
+     * Returns the number of epochs used for training
+     * @return the number of epochs used for training
+     */
+    public int getEpochs()
+    {
+        return epochs;
+    }
+
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        CategoricalResults cr = new CategoricalResults(2);
+        Vec x = data.getNumericalValues();
+        if(x.dot(w) > 0)
+            cr.setProb(1, 1);
+        else
+            cr.setProb(0, 1);
+        
+        return cr;
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet, ExecutorService threadPool)
+    {
+        trainC(dataSet);
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet)
+    {
+        BaseUpdateableClassifier.trainEpochs(dataSet, this, epochs);
+    }
+
+    @Override
+    public boolean supportsWeightedData()
+    {
+        return false;
+    }
+
+    @Override
+    public void setUp(CategoricalData[] categoricalAttributes, int numericAttributes, CategoricalData predicting)
+    {
+        if(predicting.getNumOfCategories() != 2)
+            throw new FailedToFitException("Only supports binary classification problems");
+        else if(numericAttributes < 1)
+            throw new FailedToFitException("only suppors learning from numeric attributes");
+        w = new DenseVector(numericAttributes);
+    }
+    
+    @Override
+    public void setUp(CategoricalData[] categoricalAttributes, int numericAttributes)
+    {
+        if(numericAttributes < 1)
+            throw new FailedToFitException("only suppors learning from numeric attributes");
+        w = new DenseVector(numericAttributes);
+    }
+
+    @Override
+    public void update(DataPoint dataPoint, int targetClass)
+    {
+        Vec x = dataPoint.getNumericalValues();
+        final int y_t = targetClass*2-1;
+        final double dot = x.dot(w);
+        
+        final double loss = Math.max(0, 1-y_t*dot);
+        if(loss == 0)
+            return;
+        
+        final double tau = getCorrection(loss, x);
+        
+        w.mutableAdd(y_t*tau, x);
+    }
+    
+    @Override
+    public void update(DataPoint dataPoint, double targetValue)
+    {
+        Vec x = dataPoint.getNumericalValues();
+        final double y_t = targetValue;
+        final double y_p = x.dot(w);
+        
+        final double loss = Math.max(0, Math.abs(y_p-y_t)-eps);
+        if(loss == 0)
+            return;
+        
+        final double tau = getCorrection(loss, x);
+        
+        w.mutableAdd(Math.signum(y_t-y_p)*tau, x);
+    }
+    
+    private double getCorrection(final double loss, Vec x)
+    {
+        final double xNorm = Math.pow(x.pNorm(2), 2);
+        if(mode == Mode.PA1)
+            return Math.min(C, loss/xNorm);
+        else if(mode == Mode.PA2)
+            return loss/(xNorm+1.0/(2*C));
+        else
+            return loss/xNorm;
+    }
+
+    @Override
+    public double regress(DataPoint data)
+    {
+        return w.dot(data.getNumericalValues());
+    }
+
+    @Override
+    public void train(RegressionDataSet dataSet, ExecutorService threadPool)
+    {
+        train(dataSet);
+    }
+
+    @Override
+    public void train(RegressionDataSet dataSet)
+    {
+        BaseUpdateableRegressor.trainEpochs(dataSet, this, epochs);
+    }
+
+    @Override
+    public PassiveAggressive clone()
+    {
+        PassiveAggressive clone = new PassiveAggressive(epochs, mode);
+        clone.eps = this.eps;
+        clone.C = this.C;
+        if(this.w != null)
+            clone.w = this.w;
+        
+        return clone;
+    }
+    
+    @Override
+    public List<Parameter> getParameters()
+    {
+        return Collections.unmodifiableList(params);
+    }
+
+    @Override
+    public Parameter getParameter(String paramName)
+    {
+        return paramMap.get(paramName);
+    }
+}
---
+++ JSAT/test/jsat/classifiers/linear/PassiveAggressiveTest.java
@@ -0,0 +1,96 @@
+
+package jsat.classifiers.linear;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import jsat.FixedProblems;
+import jsat.classifiers.*;
+import jsat.linear.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public class PassiveAggressiveTest
+{
+    
+    public PassiveAggressiveTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of trainC method, of class PassiveAggressive.
+     */
+    @Test
+    public void testTrainC_ClassificationDataSet()
+    {
+        System.out.println("trainC");
+        ClassificationDataSet train = FixedProblems.get2ClassLinear(400, new Random());
+        
+        for(PassiveAggressive.Mode mode : PassiveAggressive.Mode.values())
+        {
+            PassiveAggressive pa = new PassiveAggressive();
+            pa.setMode(mode);
+            pa.trainC(train);
+
+            ClassificationDataSet test = FixedProblems.get2ClassLinear(400, new Random());
+
+            for(DataPointPair<Integer> dpp : test.getAsDPPList())
+                assertEquals(dpp.getPair().longValue(), pa.classify(dpp.getDataPoint()).mostLikely());
+        }
+    }
+
+    /**
+     * Test of train method, of class PassiveAggressive.
+     */
+    @Test
+    public void testTrain_RegressionDataSet()
+    {
+        System.out.println("train");
+        Random rand = new Random(123);
+        
+        for(PassiveAggressive.Mode mode : PassiveAggressive.Mode.values())
+        {
+            PassiveAggressive pa = new PassiveAggressive();
+            pa.setMode(mode);
+            pa.setEps(0.00001);
+            pa.train(FixedProblems.getLinearRegression(400, rand));
+
+            for(DataPointPair<Double> dpp : FixedProblems.getLinearRegression(100, rand).getAsDPPList())
+            {
+                double truth = dpp.getPair();
+                double pred = pa.regress(dpp.getDataPoint());
+
+                double relErr = (truth-pred)/truth;
+                assertEquals(0.0, relErr, 0.1);//Give it a decent wiggle room b/c of regularization
+            }
+        }
+    }
+
+}
