cd30c3c02906ea2d8263136c8935c772608a4cae
Edward Raff <Raff.Edward@gmail.com>
2013-03-27 14:57:43 +0000

    Adding a new IntSet for a fixed sized range of values that supports fast constant accesses and iteration.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@659 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/utils/IntSetFixedSize.java
@@ -0,0 +1,175 @@
+package jsat.utils;
+
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+
+/**
+ * A set for integers that is of a fixed initial size, and can only accept 
+ * integers in the range [0, size). Insertions, removals, and checks are all 
+ * constant time with a fast iterator. <br>
+ * Null values are not supported
+ * 
+ * @author Edward Raff
+ */
+public class IntSetFixedSize extends AbstractSet<Integer> implements Serializable
+{
+    private static final int STOP = -1;
+    private int nnz = 0;
+    private int first = -1;
+    private boolean[] has;
+    //Use as a linked list
+    private int[] prev;
+    private int[] next;
+    
+    /**
+     * Creates a new fixed size int set
+     * @param size the size of the int set
+     */
+    public IntSetFixedSize(int size)
+    {
+        has = new boolean[size];
+        prev = new int[size];
+        next = new int[size];
+        first = STOP;
+    }
+
+    @Override
+    public boolean add(Integer e)
+    {
+        return add(e.intValue());
+    }
+    
+    /**
+     * Adds a new integer into the set
+     * @param e the value to add into the set
+     * @return {@code true} if the operation modified the set, {@code false} 
+     * otherwise. 
+     */
+    public boolean add(int e)
+    {
+        if(e < 0 || e >= has.length)
+            throw new IllegalArgumentException("Input must be in range [0, " + has.length + ") not " + e);
+        else if(contains(e) )
+            return false;
+        else
+        {
+            if (nnz == 0)
+            {
+                first = e;
+                next[e] = prev[e] = STOP;
+            }
+            else
+            {
+                prev[first] = e;
+                next[e] = first;
+                prev[e] = STOP;
+                first = e;
+            }
+            nnz++;
+            return has[e] = true;
+        }
+    }
+
+    @Override
+    public boolean remove(Object o)
+    {
+        if(o instanceof Integer)
+            return remove_int((Integer)o);
+        return super.remove(o);
+    }
+    
+    /**
+     * Removes the specified integer from the set
+     * @param val the value to remove 
+     * @return {@code true} if the set was modified by this operation, 
+     * {@code false} if it was not.
+     */
+    public boolean remove(int val)
+    {
+        return remove_int(val);
+    }
+
+    @Override
+    public boolean contains(Object o)
+    {
+        if(o instanceof Integer)
+        {
+            int val = (Integer)o;
+            return contains(val);
+        }
+        else
+            return false;
+    }
+    
+    /**
+     * Checks if the given value is contained in this set
+     * @param val the value to check for 
+     * @return {@code true} if the value is in the set, {@code false} otherwise.
+     */
+    public boolean contains(int val)
+    {
+        if(val < 0 || val >= has.length)
+            return false;
+        return has[val];
+    }
+    
+    
+    private boolean remove_int(int index)
+    {
+        if (contains(index))
+        {
+            if (first == index)
+                first = next[index];
+            else
+                next[prev[index]] = next[index];
+            
+            if (next[index] != STOP)
+                prev[next[index]] = prev[index];
+            next[index] = STOP;
+            has[index] = false;
+            nnz--;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+
+    @Override
+    public Iterator<Integer> iterator()
+    {
+        final Iterator<Integer> iterator = new Iterator<Integer>() 
+        {
+            int prev = STOP;
+            int runner = first;
+            @Override
+            public boolean hasNext()
+            {
+                return runner != STOP;
+            }
+
+            @Override
+            public Integer next()
+            {
+                prev = runner;
+                runner = next[runner];
+                return prev;
+            }
+
+            @Override
+            public void remove()
+            {
+                remove_int(prev);
+            }
+        };
+        return iterator;
+    }
+
+    @Override
+    public int size()
+    {
+        return nnz;
+    }
+    
+}
---
+++ JSAT/test/jsat/utils/IntSetFixedSizeTest.java
@@ -0,0 +1,200 @@
+
+package jsat.utils;
+
+import java.util.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public class IntSetFixedSizeTest
+{
+    
+    public IntSetFixedSizeTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of add method, of class IntSetFixedSize.
+     */
+    @Test
+    public void testAdd_Integer()
+    {
+        System.out.println("add");
+        IntSetFixedSize set = new IntSetFixedSize(5);
+        assertTrue(set.add(new Integer(1)));
+        assertTrue(set.add(new Integer(2)));
+        assertFalse(set.add( new Integer(1) ));
+        assertTrue(set.add( new Integer(3) ));
+
+        for (int badVal : Arrays.asList(-1, 5, 10, -23))
+            try
+            {
+                set.add( new Integer(badVal));
+                fail("Should not have been added");
+            }
+            catch (Exception ex)
+            {
+            
+            }
+    }
+
+    /**
+     * Test of add method, of class IntSetFixedSize.
+     */
+    @Test
+    public void testAdd_int()
+    {
+        System.out.println("add");
+        IntSetFixedSize set = new IntSetFixedSize(5);
+        assertTrue(set.add(1));
+        assertTrue(set.add(2));
+        assertFalse(set.add(1));
+        assertTrue(set.add(3));
+
+        for (int badVal : Arrays.asList(-1, 5, 10, -23))
+            try
+            {
+                set.add(badVal);
+                fail("Should not have been added");
+            }
+            catch (Exception ex)
+            {
+            }
+    }
+
+    /**
+     * Test of contains method, of class IntSetFixedSize.
+     */
+    @Test
+    public void testContains_Object()
+    {
+        System.out.println("contains");
+        IntSetFixedSize instance = new IntSetFixedSize(100);
+        List<Integer> intList = new ArrayList<>();
+        ListUtils.addRange(intList, 0, 100, 1);
+        Collections.shuffle(intList);
+        
+        instance.addAll(intList.subList(0, 50));
+        for(int i : intList.subList(0, 50))
+            assertTrue(instance.contains(i));
+        for(int i : intList.subList(50, 100))
+            assertFalse(instance.contains(i));
+        
+    }
+
+    /**
+     * Test of contains method, of class IntSetFixedSize.
+     */
+    @Test
+    public void testContains_int()
+    {
+        System.out.println("contains");
+        IntSetFixedSize instance = new IntSetFixedSize(100);
+        List<Integer> intList = new ArrayList<>();
+        ListUtils.addRange(intList, 0, 100, 1);
+        Collections.shuffle(intList);
+        
+        instance.addAll(intList.subList(0, 50));
+        for(int i : intList.subList(0, 50))
+            assertTrue(instance.contains(i));
+        for(int i : intList.subList(50, 100))
+            assertFalse(instance.contains(i));
+    }
+
+    /**
+     * Test of iterator method, of class IntSetFixedSize.
+     */
+    @Test
+    public void testIterator()
+    {
+        System.out.println("iterator");
+        IntSetFixedSize set = new IntSetFixedSize(10);
+        set.add(5);
+        set.add(3);
+        set.add(4);
+        set.add(1);
+        set.add(2);
+        Set<Integer> copySet = new HashSet<>(set);
+        int prev = Integer.MIN_VALUE;
+        Iterator<Integer> iter = set.iterator();
+        int count = 0;
+        while(iter.hasNext())
+        {
+            int val = iter.next();
+            copySet.remove(val);
+            count++;
+        }
+        assertEquals(5, set.size());
+        assertEquals(5, count);
+        assertEquals(0, copySet.size());
+        
+        //Test removing some elements
+        iter = set.iterator();
+        while(iter.hasNext())
+        {
+            int val = iter.next();
+            if(val == 2 || val == 4)
+                iter.remove();
+        }
+        assertEquals(3, set.size());
+        
+        //Make sure the corect values were actually removed
+        iter = set.iterator();
+        count = 0;
+        while(iter.hasNext())
+        {
+            int val = iter.next();
+            assertFalse(val == 2 || val == 4);
+            count++;
+        }
+        assertEquals(3, set.size());
+        assertEquals(3, count);
+    }
+
+    /**
+     * Test of size method, of class IntSetFixedSize.
+     */
+    @Test
+    public void testSize()
+    {
+        System.out.println("size");
+        IntSetFixedSize set = new IntSetFixedSize(10);
+        assertEquals(0, set.size());
+        set.add(1);
+        assertEquals(1, set.size());
+        set.add(1);
+        set.add(2);
+        assertEquals(2, set.size());
+        set.add(5);
+        set.add(7);
+        set.add(2);
+        assertEquals(4, set.size());
+    }
+}
