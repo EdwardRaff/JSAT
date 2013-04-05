bbdfbf234f97062ce2895d6bbcf8b4aaed01942f
Edward Raff <Raff.Edward@gmail.com>
2013-04-05 02:47:34 +0000

    Implements the Pearson Correlation Distance metric
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@672 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/linear/distancemetrics/PearsonDistance.java
@@ -0,0 +1,243 @@
+
+package jsat.linear.distancemetrics;
+
+import java.util.Iterator;
+import jsat.linear.IndexValue;
+import jsat.linear.Vec;
+
+/**
+ * A valid distance metric formed from the Pearson Correlation between two vectors.
+ * The distance in the range of [0, 1]. 
+ * 
+ * @author Edward Raff
+ */
+public class PearsonDistance implements DistanceMetric
+{
+    private boolean bothNonZero;
+    private boolean absoluteDistance;
+
+    /**
+     * Creates a new standard Pearson Distance that does not ignore zero values 
+     * and anti-correlated values are considered far away. 
+     */
+    public PearsonDistance()
+    {
+        this(false, false);
+    }
+
+    /**
+     * Creates a new Pearson Distance object
+     * @param bothNonZero {@code true} if non zero values should be treated as 
+     * "missing" or "no vote", and will not contribute. But this will not 
+     * change the mean value used. {@code false} produces the standard Pearson value. 
+     * @param absoluteDistance {@code true} to use the absolute correlation, meaning 
+     * correlated and anti-correlated values will have the same distance. 
+     */
+    public PearsonDistance(boolean bothNonZero, boolean absoluteDistance)
+    {
+        this.bothNonZero = bothNonZero;
+        this.absoluteDistance = absoluteDistance;
+    }
+
+    @Override
+    public double dist(Vec a, Vec b)
+    {
+        double r = correlation(a, b, bothNonZero);
+        if(Double.isNaN(r))
+            return Double.MAX_VALUE;
+        if(absoluteDistance)
+            return Math.sqrt(1-r*r);
+        else
+            return Math.sqrt((1-r)*0.5);
+    }
+
+    @Override
+    public boolean isSymmetric()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isSubadditive()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isIndiscemible()
+    {
+        return true;
+    }
+
+    @Override
+    public double metricBound()
+    {
+        return 1;
+    }
+
+    @Override
+    public PearsonDistance clone()
+    {
+        return new PearsonDistance(bothNonZero, absoluteDistance);
+    }
+    
+    /**
+     * Computes the Pearson correlation between two vectors. If one of the vectors is all zeros, the result is undefined. 
+     * In cases where both are zero vectors, 1 will be returned to indicate they are the same. In cases where one of the 
+     * numerator coefficients is zero, its value will be bumped up to an epsilon to provide a near result. 
+     * @param a the first vector
+     * @param b the second vector
+     * @param bothNonZero {@code false} is the normal Pearson correlation. {@code true} will make the computation ignore 
+     * all indexes where one of the values is zero, but it will not change the mean. 
+     * @return the Pearson correlation in [-1, 1]
+     */
+    public static double correlation(Vec a, Vec b, boolean bothNonZero)
+    {
+        final double aMean = a.mean();
+        final double bMean = b.mean();
+
+        double r = 0;
+        double aSqrd = 0, bSqrd = 0;
+
+        if (a.isSparse() || b.isSparse())
+        {
+            Iterator<IndexValue> aIter = a.getNonZeroIterator();
+            Iterator<IndexValue> bIter = b.getNonZeroIterator();
+
+            //if one is empty, then a zero forms on the denomrinator
+            if (!aIter.hasNext() && !bIter.hasNext())
+                return 1;
+            if (!aIter.hasNext() || !bIter.hasNext())
+                return Double.MAX_VALUE;
+
+            IndexValue aCur = null;
+            IndexValue bCur = null;
+
+            boolean newA = true, newB = true;
+            int lastObservedIndex = -1;
+            do
+            {
+
+                if (newA)
+                {
+                    if (!aIter.hasNext())
+                        break;
+                    aCur = aIter.next();
+                    newA = false;
+                }
+                if (newB)
+                {
+                    if (!bIter.hasNext())
+                        break;
+                    bCur = bIter.next();
+                    newB = false;
+                }
+
+                if (aCur.getIndex() == bCur.getIndex())
+                {
+                    //accumulate skipped positions where both are zero
+                    if(!bothNonZero)
+                        r += aMean * bMean * (aCur.getIndex()-lastObservedIndex - 1);
+                    lastObservedIndex = aCur.getIndex();
+
+                    double aVal = aCur.getValue() - aMean;
+                    double bVal = bCur.getValue() - bMean;
+                    r += aVal * bVal;
+
+                    aSqrd += aVal * aVal;
+                    bSqrd += bVal * bVal;
+
+                    newA = newB = true;
+                }
+                else if (aCur.getIndex() > bCur.getIndex())
+                {
+                    if (!bothNonZero)
+                    {
+                        //accumulate skipped positions where both are zero
+                        r += aMean * bMean * (bCur.getIndex()-lastObservedIndex - 1);
+                        lastObservedIndex = bCur.getIndex();
+
+                        double bVal = bCur.getValue() - bMean;
+                        r += -aMean * bVal;
+                        bSqrd += bVal * bVal;
+                    }
+                    newB = true;
+                }
+                else if (aCur.getIndex() < bCur.getIndex())
+                {
+                    if (!bothNonZero)
+                    {
+                        //accumulate skipped positions where both are zero
+                        r += aMean * bMean * (aCur.getIndex()-lastObservedIndex - 1);
+                        lastObservedIndex = aCur.getIndex();
+                    
+                        double aVal = aCur.getValue() - aMean;
+                        r += aVal * -bMean;
+                        aSqrd += aVal * aVal;
+                    }
+                    newA = true;
+                }
+            }
+            while (true);
+
+            if (!bothNonZero)
+            {
+                //only one of the loops bellow will execute
+                while (!newA || (newA && aIter.hasNext()))
+                {
+                    if(newA)
+                        aCur = aIter.next();
+                    //accumulate skipped positions where both are zero
+                    r += aMean * bMean * (aCur.getIndex()-lastObservedIndex - 1);
+                    lastObservedIndex = aCur.getIndex();
+
+                    double aVal = aCur.getValue() - aMean;
+                    r += aVal * -bMean;
+                    aSqrd += aVal * aVal;
+                    newA = true;
+                }
+
+                while (!newB || (newB && bIter.hasNext()))
+                {
+                    if(newB)
+                        bCur = bIter.next();
+                    //accumulate skipped positions where both are zero
+                    r += aMean * bMean * (bCur.getIndex()-lastObservedIndex - 1);
+                    lastObservedIndex = bCur.getIndex();
+
+                    double bVal = bCur.getValue() - bMean;
+                    r += -aMean * bVal;
+                    bSqrd += bVal * bVal;
+                    newB = true;
+                }
+
+                r += aMean * bMean * (a.length()-lastObservedIndex - 1);
+                aSqrd += aMean * aMean * (a.length()-a.nnz());
+                bSqrd += bMean * bMean * (b.length()-b.nnz());
+            }
+        }
+        else//dense!
+        {
+            for(int i = 0; i < a.length(); i++)
+            {
+                double aTmp = a.get(i);
+                double bTmp = b.get(i);
+                if(bothNonZero && (aTmp == 0 || bTmp == 0))
+                    continue;
+                double aVal = aTmp-aMean;
+                double bVal = bTmp-bMean;
+                r += aVal*bVal;
+                aSqrd += aVal*aVal;
+                bSqrd += bVal*bVal;
+            }
+        }
+        
+        if(bSqrd == 0 && aSqrd == 0)
+            return Double.MAX_VALUE;
+        else if(bSqrd == 0 || aSqrd == 0)
+            return r/Math.sqrt((aSqrd+1e-10)*(bSqrd+1e-10));
+
+        return r/Math.sqrt(aSqrd*bSqrd);
+    }
+
+}
---
+++ JSAT/test/jsat/linear/distancemetrics/PearsonDistanceTest.java
@@ -0,0 +1,154 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsat.linear.distancemetrics;
+
+import jsat.linear.DenseVector;
+import jsat.linear.SparseVector;
+import jsat.linear.Vec;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public class PearsonDistanceTest
+{
+    Vec x1, x2, x3, x4, x5;
+    
+    public PearsonDistanceTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+        x1 = new DenseVector(new double[]{43, 21, 25, 42, 57, 59});
+        x2 = new DenseVector(new double[]{99, 65, 79, 75, 87, 81});
+        x3 = new DenseVector(new double[]{ 0, 12,  0, 38, 19,  0});
+        x4 = new DenseVector(new double[]{ 0, 60, 27,  0, 13,  9});
+        x5 = new DenseVector(new double[]{ 0,  0, 25, 42,  0,  0});
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of correlation method, of class PearsonDistance.
+     */
+    @Test
+    public void testCorrelation()
+    {
+        System.out.println("correlation");
+        
+        Vec[][] pairs = new Vec[][]
+        {
+            {x1, x2},
+            {x2, x1},
+            {x2, x2},
+            {x3, x4},
+            {x4, x3},
+            {x1, x5},
+            {x3, x5},
+            {x4, x5},
+        };
+        
+        double[] expectedVals = new double[]
+        {
+            0.5298089018901744,
+            0.5298089018901744,
+            1.0, 
+            -0.16532953228838151,
+            -0.16532953228838151,
+            -0.2587220950032312,
+            0.5995147431422027,
+            -0.2625496015918526,
+        };
+        
+        for(int i = 0; i < pairs.length; i++)
+        {
+            double result = PearsonDistance.correlation(pairs[i][0], pairs[i][1], false);
+            double expected = expectedVals[i];
+            assertEquals(expected, result, 1e-14);
+            result = PearsonDistance.correlation(pairs[i][1], pairs[i][0], false);
+            assertEquals(expected, result, 1e-14);
+        }
+        //on sparse inputs
+        for(int i = 0; i < pairs.length; i++)
+        {
+            double result = PearsonDistance.correlation(new SparseVector(pairs[i][0]), new SparseVector(pairs[i][1]), false);
+            double expected = expectedVals[i];
+            assertEquals(expected, result, 1e-14);
+            result = PearsonDistance.correlation(new SparseVector(pairs[i][1]), new SparseVector(pairs[i][0]), false);
+            assertEquals(expected, result, 1e-14);
+        }
+                
+    }
+    
+    @Test
+    public void testCorrelationBNZ()
+    {
+        System.out.println("correlation");
+        
+        Vec[][] pairs = new Vec[][]
+        {
+            {x1, x2},
+            {x2, x1},
+            {x2, x2},
+            {x3, x4},
+            {x4, x3},
+            {x1, x5},
+            {x3, x5},
+            {x4, x5},
+        };
+        
+        double[] expectedVals = new double[]
+        {
+            0.5298089018901744,
+            0.5298089018901744,
+            1.0, 
+            -0.0562857602218547,
+            -0.0562857602218547,
+            -0.36182870371730796,
+            1.0,
+            1.0,
+        };
+        
+        for(int i = 0; i < pairs.length; i++)
+        {
+            double result = PearsonDistance.correlation(pairs[i][0], pairs[i][1], true);
+            double expected = expectedVals[i];
+            assertEquals(expected, result, 1e-14);
+            result = PearsonDistance.correlation(pairs[i][1], pairs[i][0], true);
+            assertEquals(expected, result, 1e-14);
+        }
+        //on sparse inputs
+        for(int i = 0; i < pairs.length; i++)
+        {
+            double result = PearsonDistance.correlation(new SparseVector(pairs[i][0]), new SparseVector(pairs[i][1]), true);
+            double expected = expectedVals[i];
+            assertEquals(expected, result, 1e-14);
+            result = PearsonDistance.correlation(new SparseVector(pairs[i][1]), new SparseVector(pairs[i][0]), true);
+            assertEquals(expected, result, 1e-14);
+        }
+                
+    }
+}
