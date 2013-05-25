2216d86fa9622190b63b32b04d0490a73ba6e066
Edward Raff <Raff.Edward@gmail.com>
2013-05-25 22:27:07 +0000

    Adding two new Boosting implementations.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@701 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/classifiers/boosting/EmphasisBoost.java
@@ -0,0 +1,280 @@
+package jsat.classifiers.boosting;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import jsat.classifiers.*;
+import jsat.parameters.Parameter;
+import jsat.parameters.Parameterized;
+import jsat.utils.DoubleList;
+import jsat.utils.FakeExecutor;
+
+/**
+ * Emphasis Boost is a generalization of the Real AdaBoost algorithm, expanding 
+ * the update term and providing the {@link #setLambda(double) &lambda; } term 
+ * to control the trade off. With &lambda; = 1/2, it becomes equivalent to Real 
+ * AdaBoost. If the weak learner does not support confidence outputs (non-hard 
+ * decisions), then it further becomes equivalent to Discrete Ada Boost. <br>
+ * Emphasis Boost only supports binary classification problems, the learner used
+ * should support weighted predictions. 
+ * <br><br>
+ * NOTE: In the face of extreme outliers, it is possible for numerical 
+ * instability to occur. This implementation attempts to reset weights when 
+ * numerical issues occur. 
+ * <br><br>
+ * See: <br>
+ * Gómez-Verdejo, V., Ortega-Moral, M., Arenas-García, J., & Figueiras-Vidal, 
+ * A. R. (2006). <i>Boosting by weighting critical and erroneous samples</i>.
+ * Neurocomputing, 69(7-9), 679–685. doi:10.1016/j.neucom.2005.12.011
+ * 
+ * @author Edward Raff
+ */
+public class EmphasisBoost implements Classifier, Parameterized
+{
+    private Classifier weakLearner;
+    private int maxIterations;
+    /**
+     * The list of weak hypothesis
+     */
+    protected List<Classifier> hypoths;
+    /**
+     * The weights for each weak learner
+     */
+    protected List<Double> hypWeights;
+    protected CategoricalData predicting;
+    private double lambda;
+
+    /**
+     * Creates a new EmphasisBoost learner
+     * @param weakLearner the weak learner to use
+     * @param maxIterations the maximum number of boosting iterations
+     * @param lambda the trade off parameter in [0, 1]
+     */
+    public EmphasisBoost(Classifier weakLearner, int maxIterations, double lambda)
+    {
+        setWeakLearner(weakLearner);
+        setMaxIterations(maxIterations);
+        setLambda(lambda);
+    }
+
+    /**
+     * Copy constructor
+     * @param toClone the object to clone
+     */
+    protected EmphasisBoost(EmphasisBoost toClone)
+    {
+        this(toClone.weakLearner.clone(), toClone.maxIterations, toClone.lambda);
+        if(toClone.hypWeights != null)
+        {
+            this.hypWeights = new DoubleList(toClone.hypWeights);
+            this.hypoths = new ArrayList<Classifier>(toClone.maxIterations);
+            for(Classifier weak : toClone.hypoths)
+                this.hypoths.add(weak.clone());
+            this.predicting = toClone.predicting.clone();
+        }
+    }
+    
+    /**
+     * Returns the maximum number of iterations used
+     * @return the maximum number of iterations used
+     */
+    public int getMaxIterations()
+    {
+        return maxIterations;
+    }
+
+    /**
+     * Sets the maximal number of boosting iterations that may be performed 
+     * @param maxIterations the maximum number of iterations
+     */
+    public void setMaxIterations(int maxIterations)
+    {
+        if(maxIterations < 1)
+            throw new IllegalArgumentException("Iterations must be positive, not " + maxIterations);
+        this.maxIterations = maxIterations;
+    }
+
+    /**
+     * Returns the weak learner currently being used by this method. 
+     * @return the weak learner currently being used by this method. 
+     */
+    public Classifier getWeakLearner()
+    {
+        return weakLearner;
+    }
+
+    /**
+     * Sets the weak learner used during training. 
+     * @param weakLearner the weak learner to use
+     */
+    public void setWeakLearner(Classifier weakLearner)
+    {
+        if(!weakLearner.supportsWeightedData())
+            throw new IllegalArgumentException("WeakLearner must support weighted data to be boosted");
+        this.weakLearner = weakLearner;
+    }
+
+    /**
+     * &lambda; controls the trade off between weighting the errors based on 
+     * their distance to the margin and the quadratic error of the output. The 
+     * three extreme values are: <br>
+     * <ul>
+     * <li> &lambda; = 0 , in this case all the weight is placed on points based
+     * on their distance to the margin of the classification boundary. </li>
+     * <li>&lambda; = 1/2, in this case weight is balanced between the margin 
+     * distance and the quadratic error. This is equivalent to Real Ada Boost
+     * </li>
+     * <li>&lambda; = 1, in this case the weight is placed purely based on the 
+     * quadratic error of the output</li>
+     * </ul>
+     * <br><br> According to the original paper, values in the range [0.3, 0.4] 
+     * often perform well. 
+     * 
+     * @param lambda the trade off parameter in [0, 1]
+     */
+    public void setLambda(double lambda)
+    {
+        this.lambda = lambda;
+    }
+
+    /**
+     * Returns the value of the &lambda; trade off parameter
+     * @return the value of the &lambda; trade off parameter 
+     */
+    public double getLambda()
+    {
+        return lambda;
+    }
+
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        if(predicting == null)
+            throw new RuntimeException("Classifier has not been trained yet");
+        
+        CategoricalResults cr = new CategoricalResults(predicting.getNumOfCategories());
+        
+        double score = 0;
+        for(int i = 0; i < hypoths.size(); i++)
+            score += H(hypoths.get(i), data)*hypWeights.get(i);
+        
+        if(score < 0)
+            cr.setProb(0, 1.0);
+        else
+            cr.setProb(1, 1.0);
+        return cr;
+    }
+    
+    private double H(Classifier weak, DataPoint dp )
+    {
+        CategoricalResults catResult = weak.classify(dp);
+        
+        return catResult.getProb(1)*2-1;
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet, ExecutorService threadPool)
+    {
+        predicting = dataSet.getPredicting();
+        hypWeights = new DoubleList(maxIterations);
+        hypoths = new ArrayList<Classifier>(maxIterations);
+        final int N = dataSet.getSampleSize();
+        
+        List<DataPointPair<Integer>> dataPoints = dataSet.getTwiceShallowClone().getAsDPPList();
+        //Initialization step, set up the weights  so they are all 1 / size of dataset
+        for(DataPointPair<Integer> dpp : dataPoints)
+            dpp.getDataPoint().setWeight(1.0/N);//Scaled, they are all 1 
+        double weightSum = 1;
+        
+        
+        //Keep track of the cumaltive score for everything
+        double[] H_cur = new double[N];
+        double[] curH_Result = new double[N];
+        
+        for(int t = 0; t < maxIterations; t++)
+        {
+            Classifier weak = weakLearner.clone();
+            if(threadPool != null && !(threadPool instanceof FakeExecutor))
+                weak.trainC(new ClassificationDataSet(dataPoints, predicting), threadPool);
+            else
+                weak.trainC(new ClassificationDataSet(dataPoints, predicting));
+
+            double error = 0.0;
+            for(int i = 0; i < dataPoints.size(); i++)
+            {
+                DataPointPair<Integer> dpp = dataPoints.get(i);
+                double y_hat = H_cur[i] = H(weak, dpp.getDataPoint());
+                double y_true = dpp.getPair()*2-1;//{-1 or 1}
+                error += dpp.getDataPoint().getWeight()*y_hat*y_true;
+            }
+            
+            
+            if(error < 0)
+                return;
+            
+            double alpha_m = Math.log((1+error)/(1-error))/2;
+            
+            weightSum = 0;
+            
+            for(int i = 0; i < dataPoints.size(); i++)
+            {
+                curH_Result[i] += alpha_m * H_cur[i];
+                double f_t = curH_Result[i];
+                
+                DataPointPair<Integer> dpp = dataPoints.get(i);
+                DataPoint dp = dpp.getDataPoint();
+                double y_true = dpp.getPair()*2-1;
+
+                double w_i = Math.exp(lambda*Math.pow(f_t-y_true, 2) - (1-lambda)*f_t*f_t);
+                if(Double.isInfinite(w_i))
+                    w_i = 50;//Let it grow back isntead of bizaro huge values
+                weightSum += w_i;
+                dp.setWeight(w_i);
+            }
+            
+            for(int i = 0; i < dataPoints.size(); i++)
+            {
+                DataPointPair<Integer> dpp = dataPoints.get(i);
+                DataPoint dp = dpp.getDataPoint();
+                dp.setWeight(dp.getWeight()/weightSum);
+            }
+            
+            hypoths.add(weak);
+            hypWeights.add(alpha_m);
+        }
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet)
+    {
+        trainC(dataSet, null);
+    }
+
+    @Override
+    public boolean supportsWeightedData()
+    {
+        return false;
+    }
+
+    @Override
+    public Classifier clone()
+    {
+        return new EmphasisBoost(this);
+    }
+    
+    private List<Parameter> params = Parameter.getParamsFromMethods(this);
+    private Map<String, Parameter> paramMap = Parameter.toParameterMap(params);
+
+    @Override
+    public List<Parameter> getParameters()
+    {
+        return params;
+    }
+
+    @Override
+    public Parameter getParameter(String paramName)
+    {
+        return paramMap.get(paramName);
+    }
+}
---
+++ JSAT/src/jsat/classifiers/boosting/ModestAdaBoost.java
@@ -0,0 +1,249 @@
+package jsat.classifiers.boosting;
+
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import jsat.classifiers.*;
+import jsat.parameters.Parameter;
+import jsat.parameters.Parameterized;
+import jsat.utils.DoubleList;
+import jsat.utils.FakeExecutor;
+
+/**
+ * Modest Ada Boost is a generalization of Discrete Ada Boost that attempts to 
+ * reduce the generalization error and avoid over-fitting. Empirically, 
+ * ModestBoost usually maintains a higher training-set error, and may take more
+ * iterations to obtain the same test set error as other algorithms, but doesn't
+ * not increase as much after it reaches the minimum error - which should make 
+ * it easier to obtain the higher accuracy.
+ * <br>
+ * See: <br>
+ * Vezhnevets, A., & Vezhnevets, V. (2005). <i>“Modest AdaBoost” – Teaching 
+ * AdaBoost to Generalize Better</i>. GraphiCon. Novosibirsk Akademgorodok, 
+ * Russia. Retrieved from 
+ * <a href="http://www.inf.ethz.ch/personal/vezhneva/Pubs/ModestAdaBoost.pdf">
+ * here</a>
+ * 
+ * @author Edward Raff
+ */
+public class ModestAdaBoost  implements Classifier, Parameterized
+{
+    private Classifier weakLearner;
+    private int maxIterations;
+    /**
+     * The list of weak hypothesis
+     */
+    protected List<Classifier> hypoths;
+    /**
+     * The weights for each weak learner
+     */
+    protected List<Double> hypWeights;
+    protected CategoricalData predicting;
+
+    /**
+     * Creates a new ModestBoost learner
+     * @param weakLearner the weak learner to use
+     * @param maxIterations the maximum number of boosting iterations
+     */
+    public ModestAdaBoost(Classifier weakLearner, int maxIterations)
+    {
+        setWeakLearner(weakLearner);
+        setMaxIterations(maxIterations);
+    }
+    
+    /**
+     * Copy constructor
+     * @param toClone the object to clone
+     */
+    protected ModestAdaBoost(ModestAdaBoost toClone)
+    {
+        this(toClone.weakLearner.clone(), toClone.maxIterations);
+        if(toClone.hypWeights != null)
+        {
+            this.hypWeights = new DoubleList(toClone.hypWeights);
+            this.hypoths = new ArrayList<Classifier>(toClone.maxIterations);
+            for(Classifier weak : toClone.hypoths)
+                this.hypoths.add(weak.clone());
+            this.predicting = toClone.predicting.clone();
+        }
+    }
+    
+    /**
+     * Returns the maximum number of iterations used
+     * @return the maximum number of iterations used
+     */
+    public int getMaxIterations()
+    {
+        return maxIterations;
+    }
+
+    /**
+     * Sets the maximal number of boosting iterations that may be performed 
+     * @param maxIterations the maximum number of iterations
+     */
+    public void setMaxIterations(int maxIterations)
+    {
+        if(maxIterations < 1)
+            throw new IllegalArgumentException("Iterations must be positive, not " + maxIterations);
+        this.maxIterations = maxIterations;
+    }
+
+    /**
+     * Returns the weak learner currently being used by this method. 
+     * @return the weak learner currently being used by this method. 
+     */
+    public Classifier getWeakLearner()
+    {
+        return weakLearner;
+    }
+
+    /**
+     * Sets the weak learner used during training. 
+     * @param weakLearner the weak learner to use
+     */
+    public void setWeakLearner(Classifier weakLearner)
+    {
+        if(!weakLearner.supportsWeightedData())
+            throw new IllegalArgumentException("WeakLearner must support weighted data to be boosted");
+        this.weakLearner = weakLearner;
+    }
+
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        if(predicting == null)
+            throw new RuntimeException("Classifier has not been trained yet");
+        
+        CategoricalResults cr = new CategoricalResults(predicting.getNumOfCategories());
+        
+        double score = 0;
+        for(int i = 0; i < hypoths.size(); i++)
+            score += (hypoths.get(i).classify(data).getProb(1)*2-1)*hypWeights.get(i);
+        
+        if(score < 0)
+            cr.setProb(0, 1.0);
+        else
+            cr.setProb(1, 1.0);
+        return cr;
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet, ExecutorService threadPool)
+    {
+        predicting = dataSet.getPredicting();
+        hypWeights = new DoubleList(maxIterations);
+        hypoths = new ArrayList<Classifier>(maxIterations);
+        final int N = dataSet.getSampleSize();
+        
+        double[] D_inv = new double[N];
+        double[] D = new double[N];
+        
+        List<DataPointPair<Integer>> dataPoints = dataSet.getTwiceShallowClone().getAsDPPList();
+        Arrays.fill(D, 1.0/N);
+        for(DataPointPair<Integer> dpp : dataPoints)
+            dpp.getDataPoint().setWeight(D[0]);//Scaled, they are all 1 
+        double weightSum = 1;
+        
+        double[] H_cur = new double[N];
+        
+        for(int t = 0; t < maxIterations; t++)
+        {
+            Classifier weak = weakLearner.clone();
+            if(threadPool == null || threadPool instanceof FakeExecutor)
+                weak.trainC(new ClassificationDataSet(dataPoints, predicting));
+            else
+                weak.trainC(new ClassificationDataSet(dataPoints, predicting), threadPool);
+            
+            double invSum = 0;
+            for(int i = 0; i < N; i++)
+                invSum += (D_inv[i] = 1-D[i]);
+            
+            for(int i = 0; i < N; i++)
+                D_inv[i] /= invSum;
+            double p_d = 0, p_id = 0, n_d = 0, n_id = 0;
+            
+            for(int i = 0; i < N; i++)
+            {
+                DataPointPair<Integer> dpp = dataPoints.get(i);
+                
+                H_cur[i] = (weak.classify(dpp.getDataPoint()).getProb(1)*2-1);
+                double outPut = Math.signum(H_cur[i]);
+                int c = dpp.getPair();
+                if(c == 1)//positive example case
+                {
+                    p_d  += outPut * D[i];
+                    p_id += outPut * D_inv[i];
+                }
+                else
+                {
+                    n_d  += outPut * D[i];
+                    n_id += outPut * D_inv[i];
+                }
+                
+            }
+            
+            double alpha_m = p_d * (1 - p_id) - n_d * (1 - n_id); 
+            
+            if(Math.signum(alpha_m) != Math.signum(p_d-n_d) || Math.abs((p_d - n_d)) < 1e-6 || alpha_m <= 0)
+                return;
+            
+            weightSum = 0;
+            for(int i = 0; i < N; i++)
+            {
+                DataPoint dp = dataPoints.get(i).getDataPoint();
+                double w_i = dp.getWeight();
+                int y_i = dataPoints.get(i).getPair()*2-1;
+                w_i *= Math.exp(-y_i*alpha_m*H_cur[i]);
+                if(Double.isInfinite(w_i))
+                    w_i = 1;//Let it grow back
+                else if(w_i <= 0)
+                    w_i = 1e-3/N;//Dont let it go quit to zero
+                weightSum += w_i;
+                dp.setWeight(w_i);
+            }
+            
+            for(int i = 0; i < N; i++)
+            {
+                DataPoint dp = dataPoints.get(i).getDataPoint();
+                double w_i = dp.getWeight();
+                dp.setWeight(Math.max(w_i/weightSum, 1e-10));//no zeros allowed
+            }
+            
+            hypWeights.add(alpha_m);
+            hypoths.add(weak);
+        }
+    }
+
+    @Override
+    public void trainC(ClassificationDataSet dataSet)
+    {
+        trainC(dataSet, null);
+    }
+
+    @Override
+    public boolean supportsWeightedData()
+    {
+        return false;
+    }
+
+    @Override
+    public Classifier clone()
+    {
+        return new ModestAdaBoost(this);
+    }
+
+    private List<Parameter> params = Parameter.getParamsFromMethods(this);
+    private Map<String, Parameter> paramMap = Parameter.toParameterMap(params);
+
+    @Override
+    public List<Parameter> getParameters()
+    {
+        return params;
+    }
+
+    @Override
+    public Parameter getParameter(String paramName)
+    {
+        return paramMap.get(paramName);
+    }
+    
+}
