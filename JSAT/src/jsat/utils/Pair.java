c276e9de4e814790174125b474c2f0c5950ed942
Edward Raff <Raff.Edward@gmail.com>
2013-05-29 23:14:28 +0000

    Forgot to include "Pair" object used in previous commit
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@705 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/utils/Pair.java
@@ -0,0 +1,37 @@
+package jsat.utils;
+
+/**
+ * A simple object to hold a pair of values
+ * @author Edward Raff
+ */
+public class Pair<X, Y>
+{
+    private X firstItem;
+    private Y secondItem;
+
+    public Pair(X firstItem, Y secondItem)
+    {
+        setFirstItem(firstItem);
+        setSecondItem(secondItem);
+    }
+
+    public void setFirstItem(X firstItem)
+    {
+        this.firstItem = firstItem;
+    }
+
+    public X getFirstItem()
+    {
+        return firstItem;
+    }
+
+    public void setSecondItem(Y secondItem)
+    {
+        this.secondItem = secondItem;
+    }
+
+    public Y getSecondItem()
+    {
+        return secondItem;
+    }
+}
