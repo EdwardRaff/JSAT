e5388c38f0462e00a42f76a315e5929efb755630
Edward Raff <Raff.Edward@gmail.com>
2013-07-21 05:30:37 +0000

    Adding L1 Sparse Truncated Gradient Descent. Includes test case.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@755 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/classifiers/linear/STGD.java
@@ -0,0 +1,294 @@
+package jsat.classifiers.linear;
+
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import jsat.classifiers.*;
+import jsat.classifiers.calibration.BinaryScoreClassifier;
+import jsat.exceptions.FailedToFitException;
+import jsat.linear.DenseVector;
+import jsat.linear.IndexValue;
+import jsat.linear.Vec;
+import jsat.parameters.Parameter;
+import jsat.parameters.Parameterized;
+import jsat.regression.BaseUpdateableRegressor;
+import jsat.regression.RegressionDataSet;
+import jsat.regression.UpdateableRegressor;
+
+/**
+ * This provides an implementation of Sparse Truncated Gradient Descent for 
+ * L<sub>1</sub> regularized linear classification and regression on sparse data
+ * sets. 
+ * <br><br>
+ * Unlike normal L<sub>1</sub> regression, regularization is controlled by the 
+ * {@link #setGravity(double) gravity} parameter, but other parameters 
+ * contribute to the level of sparsity. 
+ * <br><br>
+ * See: Langford, J., Li, L., & Zhang, T. (2009). <i>Sparse online learning via 
+ * truncated gradient</i>. The Journal of Machine Learning Research, 10, 
+ * 777–801. Retrieved from <a href="http://dl.acm.org/citation.cfm?id=1577097">
+ * here</a>
+ * @author Edward Raff
+ */
+public class STGD extends BaseUpdateableClassifier implements UpdateableRegressor, BinaryScoreClassifier, Parameterized
+{
+    private Vec w;
+    
+    private int K;
+    private double learningRate;
+    private double threshold;
+    private double gravity;
+    
+    private int time;
+    private int[] t;
+    
+    private List<Parameter> params = Parameter.getParamsFromMethods(this);
+    private Map<String, Parameter> paramMap = Parameter.toParameterMap(params);
+
+    /**
+     * Creates a new STGD learner
+     * @param K the regularization frequency
+     * @param learningRate the learning rate to use
+     * @param threshold the regularization threshold
+     * @param gravity the regularization parameter
+     */
+    public STGD(int K, double learningRate, double threshold, double gravity)
+    {
+        setK(K);
+        setLearningRate(learningRate);
+        setThreshold(threshold);
+        setGravity(gravity);
+    }
+
+    /**
+     * Copy constructor
+     * @param toCopy the object to copy
+     */
+    protected STGD(STGD toCopy)
+    {
+        if(toCopy.w != null)
+            this.w = toCopy.w.clone();
+        this.K = toCopy.K;
+        this.learningRate = toCopy.learningRate;
+        this.threshold = toCopy.threshold;
+        this.gravity = toCopy.gravity;
+        this.time = toCopy.time;
+        if(toCopy.t != null)
+            this.t = Arrays.copyOf(toCopy.t, toCopy.t.length);
+    }
+
+    /**
+     * Sets the frequency of applying the {@link #setGravity(double) gravity} 
+     * parameter to the weight vector. This value must be positive, and the 
+     * gravity will be applied every <i>K</i> updates.  Increasing this value 
+     * encourages greater sparsity. 
+     * 
+     * @param K the frequency to apply regularization in [1, Infinity )
+     */
+    public void setK(int K)
+    {
+        if(K < 1)
+            throw new IllegalArgumentException("K must be positive, not " + K);
+        this.K = K;
+    }
+
+    /**
+     * Returns the frequency of regularization application
+     * @return the frequency of regularization application
+     */
+    public int getK()
+    {
+        return K;
+    }
+    
+    /**
+     * Sets the learning rate to use
+     * @param learningRate the learning rate > 0. 
+     */
+    public void setLearningRate(double learningRate)
+    {
+        if(Double.isInfinite(learningRate) || Double.isNaN(learningRate) || learningRate <= 0)
+            throw new IllegalArgumentException("Learning rate must be positive, not " + learningRate);
+        this.learningRate = learningRate;
+    }
+
+    /**
+     * Returns the learning rate to use
+     * @return  the learning rate to use
+     */
+    public double getLearningRate()
+    {
+        return learningRate;
+    }
+
+    /**
+     * Sets the threshold for a coefficient value to avoid regularization. While
+     * a coefficient reaches this magnitude, regularization will not be applied. 
+     * @param threshold the coefficient regularization threshold in 
+     * ( 0, Infinity ]
+     */
+    public void setThreshold(double threshold)
+    {
+        if(Double.isNaN(threshold) || threshold <= 0)
+            throw new IllegalArgumentException("Threshold must be positive, not " + threshold);
+        this.threshold = threshold;
+    }
+
+    /**
+     * Returns the coefficient threshold parameter
+     * @return the coefficient threshold parameter
+     */
+    public double getThreshold()
+    {
+        return threshold;
+    }
+
+    /**
+     * Sets the gravity regularization parameter that "weighs down" the
+     * coefficient values. Larger gravity values impose stronger regularization,
+     * and encourage greater sparsity. 
+     * 
+     * @param gravity the regularization parameter in ( 0, Infinity )
+     */
+    public void setGravity(double gravity)
+    {
+        if(Double.isInfinite(gravity) || Double.isNaN(gravity) || gravity <= 0)
+            throw new IllegalArgumentException("Gravity must be positive, not " + gravity);
+        this.gravity = gravity;
+    }
+
+    /**
+     * Returns the regularization parameter
+     * @return the regularization parameter
+     */
+    public double getGravity()
+    {
+        return gravity;
+    }
+    
+    @Override
+    public STGD clone()
+    {
+        return new STGD(this);
+    }
+
+    @Override
+    public void setUp(CategoricalData[] categoricalAttributes, int numericAttributes, CategoricalData predicting)
+    {
+        if(predicting.getNumOfCategories() != 2)
+            throw new FailedToFitException("STGD supports only binary classification");
+        setUp(categoricalAttributes, numericAttributes);
+    }
+    
+    @Override
+    public void setUp(CategoricalData[] categoricalAttributes, int numericAttributes)
+    {
+        if(numericAttributes < 1)
+            throw new FailedToFitException("STGD requires numeric features");
+        w = new DenseVector(numericAttributes);
+        t = new int[numericAttributes];
+    }
+    
+    @Override
+    public void train(RegressionDataSet dataSet, ExecutorService threadPool)
+    {
+        train(dataSet);
+    }
+
+    @Override
+    public void train(RegressionDataSet dataSet)
+    {
+        BaseUpdateableRegressor.trainEpochs(dataSet, this, getEpochs());
+    }
+    
+    private static double T(double v_j, double a, double theta)
+    {
+        if(v_j >= 0 && v_j <= theta)
+            return Math.max(0, v_j-a);
+        else if(v_j <= 0 && v_j >= -theta)
+            return Math.min(0, v_j+a);
+        else
+            return v_j;
+    }
+
+    @Override
+    public void update(DataPoint dataPoint, int targetClass)
+    {
+        time++;
+        final Vec x = dataPoint.getNumericalValues();
+        final int y = targetClass*2-1;
+        final int yHat = (int) Math.signum(w.dot(x));
+        if(yHat == y)//Not part of the described algorithm (using signum), but needed
+            return;
+        performUpdate(x, y, yHat);
+    }
+    
+    @Override
+    public void update(DataPoint dataPoint, final double y)
+    {
+        time++;
+        final Vec x = dataPoint.getNumericalValues();
+        final double yHat = w.dot(x);
+        performUpdate(x, y, yHat);
+    }
+    
+    /**
+     * Performs the sparse update of the weight vector
+     * @param x the input vector
+     * @param y the true value
+     * @param yHat the predicted value
+     */
+    private void performUpdate(final Vec x, final double y, final double yHat)
+    {
+        for(IndexValue iv : x)
+        {
+            final int j = iv.getIndex();
+            w.set(j, 
+                    T(w.get(j)+2*learningRate*(y-yHat)*iv.getValue(), 
+                    ((time-t[j])/K)*gravity*learningRate,
+                    threshold));
+            
+            t[j] += ((time-t[j])/K)*K;
+        }
+    }
+    
+    @Override
+    public CategoricalResults classify(DataPoint data)
+    {
+        CategoricalResults cr = new CategoricalResults(2);
+        if(getScore(data) > 0)
+            cr.setProb(1, 1.0);
+        else
+            cr.setProb(0, 1.0);
+        return cr;
+    }
+    
+    @Override
+    public double regress(DataPoint data)
+    {
+        return getScore(data);
+    }
+
+    @Override
+    public boolean supportsWeightedData()
+    {
+        return false;
+    }
+
+    @Override
+    public double getScore(DataPoint dp)
+    {
+        return w.dot(dp.getNumericalValues());
+    }
+    
+    @Override
+    public List<Parameter> getParameters()
+    {
+        return Collections.unmodifiableList(params);
+    }
+
+    @Override
+    public Parameter getParameter(String paramName)
+    {
+        return paramMap.get(paramName);
+    }
+}
---
+++ JSAT/test/jsat/classifiers/linear/STGDTest.java
@@ -0,0 +1,86 @@
+package jsat.classifiers.linear;
+
+import java.util.List;
+import java.util.Random;
+import jsat.FixedProblems;
+import jsat.classifiers.*;
+import jsat.parameters.Parameter;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public class STGDTest
+{
+    
+    public STGDTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+    
+    /**
+     * Test of train method, of class STGD.
+     */
+    @Test
+    public void testTrain_RegressionDataSet()
+    {
+        System.out.println("train");
+        Random rand = new Random(123);
+        
+        STGD scd = new STGD(5, 0.1, Double.POSITIVE_INFINITY, 0.1);
+        scd.train(FixedProblems.getLinearRegression(400, rand));
+        
+        for(DataPointPair<Double> dpp : FixedProblems.getLinearRegression(400, rand).getAsDPPList())
+        {
+            double truth = dpp.getPair();
+            double pred = scd.regress(dpp.getDataPoint());
+            
+            double relErr = (truth-pred)/truth;
+            assertEquals(0.0, relErr, 0.1);//Give it a decent wiggle room b/c of regularization
+        }
+    }
+
+
+    /**
+     * Test of trainC method, of class STGD.
+     */
+    @Test
+    public void testTrainC_ClassificationDataSet()
+    {
+        System.out.println("trainC");
+        ClassificationDataSet train = FixedProblems.get2ClassLinear(400, new Random());
+        
+        STGD scd = new STGD(5, 0.5, Double.POSITIVE_INFINITY, 0.1);
+        scd.trainC(train);
+        
+        ClassificationDataSet test = FixedProblems.get2ClassLinear(400, new Random());
+        
+        for(DataPointPair<Integer> dpp : test.getAsDPPList())
+            assertEquals(dpp.getPair().longValue(), scd.classify(dpp.getDataPoint()).mostLikely());
+    }
+}
