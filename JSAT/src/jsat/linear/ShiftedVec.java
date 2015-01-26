ee096f5705849ee102f05587cfbbb20bcda50e80
Edward Raff <Raff.Edward@gmail.com>
2015-01-26 04:54:43 +0000

    Adding Vec object for representing a Vector shifted over by a scalar constant. includes some test cases of overloaded methods
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@950 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/linear/ShiftedVec.java
@@ -0,0 +1,348 @@
+
+package jsat.linear;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import java.util.Iterator;
+
+/**
+ * A wrapper for a vector that represents the vector added with a scalar value.
+ * This allows for using and altering the value added by a 
+ * constant factor quickly. Mutable operations will alter the underling vector, 
+ * and all operations will automatically be adjusted on a per element basis as 
+ * needed. The most significant performance difference is in representing a 
+ * sparse input sifted by a constant factor. Note, that when using a sparse base
+ * vector - the ShiftedVec will always return {@code true} for 
+ * {@link #isSparse() }, even if shifting makes all values non-zero
+ * <br>
+ * 
+ * @author Edward Raff
+ */
+public class ShiftedVec extends Vec
+{
+    private Vec base;
+    private double shift;
+
+    /**
+     * Creates a new vector represented as <code><b>base</b>+shift</code>
+     * @param base the base vector to represent
+     * @param shift the scalar shift to add to the base vector
+     */
+    public ShiftedVec(Vec base, double shift)
+    {
+        this.base = base;
+        this.shift = shift;
+    }
+
+    /**
+     * @return the base vector used by this object
+     */
+    public Vec getBase()
+    {
+        return base;
+    }
+
+    /**
+     * Directly alters the shift value used for this vector. The old sift is 
+     * "forgotten" immediately. 
+     * @param shift the new value to use for the additive scalar
+     */
+    public void setShift(double shift)
+    {
+        this.shift = shift;
+    }
+
+    /**
+     * 
+     * @return the additive scalar used to shift over the {@link #getBase() 
+     * base} value
+     */
+    public double getShift()
+    {
+        return shift;
+    }
+    
+    /**
+     * Embeds the current shift scalar into the base vector, modifying it and 
+     * then setting the new shit to zero. This makes the base vector have the 
+     * value represented by this ShiftedVec 
+     */
+    public void embedShift()
+    {
+        base.mutableAdd(shift);
+        shift = 0;
+    }
+    
+    @Override
+    public int length()
+    {
+        return base.length();
+    }
+
+    @Override
+    public double get(int index)
+    {
+        return base.get(index)+shift;
+    }
+
+    @Override
+    public void set(int index, double val)
+    {
+        base.set(index, val-shift);
+    }
+
+    @Override
+    public void increment(int index, double val)
+    {
+        base.increment(index, val);
+    }
+
+    @Override
+    public void mutableAdd(Vec b)
+    {
+        if(b instanceof ShiftedVec)
+        {
+            ShiftedVec other = (ShiftedVec) b;
+            base.mutableAdd(other.base);
+            shift += other.shift;
+        }
+        else
+            base.mutableAdd(b);
+    }
+
+    @Override
+    public void mutableAdd(double c)
+    {
+        shift += c;
+    }
+
+    @Override
+    public void mutableAdd(double c, Vec b)
+    {
+        if(b instanceof ShiftedVec)
+        {
+            ShiftedVec other = (ShiftedVec) b;
+            base.mutableAdd(c, other.base);
+            shift += other.shift*c;
+        }
+        else
+            base.mutableAdd(c, b); 
+    }
+
+    @Override
+    public void mutableDivide(double c)
+    {
+        base.mutableDivide(c);
+        shift /= c;
+        if(Double.isNaN(shift))
+            shift = 0;
+    }
+
+    @Override
+    public void mutableMultiply(double c)
+    {
+        base.mutableMultiply(c);
+        shift*=c;
+    }
+
+    @Override
+    public void mutablePairwiseDivide(Vec b)
+    {
+        //this would require multiple different shifts, so we have to fold it back into the base vector
+        base.mutableAdd(shift);
+        shift = 0;
+        base.mutablePairwiseDivide(b);
+    }
+
+    @Override
+    public void mutablePairwiseMultiply(Vec b)
+    {
+        //this would require multiple different shifts, so we have to fold it back into the base vector
+        base.mutableAdd(shift);
+        shift = 0;
+        base.mutablePairwiseMultiply(b);
+    }
+    
+    
+//    No real performance to gain by re-implementing matrix mul ops
+//    @Override
+//    public void multiply(double c, Matrix A, Vec b)
+    
+    @Override
+    public double dot(Vec v)
+    {
+        if(v instanceof  ShiftedVec)
+        {
+            ShiftedVec other = (ShiftedVec) v;
+            return this.base.dot(other.base) + other.base.sum()*this.shift + this.base.sum()*other.shift + this.length()*this.shift*other.shift;
+        }
+        return base.dot(v) + v.sum()*shift;
+    }
+
+    @Override
+    public void zeroOut()
+    {
+        base.zeroOut();
+        shift = 0;
+    }
+
+    @Override
+    public double pNorm(double p)
+    {
+        if(!isSparse())
+            return super.pNorm(p);
+        //else sparse base, we can save some work
+        //contributes of zero values
+        double baseZeroContribs = pow(abs(shift), p)*(length()-base.nnz());
+        //+ contribution of non zero values
+        double baseNonZeroContribs = 0;
+        for(IndexValue iv : base)
+            baseNonZeroContribs += pow(abs(iv.getValue()+shift), p);
+        return pow(baseNonZeroContribs+baseZeroContribs, 1/p);
+    }
+
+//    TODO: In the case of the y also being a ShiftedVec and sparse some significant performance could be saved.
+//    public double pNormDist(double p, Vec y)
+            
+    
+    @Override
+    public double mean()
+    {
+        return base.mean()+ shift;
+    }
+
+    @Override
+    public double variance()
+    {
+        return base.variance();
+    }
+
+    @Override
+    public double standardDeviation()
+    {
+        return base.standardDeviation();
+    }
+
+    @Override
+    public double kurtosis()
+    {
+        return base.kurtosis();
+    }
+
+    @Override
+    public double max()
+    {
+        return base.max()+shift;
+    }
+
+    @Override
+    public double min()
+    {
+        return base.min()+shift;
+    }
+
+    @Override
+    public double median()
+    {
+        return base.median()+shift;
+    }
+    
+    @Override
+    public Iterator<IndexValue> getNonZeroIterator(final int start)
+    {
+        if(!isSparse())//dense case, just add the shift and use the base implemenaton since its going to do the exact same thing I would
+            return super.getNonZeroIterator(start);
+        final Iterator<IndexValue> baseIter = base.getNonZeroIterator(start);
+        if(shift == 0)//easy case, just use the base's iterator
+            return baseIter;
+        
+        //ugly case, sparse vec with shifted values iterating over non zeros (which should generally be all of them)
+        final int lastIndx = length()-1;
+        
+        return new Iterator<IndexValue>()
+        {
+            IndexValue nextBaseVal;
+            IndexValue nextVal;
+            IndexValue toRet = null;
+            
+            //init
+            {
+                
+                for(int effectiveStart = start; effectiveStart <= lastIndx; effectiveStart++)
+                {
+                    nextBaseVal = baseIter.hasNext() ? baseIter.next() : null;
+                    if (nextBaseVal != null && nextBaseVal.getIndex() == effectiveStart)
+                    {
+                        if (nextBaseVal.getValue() + shift == 0)
+                            continue;//no starting on zero!
+                        else
+                            nextVal = new IndexValue(effectiveStart, nextBaseVal.getValue() + shift);
+                        nextBaseVal = baseIter.hasNext() ? baseIter.next() : null;
+                    }
+                    else//was zero + shift
+                        nextVal = new IndexValue(effectiveStart, shift);
+                    toRet = new IndexValue(effectiveStart, shift);
+                    break;
+                }
+            }
+
+            @Override
+            public boolean hasNext()
+            {
+                return nextVal != null;
+            }
+            
+            
+
+            @Override
+            public IndexValue next()
+            {
+                toRet.setIndex(nextVal.getIndex());
+                toRet.setValue(nextVal.getValue());
+                
+                //loop to get next value b/c we may have to skip over zeros
+                do
+                {
+                    nextVal.setIndex(nextVal.getIndex()+1);//pre-bump index 
+                    //prep next value
+                    if(nextVal.getIndex() == lastIndx+1)
+                        nextVal = null;//done
+                    else
+                    {
+                        if(nextBaseVal != null && nextBaseVal.getIndex() == nextVal.getIndex())//there is a base non-zero next
+                        {
+                            nextVal.setValue(nextBaseVal.getValue()+shift);
+                            nextBaseVal = baseIter.hasNext() ? baseIter.next() : null;
+                        }
+                        else//a base non-zero in our imediate future
+                        {
+                            nextVal.setValue(shift);
+                        }
+                    }
+                }
+                while(nextVal != null && nextVal.getValue() == 0);
+                
+                return toRet;
+            }
+
+            @Override
+            public void remove()
+            {
+                throw new UnsupportedOperationException("Not supported.");
+            }
+        };
+    }
+
+    @Override
+    public boolean isSparse()
+    {
+        return base.isSparse();
+    }
+
+    @Override
+    public ShiftedVec clone()
+    {
+        return new ShiftedVec(base.clone(), shift);
+    }
+    
+}
---
+++ JSAT/test/jsat/linear/ShiftedVecTest.java
@@ -0,0 +1,549 @@
+
+
+package jsat.linear;
+
+import java.util.Iterator;
+import java.util.Random;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public class ShiftedVecTest
+{
+    private Vec a_base;
+    private Vec b_base;
+    
+    private double shift_a;
+    private double shift_b;
+    
+    private Vec a_dense;
+    private Vec a_sparse;
+    private Vec b_dense;
+    private Vec b_sparse;
+    
+    private Vec rand_x;
+    private Vec rand_y;
+    
+    public ShiftedVecTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+        a_base = new SparseVector(12, 6);
+        a_base.set(0, 1.0);
+        a_base.set(1, 2.0);
+        a_base.set(3, -2.0);
+        a_base.set(4, 5.0);
+        a_base.set(8, -3.0);
+        a_base.set(11, 1.0);
+        
+        b_base = new SparseVector(12, 6);
+        b_base.set(0, 1.0);
+        b_base.set(1, -2.0);
+        b_base.set(3, -3.0);
+        b_base.set(4, 4.0);
+        b_base.set(7, -2.0);
+        b_base.set(10, 1.0);
+        
+        shift_a = -1;
+        shift_b = 2;
+        
+        a_dense = new DenseVector(a_base);
+        a_sparse = new SparseVector(a_base);
+        
+        a_dense.mutableAdd(shift_a);
+        a_sparse.mutableAdd(shift_a);
+        
+        b_dense = new DenseVector(b_base);
+        b_sparse = new SparseVector(b_base);
+        
+        b_dense.mutableAdd(shift_b);
+        b_sparse.mutableAdd(shift_b);
+        
+        Random rand = new Random();
+        rand_x = new DenseVector(a_base.length());
+        rand_y = new DenseVector(a_base.length());
+        
+        for(int i =0 ; i < rand_x.length(); i++)
+        {
+            rand_x.set(i, Math.round(rand.nextDouble()*10));
+            rand_y.set(i, Math.round(rand.nextDouble()*10));
+        }
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of setShift method, of class ShiftedVec.
+     */
+    @Test
+    public void testSetShift()
+    {
+        System.out.println("setShift");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        
+        assertEquals(shift_a, a.getShift(), 0.0);
+        
+        a.setShift(shift_b);
+        
+        assertEquals(shift_b, a.getShift(), 0.0);
+    }
+
+    /**
+     * Test of embedShift method, of class ShiftedVec.
+     */
+    @Test
+    public void testEmbedShift()
+    {
+        System.out.println("embedShift");
+        
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        
+        a.embedShift();
+        
+        assertEquals(0.0, a.getShift(), 0.0);
+        
+        assertTrue(a.getBase().equals(a_dense, 1e-10));
+    }
+
+    /**
+     * Test of length method, of class ShiftedVec.
+     */
+    @Test
+    public void testLength()
+    {
+        System.out.println("length");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        assertEquals(a_base.length(), a.length());
+    }
+
+    /**
+     * Test of get method, of class ShiftedVec.
+     */
+    @Test
+    public void testGet()
+    {
+        System.out.println("get");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        ShiftedVec b = new ShiftedVec(b_base, shift_b);
+        
+        for(int i =0 ; i < a.length(); i++)
+        {
+            assertEquals(a_dense.get(i), a.get(i), 1e-10);
+            assertEquals(b_dense.get(i), b.get(i), 1e-10);
+        }
+    }
+
+    /**
+     * Test of set method, of class ShiftedVec.
+     */
+    @Test
+    public void testSet()
+    {
+        System.out.println("set");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        
+        Random rand = new Random();
+        for(int round = 0; round < 100; round++)
+        {
+            int indx = rand.nextInt(a.length());
+            double tmp = rand.nextDouble();
+            a.set(indx, tmp);
+            assertEquals(tmp, a.get(indx), 1e-10);
+            assertEquals(tmp-shift_a, a_base.get(indx), 1e-10);
+        }
+    }
+
+    /**
+     * Test of increment method, of class ShiftedVec.
+     */
+    @Test
+    public void testIncrement()
+    {
+        System.out.println("increment");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        
+        for(int i =0 ; i < a.length(); i++)
+        {
+            a.increment(i, (i+1));
+            a_dense.increment(i, (i+1));
+            assertEquals(a_dense.get(i), a.get(i), 1e-10);
+        }
+    }
+
+    /**
+     * Test of mutableAdd method, of class ShiftedVec.
+     */
+    @Test
+    public void testMutableAdd_Vec()
+    {
+        System.out.println("mutableAdd");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        a.mutableAdd(rand_x);
+        a_dense.mutableAdd(rand_x);
+        
+        assertTrue(a.equals(a_dense, 1e-10));
+        
+        ShiftedVec b = new ShiftedVec(b_base, shift_b);
+        b.mutableAdd(a);
+        b_dense.mutableAdd(a_dense);
+        
+        
+        assertTrue(b.equals(b_dense, 1e-10));
+    }
+
+    /**
+     * Test of mutableAdd method, of class ShiftedVec.
+     */
+    @Test
+    public void testMutableAdd_double()
+    {
+        System.out.println("mutableAdd");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        a.mutableAdd(10);
+        a_dense.mutableAdd(10);
+        
+        assertTrue(a.equals(a_dense, 1e-10));
+        
+        ShiftedVec b = new ShiftedVec(b_base, shift_b);
+        b.mutableAdd(2);
+        b_dense.mutableAdd(2);
+        
+        assertTrue(b.equals(b_dense, 1e-10));
+    }
+
+    /**
+     * Test of mutableAdd method, of class ShiftedVec.
+     */
+    @Test
+    public void testMutableAdd_double_Vec()
+    {
+        System.out.println("mutableAdd");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        a.mutableAdd(10, rand_x);
+        a_dense.mutableAdd(10, rand_x);
+        
+        assertTrue(a.equals(a_dense, 1e-10));
+        
+        ShiftedVec b = new ShiftedVec(b_base, shift_b);
+        b.mutableAdd(-3, a);
+        b_dense.mutableAdd(-3, a_dense);
+        
+        
+        assertTrue(b.equals(b_dense, 1e-10));
+    }
+
+    /**
+     * Test of mutableDivide method, of class ShiftedVec.
+     */
+    @Test
+    public void testMutableDivide()
+    {
+        System.out.println("mutableDivide");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        a.mutableDivide(10);
+        a_dense.mutableDivide(10);
+        
+        assertTrue(a.equals(a_dense, 1e-10));
+    }
+
+    /**
+     * Test of mutableMultiply method, of class ShiftedVec.
+     */
+    @Test
+    public void testMutableMultiply()
+    {
+        System.out.println("mutableMultiply");
+        
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        a.mutableMultiply(10);
+        a_dense.mutableMultiply(10);
+        
+        assertTrue(a.equals(a_dense, 1e-10));
+        
+        
+        ShiftedVec b = new ShiftedVec(b_base, shift_b);
+        b.mutableMultiply(0);
+        b_dense.mutableMultiply(0);
+        
+        assertTrue(b.equals(b_dense, 1e-10));
+        
+        assertEquals(0.0, b.getShift(), 0.0);
+    }
+
+    /**
+     * Test of mutablePairwiseDivide method, of class ShiftedVec.
+     */
+    @Test
+    public void testMutablePairwiseDivide()
+    {
+        System.out.println("mutablePairwiseDivide");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        a.mutablePairwiseDivide(rand_x.add(1));
+        a_dense.mutablePairwiseDivide(rand_x.add(1));
+        
+        assertTrue(a.equals(a_dense, 1e-10));
+    }
+
+    /**
+     * Test of mutablePairwiseMultiply method, of class ShiftedVec.
+     */
+    @Test
+    public void testMutablePairwiseMultiply()
+    {
+        System.out.println("mutablePairwiseMultiply");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        a.mutablePairwiseDivide(rand_x);
+        a_dense.mutablePairwiseDivide(rand_x);
+        
+        assertTrue(a.equals(a_dense, 1e-10));
+    }
+
+    /**
+     * Test of dot method, of class ShiftedVec.
+     */
+    @Test
+    public void testDot()
+    {
+        System.out.println("dot");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        ShiftedVec b = new ShiftedVec(b_base, shift_b);
+        
+        assertEquals(a_dense.dot(rand_x), a.dot(rand_x), 1e-10);
+        assertEquals(a_dense.dot(rand_x), rand_x.dot(a), 1e-10);
+        
+        assertEquals(a_dense.dot(b_dense), a.dot(b), 1e-10);//shift on shift
+        assertEquals(a_dense.dot(b_dense), a.dot(b_dense), 1e-10);//shift on dense
+        assertEquals(a_dense.dot(b_dense), a_dense.dot(b), 1e-10);//dense on shift
+        assertEquals(a_dense.dot(b_dense), a.dot(b_sparse), 1e-10);//shift on sparse
+        assertEquals(a_dense.dot(b_dense), a_sparse.dot(b), 1e-10);//sparse on shift
+    }
+
+    /**
+     * Test of zeroOut method, of class ShiftedVec.
+     */
+    @Test
+    public void testZeroOut()
+    {
+        System.out.println("zeroOut");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        a.zeroOut();
+        
+        for(int i = 0; i < a.length(); i++)
+            assertEquals(0.0, a.get(i), 0.0);
+        assertEquals(0.0, a.getShift(), 0.0);
+    }
+
+    /**
+     * Test of pNorm method, of class ShiftedVec.
+     */
+    @Test
+    public void testPNorm()
+    {
+        System.out.println("pNorm");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        double[] ps = {0.5, 1, 2, 2.5, 3, 10 };
+        
+        for(double p : ps)
+            assertEquals(a_dense.pNorm(p), a.pNorm(p), 1e-10);
+    }
+
+    /**
+     * Test of mean method, of class ShiftedVec.
+     */
+    @Test
+    public void testMean()
+    {
+        System.out.println("mean");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        assertEquals(a_dense.mean(), a.mean(), 1e-10);
+    }
+
+    /**
+     * Test of variance method, of class ShiftedVec.
+     */
+    @Test
+    public void testVariance()
+    {
+        System.out.println("variance");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        assertEquals(a_dense.variance(), a.variance(), 1e-10);
+    }
+
+    /**
+     * Test of standardDeviation method, of class ShiftedVec.
+     */
+    @Test
+    public void testStandardDeviation()
+    {
+        System.out.println("standardDeviation");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        assertEquals(a_dense.standardDeviation(), a.standardDeviation(), 1e-10);
+    }
+
+    /**
+     * Test of kurtosis method, of class ShiftedVec.
+     */
+    @Test
+    public void testKurtosis()
+    {
+        System.out.println("kurtosis");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        assertEquals(a_dense.kurtosis(), a.kurtosis(), 1e-10);
+    }
+
+    /**
+     * Test of max method, of class ShiftedVec.
+     */
+    @Test
+    public void testMax()
+    {
+        System.out.println("max");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        assertEquals(a_dense.max(), a.max(), 1e-10);
+    }
+
+    /**
+     * Test of min method, of class ShiftedVec.
+     */
+    @Test
+    public void testMin()
+    {
+        System.out.println("min");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        assertEquals(a_dense.min(), a.min(), 1e-10);
+    }
+
+    /**
+     * Test of median method, of class ShiftedVec.
+     */
+    @Test
+    public void testMedian()
+    {
+        System.out.println("median");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        assertEquals(a_dense.median(), a.median(), 1e-10);
+    }
+
+    /**
+     * Test of getNonZeroIterator method, of class ShiftedVec.
+     */
+    @Test
+    public void testGetNonZeroIterator()
+    {
+        System.out.println("getNonZeroIterator");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        ShiftedVec b = new ShiftedVec(b_base, shift_b);
+        ShiftedVec c = new ShiftedVec(new ConstantVector(1.0, 12), -1);
+        
+        for(int start = 0; start < a.length(); start++)
+        {
+            Iterator<IndexValue> expIter = a_sparse.getNonZeroIterator(start);
+            Iterator<IndexValue> actIter = a.getNonZeroIterator(start);
+            
+            assertTrue(expIter.hasNext() == actIter.hasNext());
+            
+            while(expIter.hasNext())
+            {
+                IndexValue expIV = expIter.next();
+                IndexValue actIV = actIter.next();
+                
+                assertEquals(expIV.getIndex(), actIV.getIndex());
+                assertEquals(expIV.getValue(), actIV.getValue(), 1e-10);
+            }
+            
+            assertTrue(expIter.hasNext() == actIter.hasNext());
+            
+            //2nd pair
+            expIter = b_sparse.getNonZeroIterator(start);
+            actIter = b.getNonZeroIterator(start);
+            
+            assertTrue(expIter.hasNext() == actIter.hasNext());
+            
+            while(expIter.hasNext())
+            {
+                IndexValue expIV = expIter.next();
+                IndexValue actIV = actIter.next();
+                
+                assertEquals(expIV.getIndex(), actIV.getIndex());
+                assertEquals(expIV.getValue(), actIV.getValue(), 1e-10);
+                
+                assertTrue(expIter.hasNext() == actIter.hasNext());
+            }
+            
+            assertTrue(expIter.hasNext() == actIter.hasNext());
+            
+            
+            //3rd pair
+            expIter = new SparseVector(c.length()).getNonZeroIterator(start);
+            actIter = c.getNonZeroIterator(start);
+            
+            assertTrue(expIter.hasNext() == actIter.hasNext());
+            
+            while(expIter.hasNext())
+            {
+                IndexValue expIV = expIter.next();
+                IndexValue actIV = actIter.next();
+                
+                assertEquals(expIV.getIndex(), actIV.getIndex());
+                assertEquals(expIV.getValue(), actIV.getValue(), 1e-10);
+                
+                assertTrue(expIter.hasNext() == actIter.hasNext());
+            }
+            
+            assertTrue(expIter.hasNext() == actIter.hasNext());
+        }
+    }
+
+    /**
+     * Test of isSparse method, of class ShiftedVec.
+     */
+    @Test
+    public void testIsSparse()
+    {
+        System.out.println("isSparse");
+        assertTrue(new ShiftedVec(a_base, shift_a).isSparse());
+        assertFalse(new ShiftedVec(a_dense, shift_a).isSparse());
+    }
+
+    /**
+     * Test of clone method, of class ShiftedVec.
+     */
+    @Test
+    public void testClone()
+    {
+        System.out.println("clone");
+        ShiftedVec a = new ShiftedVec(a_base, shift_a);
+        ShiftedVec a_clone = a.clone();
+        assertTrue(a.equals(a_dense, 1e-10));
+        assertTrue(a_clone.equals(a_dense, 1e-10));
+        
+        a.mutableAdd(rand_x);
+        
+        assertFalse(a.equals(a_dense, 1e-10));
+        assertTrue(a_clone.equals(a_dense, 1e-10));
+    }
+    
+}
