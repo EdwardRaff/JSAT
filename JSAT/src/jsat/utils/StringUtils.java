6e6f3892f23c075321f5ffe8e076933e857fa378
Edward Raff <Raff.Edward@gmail.com>
2015-03-09 05:06:34 +0000

    Initial commit of StringUtils and some (not quite complete) test cases.
    git-svn-id: https://java-statistical-analysis-tool.googlecode.com/svn/trunk@982 c8979423-a768-6f17-17f2-fb57d64bf5d2

---
+++ JSAT/src/jsat/utils/StringUtils.java
@@ -0,0 +1,237 @@
+package jsat.utils;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public class StringUtils
+{
+    public static int parseInt(CharSequence s, int start, int end, int radix)
+    {
+        boolean negative = false;
+        int val = 0;
+        
+        for(int i = start; i < end; i++)
+        {
+            char c = s.charAt(i);
+            if (c == '-')
+                if (i == start)
+                    negative = true;
+                else
+                    throw new NumberFormatException("Negative sign did not occur at begining of sequence");
+            else if (c == '+')
+                if (i == start)
+                    negative = false;//do nothing really
+                else
+                    throw new NumberFormatException("Positive sign did not occur at begining of sequence");
+            else
+            {
+                int digit = Character.digit(c, radix);
+                if(digit < 0)
+                    throw new NumberFormatException("Non digit character '" + c + "' encountered");
+                val *= radix;
+                val += digit;
+            }
+        }
+        if(negative)
+            return -val;
+        else
+            return val;
+    }
+
+    public static int parseInt(CharSequence s, int start, int end)
+    {
+        return parseInt(s, start, end, 10);
+    }
+    
+    //Float/Double to String follows algo laid out here http://krashan.ppa.pl/articles/stringtofloat/
+    
+    private enum States
+    {
+        /**
+         * Either '+' or '-', or not sign present
+         */
+        SIGN,
+        /**
+         * skipping leading zeros in the integer part of mantissa
+         */
+        LEADING_ZEROS_MANTISSA,
+        /**
+         * reading leading zeros in the fractional part of mantissa.
+         */
+        LEADING_ZEROS_FRAC,
+        /**
+         * reading integer part of mantissa
+         */
+        MANTISSA_INT_PART,
+        
+        /**
+         * reading fractional part of mantissa.
+         */
+        MANTISSA_FRAC_PART,
+        
+        /**
+         * reading sign of exponent
+         */
+        EXPO_SIGN,
+        
+        EXPO_LEADING_ZERO,
+        
+        /**
+         * reading exponent digits
+         */
+        EXPO,
+    }
+    
+    public static double parseDouble(CharSequence s, int start, int end)
+    {
+        States state = States.SIGN;
+        int pos = start;
+        
+        int sign = 1;
+        long mantissa = 0;
+        /**
+         * Mantissa can only be incremented 18 times, then any more will 
+         * overflow (2^63-1 ~= 9.2* 10^18
+         */
+        byte mantisaIncrements = 0;
+        int implicitExponent = 0;
+        //used for (val)e(val) case
+        int expoSign = 1;
+        int explicitExponent = 0;
+        
+        while(pos < end)//run the state machine
+        {
+            char c = s.charAt(pos);
+            switch(state)
+            {
+                case SIGN:
+                    if (c == '-')
+                    {
+                        sign = -1;
+                        pos++;
+                    }
+                    else if(c == '+')
+                        pos++;
+                    else if (!Character.isDigit(c))//not a '-', '+', or digit, so error
+                        throw new NumberFormatException();
+                    state = States.LEADING_ZEROS_MANTISSA;
+                    continue;
+                case LEADING_ZEROS_MANTISSA:
+                    if(c == '0')
+                        pos++;
+                    else if(c == '.')
+                    {
+                        pos++;
+                        state = States.LEADING_ZEROS_FRAC;
+                    }
+                    else if (Character.isDigit(c))
+                        state = States.MANTISSA_INT_PART;
+                    else if(c == 'e' || c == 'E')//could be something like +0e0
+                        state = States.MANTISSA_FRAC_PART;//this is where that case is handeled
+                    else
+                        throw new NumberFormatException();
+                    continue;
+                case LEADING_ZEROS_FRAC:
+                    if(c == '0')
+                    {
+                        pos++;
+                        implicitExponent--;
+                    }
+                    else if(Character.isDigit(c))
+                        state = States.MANTISSA_FRAC_PART;
+                    else
+                        throw new NumberFormatException();
+                    continue;
+                case MANTISSA_INT_PART:
+                    if (c == '.')
+                    {
+                        pos++;
+                        state = States.MANTISSA_FRAC_PART;
+                    }
+                    else if (Character.isDigit(c))
+                    {
+                        if(mantisaIncrements < 18)
+                        {
+                            mantissa = mantissa * 10 + Character.digit(c, 10);
+                            mantisaIncrements++;
+                        }
+                        else//we are going to lose these, compencate with an implicit *= 10
+                            implicitExponent++;
+                        pos++;
+                    }
+                    else
+                        state = States.MANTISSA_FRAC_PART;
+                    //if we hit a invalid char it will get erred on in FRAC_PART
+                    continue;
+                case MANTISSA_FRAC_PART:
+                    if (Character.isDigit(c))
+                    {
+                        if (mantisaIncrements < 18)
+                        {
+                            mantissa = mantissa * 10 + Character.digit(c, 10);
+                            implicitExponent--;
+                            mantisaIncrements++;
+                        }
+                        else//we are going to lose these
+                        {
+                            //we would have incresed the implicit exponent
+                            //but we would have subtracted if we could 
+                            //so do nothing
+                        }
+                        pos++;
+                    }
+                    else if (c == 'e' || c == 'E')
+                    {
+                        pos++;
+                        state = States.EXPO_SIGN;
+                    }
+                    else
+                        throw new NumberFormatException();
+                    continue;
+                case EXPO_SIGN:
+                    if (c == '-')
+                    {
+                        expoSign = -1;
+                        pos++;
+                    }
+                    else if(c == '+')
+                        pos++;
+                    else if (!Character.isDigit(c))//not a '-', '+', or digit, so error
+                        throw new NumberFormatException();
+                    state = States.EXPO_LEADING_ZERO;
+                    continue;
+                case EXPO_LEADING_ZERO:
+                    if(c == '0')
+                    {
+                        pos++;
+                    }
+                    else if(Character.isDigit(c))
+                        state = States.EXPO;
+                    else
+                        throw new NumberFormatException();
+                    continue;
+                case EXPO:
+                    if(Character.isDigit(c))
+                    {
+                        explicitExponent = explicitExponent * 10 + Character.digit(c, 10);
+                        pos++;
+                    }
+                    else 
+                        throw new NumberFormatException();
+                    continue;
+            }
+        }
+        
+        int finalExpo = expoSign*explicitExponent + implicitExponent;
+        if(mantissa == 0)//easiest case!
+            if (sign == -1)
+                return -0.0;
+            else
+                return 0.0;
+        if(finalExpo == 0)//easy case! 
+            return sign*mantissa;
+        
+        return sign * (mantissa*Math.pow(10, finalExpo));
+    }
+}
---
+++ JSAT/test/jsat/utils/StringUtilsTest.java
@@ -0,0 +1,167 @@
+package jsat.utils;
+
+import java.util.Random;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * 
+ * @author Edward Raff
+ */
+public class StringUtilsTest
+{
+    
+    public StringUtilsTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of parseInt method, of class StringUtils.
+     */
+    @Test
+    public void testParseInt()
+    {
+        System.out.println("parseInt");
+        Random rand = new Random();
+        for(int radix = Character.MIN_RADIX; radix <= Character.MAX_RADIX; radix++)
+        {
+            for(int trials = 0; trials < 1000; trials++)
+            {
+                String preFix = "";
+                String postFix = "";
+
+                int prefixSize = rand.nextInt(3);
+                int postFixSize = rand.nextInt(3);
+                for(int i =0 ; i < prefixSize; i++)
+                    preFix += Character.toString((char) rand.nextInt(128));
+                for(int i =0 ; i < postFixSize; i++)
+                    postFix += Character.toString((char) rand.nextInt(128));
+
+
+                //first test a large int
+                int truth = rand.nextInt();
+                String test = preFix + Integer.toString(truth, radix) + postFix;
+
+                assertEquals(truth, StringUtils.parseInt(test, prefixSize, test.length()-postFixSize, radix));
+                
+                //now a small one
+                
+                //first test a large int
+                truth = rand.nextInt(10)*(rand.nextInt(1)*2-1);
+                test = preFix + Integer.toString(truth, radix) + postFix;
+
+                assertEquals(truth, StringUtils.parseInt(test, prefixSize, test.length()-postFixSize, radix));
+            }
+        }
+    }
+    
+    @Test
+    public void testParseDouble()
+    {
+        System.out.println("parseDouble");
+        Random rand = new Random(42);
+        
+        String[] signOps = new String[]{"+", "-", ""};
+        String[] Es = new String[]{"e", "E"};
+        String[] zeros = new String[]{"","", "", "0", "00", "000", "0000"};
+        
+        double truth, attempt;
+        String toTest;
+
+        for (int trials = 0; trials < 10000; trials++)
+        {
+            String preFix = "";
+            String postFix = "";
+
+            int prefixSize = 0;//rand.nextInt(3);
+            int postFixSize = 0;//rand.nextInt(3);
+            for (int i = 0; i < prefixSize; i++)
+                preFix += Character.toString((char) rand.nextInt(128));
+            for (int i = 0; i < postFixSize; i++)
+                postFix += Character.toString((char) rand.nextInt(128));
+
+            //easy cases that should all be exact
+            
+            //[sign][val]
+            toTest = signOps[rand.nextInt(3)] + zeros[rand.nextInt(zeros.length)] + rand.nextInt((int) Math.round(Math.pow(10, rand.nextInt(6)))) + "" + rand.nextInt((int) Math.round(Math.pow(10, rand.nextInt(6))));
+            truth = Double.parseDouble(toTest);
+            attempt = StringUtils.parseDouble(toTest, 0, toTest.length());
+            assertRelativeEquals(truth, attempt);
+            
+            //[sign][val].[val]
+            toTest = signOps[rand.nextInt(3)] + zeros[rand.nextInt(zeros.length)] + rand.nextInt((int) Math.round(Math.pow(10, rand.nextInt(6)))) + "." + zeros[rand.nextInt(zeros.length)] + rand.nextInt((int) Math.round(Math.pow(10, rand.nextInt(6))));
+            truth = Double.parseDouble(toTest);
+            attempt = StringUtils.parseDouble(toTest, 0, toTest.length());
+            assertRelativeEquals(truth, attempt);
+            
+            //[sign][val][eE][sign][val]
+            toTest = signOps[rand.nextInt(3)] + zeros[rand.nextInt(zeros.length)] + rand.nextInt((int) Math.round(Math.pow(10, rand.nextInt(6)))) + Es[rand.nextInt(2)] + signOps[rand.nextInt(3)] + zeros[rand.nextInt(zeros.length)] + rand.nextInt((int) Math.round(Math.pow(10, rand.nextInt(2))));
+            truth = Double.parseDouble(toTest);
+            attempt = StringUtils.parseDouble(toTest, 0, toTest.length());
+            assertRelativeEquals(truth, attempt);
+            
+            //harder cases, may have nonsense values (over flow to Inf/NegInf, underflows to 0)
+            
+            //[sign][val]
+            toTest = signOps[rand.nextInt(3)].replace("-", "") + zeros[rand.nextInt(zeros.length)] + Math.max(Math.abs(rand.nextLong()), 0);
+            truth = Double.parseDouble(toTest);
+            attempt = StringUtils.parseDouble(toTest, 0, toTest.length());
+            assertRelativeEquals(truth, attempt);
+            
+            //[sign][val].[val]
+            toTest = signOps[rand.nextInt(3)] + rand.nextInt(Integer.MAX_VALUE) + zeros[rand.nextInt(zeros.length)] + "." + zeros[rand.nextInt(zeros.length)] + rand.nextInt(Integer.MAX_VALUE) ;
+            truth = Double.parseDouble(toTest);
+            attempt = StringUtils.parseDouble(toTest, 0, toTest.length());
+            assertRelativeEquals(truth, attempt);
+            
+            //[sign][val][eE][sign][val]
+            toTest = signOps[rand.nextInt(3)] + zeros[rand.nextInt(zeros.length)] + rand.nextInt((int) Math.round(Math.pow(10, rand.nextInt(8)))) + Es[rand.nextInt(2)] + signOps[rand.nextInt(3)] + zeros[rand.nextInt(zeros.length)] + rand.nextInt(450);
+            truth = Double.parseDouble(toTest);
+            attempt = StringUtils.parseDouble(toTest, 0, toTest.length());
+            assertRelativeEquals(truth, attempt);
+        }
+
+    }
+
+    protected void assertRelativeEquals(double truth, double attempt)
+    {
+        String message = "Expteced " + truth + " but was " + attempt;
+        if (Double.isNaN(truth))
+            assertTrue(message, Double.isNaN(attempt));
+        else if (Double.isInfinite(truth))
+        {
+            assertTrue(message, Double.isInfinite(attempt));
+            assertTrue(Double.doubleToRawLongBits(truth) == Double.doubleToRawLongBits(attempt));//get the signs right
+        }
+        else
+        {
+            double relDiff = Math.abs(truth - attempt) / (Math.max(Math.abs(Math.max(truth, attempt)), 1e-14));
+            assertEquals(message, 0, relDiff, 1e-14);
+        }
+    }
+    
+}
