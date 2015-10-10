638deb1d33b3c11569d5d9e722789d6df0287b65
EdwardRaff <Raff.Edward@gmail.com>
2015-10-10 19:02:40 -0400

    Parallelized implementation to Isomap.

--- JSAT/src/jsat/datatransform/visualization/Isomap.java
+++ JSAT/src/jsat/datatransform/visualization/Isomap.java
@@ -19,7 +19,10 @@ package jsat.datatransform.visualization;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import jsat.DataSet;
 import jsat.SimpleDataSet;
 import jsat.linear.*;
@@ -30,6 +33,7 @@ import jsat.linear.vectorcollection.VectorCollection;
 import jsat.linear.vectorcollection.VectorCollectionFactory;
 import jsat.utils.FakeExecutor;
 import jsat.utils.FibHeap;
+import jsat.utils.SystemInfo;
 
 /**
  *
@@ -72,8 +76,8 @@ public class Isomap
     
     public <Type extends DataSet> Type transform(DataSet<Type> d, ExecutorService ex)
     {
-        int N = d.getSampleSize();
-        Matrix delta = new DenseMatrix(N, N);
+        final int N = d.getSampleSize();
+        final Matrix delta = new DenseMatrix(N, N);
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
                 if (i == j)
@@ -82,33 +86,57 @@ public class Isomap
                     delta.set(i, j, Double.MAX_VALUE);
 
         
-        List<VecPaired<Vec, Integer>> vecs = new ArrayList<VecPaired<Vec, Integer>>(N);
+        final List<VecPaired<Vec, Integer>> vecs = new ArrayList<VecPaired<Vec, Integer>>(N);
         for(int i = 0; i < N; i++)
             vecs.add(new VecPaired<Vec, Integer>(d.getDataPoint(i).getNumericalValues(), i));
-        VectorCollection<VecPaired<Vec, Integer>> vc = vcf.getVectorCollection(vecs, dm, ex);
-        List<Double> cache = dm.getAccelerationCache(vecs, ex);
+        final VectorCollection<VecPaired<Vec, Integer>> vc = vcf.getVectorCollection(vecs, dm, ex);
+        final List<Double> cache = dm.getAccelerationCache(vecs, ex);
                 
-        int knn = searchNeighbors+1;//+1 b/c we are closest to ourselves
+        final int knn = searchNeighbors+1;//+1 b/c we are closest to ourselves
         
         //bleh, ugly generics...
-        List<List<? extends VecPaired<VecPaired<Vec, Integer>, Double>>> neighborGraph = new ArrayList<List<? extends VecPaired<VecPaired<Vec, Integer>, Double>>>();
-
-        double[] avgNeighborDist = new double[N];
+        final List<List<? extends VecPaired<VecPaired<Vec, Integer>, Double>>> neighborGraph = new ArrayList<List<? extends VecPaired<VecPaired<Vec, Integer>, Double>>>();
+        for (int i = 0; i < N; i++)
+            neighborGraph.add(null);
+            
+        final double[] avgNeighborDist = new double[N];
         
         //do knn search and store results so we can do distances
-        for (int i = 0; i < N; i++)
+        final CountDownLatch latch1 = new CountDownLatch(SystemInfo.LogicalCores);
+        for(int id = 0; id < SystemInfo.LogicalCores; id++)
         {
-            List<? extends VecPaired<VecPaired<Vec, Integer>, Double>> neighbors = vc.search(vecs.get(i).getVector(), knn);
-            neighborGraph.add(neighbors);
-            //Compute stats that may be used for c-isomap version
-            for (int z = 1; z < neighbors.size(); z++)
+            final int ID = id;
+            ex.submit(new Runnable()
             {
-                VecPaired<VecPaired<Vec, Integer>, Double> neighbor = neighbors.get(z);
-                int j = neighbor.getVector().getPair();
-                double dist = neighbor.getPair();
-                avgNeighborDist[i] += dist;
-            }
-            avgNeighborDist[i] /= (neighbors.size()-1);
+
+                @Override
+                public void run()
+                {
+                    for (int i = ID; i < N; i+=SystemInfo.LogicalCores)
+                    {
+                        List<? extends VecPaired<VecPaired<Vec, Integer>, Double>> neighbors = vc.search(vecs.get(i).getVector(), knn);
+                        neighborGraph.set(i, neighbors);
+                        //Compute stats that may be used for c-isomap version
+                        for (int z = 1; z < neighbors.size(); z++)
+                        {
+                            VecPaired<VecPaired<Vec, Integer>, Double> neighbor = neighbors.get(z);
+                            double dist = neighbor.getPair();
+                            avgNeighborDist[i] += dist;
+                        }
+                        avgNeighborDist[i] /= (neighbors.size()-1);
+                    }
+                    latch1.countDown();
+                }
+            });
+        }
+        
+        try
+        {
+            latch1.await();
+        }
+        catch (InterruptedException ex1)
+        {
+            Logger.getLogger(Isomap.class.getName()).log(Level.SEVERE, null, ex1);
         }
         
         if(c_isomap)
@@ -122,37 +150,87 @@ public class Isomap
             }
         }
         
-        for(int k = 0; k < N; k++)
+        final CountDownLatch latch2 = new CountDownLatch(SystemInfo.LogicalCores);
+        for(int id = 0; id < SystemInfo.LogicalCores; id++)
         {
-            double[] tmp_dist = dijkstra(neighborGraph, k);
-            for (int i = 0; i < N; i++)
+            final int ID = id;
+            ex.submit(new Runnable()
             {
-                tmp_dist[i] = Math.min(tmp_dist[i], delta.get(i, k));
-                delta.set(i, k, tmp_dist[i]);
-                delta.set(k, i, tmp_dist[i]);
-            }
+
+                @Override
+                public void run()
+                {
+                    for (int k = ID; k < N; k += SystemInfo.LogicalCores)
+                    {
+                        double[] tmp_dist = dijkstra(neighborGraph, k);
+                        for (int i = 0; i < N; i++)
+                        {
+                            tmp_dist[i] = Math.min(tmp_dist[i], delta.get(k, i));
+                            delta.set(i, k, tmp_dist[i]);
+                            delta.set(k, i, tmp_dist[i]);
+                        }
+                    }
+                    latch2.countDown();
+                }
+
+            });
         }
+        
+        try
+        {
+            latch2.await();
+        }
+        catch (InterruptedException ex1)
+        {
+            Logger.getLogger(Isomap.class.getName()).log(Level.SEVERE, null, ex1);
+        }
+        
         
         //lets check for any disjoint groupings, replace them with something reasonable
         //we will use the largest obtainable distance to be an offset for our infinity distances
-        double largest_natural_dist = 0;
+        double largest_natural_dist_tmp = 0;
         for (int i = 0; i < N; i++)
             for (int j = i + 1; j < N; j++)
                 if(delta.get(i, j) < Double.MAX_VALUE)
-                    largest_natural_dist = Math.max(largest_natural_dist, delta.get(i, j));
+                    largest_natural_dist_tmp = Math.max(largest_natural_dist_tmp, delta.get(i, j));
+        final double largest_natural_dist = largest_natural_dist_tmp;
         
-        for (int i = 0; i < N; i++)
-            for (int j = i + 1; j < N; j++)
+        final CountDownLatch latch3 = new CountDownLatch(SystemInfo.LogicalCores);
+        for(int id = 0; id < SystemInfo.LogicalCores; id++)
+        {
+            final int ID = id;
+            ex.submit(new Runnable()
             {
-                double d_ij = delta.get(i, j);
-                if (d_ij >= Double.MAX_VALUE)//replace with the normal distance + 1 order of magnitude? 
+
+                @Override
+                public void run()
                 {
-                    d_ij = 10*dm.dist(i, j, vecs, cache)+1.5*largest_natural_dist;
-                    delta.set(i, j, d_ij);
-                    delta.set(j, i, d_ij);
+                    for (int i = ID; i < N; i += SystemInfo.LogicalCores)
+                    {
+                        for (int j = i + 1; j < N; j++)
+                        {
+                            double d_ij = delta.get(i, j);
+                            if (d_ij >= Double.MAX_VALUE)//replace with the normal distance + 1 order of magnitude? 
+                            {
+                                d_ij = 10*dm.dist(i, j, vecs, cache)+1.5*largest_natural_dist;
+                                delta.set(i, j, d_ij);
+                                delta.set(j, i, d_ij);
+                            }
+                        }
+                        latch3.countDown();
+                    }
                 }
-            }
+            });
+        }
         
+        try
+        {
+            latch3.await();
+        }
+        catch (InterruptedException ex1)
+        {
+            Logger.getLogger(Isomap.class.getName()).log(Level.SEVERE, null, ex1);
+        }
         
         SimpleDataSet emedded = mds.transform(delta, ex);
         
