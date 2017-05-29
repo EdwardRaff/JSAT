0d0e748e334d0478cfb6b3f715ccc81c42f68c6c
EdwardRaff <Raff.Edward@gmail.com>
2017-05-29 13:05:52 -0400

    Adding convenience shortcut version of get Start/End block, since it will usually be called with the number of CPU cores available.

--- JSAT/src/jsat/utils/concurrent/ParallelUtils.java
+++ JSAT/src/jsat/utils/concurrent/ParallelUtils.java
@@ -1,5 +1,6 @@
 package jsat.utils.concurrent;
 import static java.lang.Math.min;
+import jsat.utils.SystemInfo;
 
 /**
  *
@@ -25,6 +26,22 @@ public class ParallelUtils
     }
     
     /**
+     * Gets the starting index (inclusive) for splitting up a list of items into
+     * {@link SystemInfo#LogicalCores} evenly sized blocks. In the event that
+     * {@code N} is not evenly divisible by {@link SystemInfo#LogicalCores}, the
+     * size of ranges will differ by at most 1.
+     *
+     * @param N the number of items to split up
+     * @param ID the block number in [0, {@link SystemInfo#LogicalCores})
+     * @return the starting index (inclusive) of the blocks owned by the
+     * {@code ID}'th process.
+     */
+    public static int getStartBlock(int N, int ID)
+    {
+        return getStartBlock(N, ID, SystemInfo.LogicalCores);
+    }
+    
+    /**
      * Gets the ending index (exclusive) for splitting up a list of items into
      * {@code P} evenly sized blocks. In the event that {@code N} is not evenly 
      * divisible by {@code P}, the size of ranges will differ by at most 1. 
@@ -39,5 +56,22 @@ public class ParallelUtils
         int rem = N%P;
         int start = (N/P)*(ID+1)+min(rem, ID+1);
         return start;
+    }
+
+    /**
+     * Gets the ending index (exclusive) for splitting up a list of items into
+     * {@link SystemInfo#LogicalCores} evenly sized blocks. In the event that
+     * {@link SystemInfo#LogicalCores} is not evenly divisible by
+     * {@link SystemInfo#LogicalCores}, the size of ranges will differ by at
+     * most 1.
+     *
+     * @param N the number of items to split up
+     * @param ID the block number in [0, {@link SystemInfo#LogicalCores})
+     * @return the ending index (exclusive) of the blocks owned by the
+     * {@code ID}'th process.
+     */
+    public static int getEndBlock(int N, int ID)
+    {
+        return getEndBlock(N, ID, SystemInfo.LogicalCores);
     }
 }
