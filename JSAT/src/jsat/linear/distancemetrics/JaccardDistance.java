0a3645eefedc1d6080d705889cbf69fd57672d2f
Edward Raff <Raff.Edward@gmail.com>
2017-07-09 00:01:26 -0400

    Adding Jaccard Distance / Kernel

---
+++ JSAT/src/jsat/linear/distancemetrics/JaccardDistance.java
@@ -0,0 +1,267 @@
+/*
+ * Copyright (C) 2017 Edward Raff
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jsat.linear.distancemetrics;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import jsat.distributions.kernels.KernelTrick;
+import jsat.linear.IndexValue;
+import jsat.linear.Vec;
+import jsat.parameters.Parameter;
+import static java.lang.Math.*;
+import java.util.Collections;
+
+/**
+ * This class implements both the weighted Jaccard Distance and the standard
+ * Jaccard distance. If a input is given with only binary 0 or 1 values, the
+ * weighted Jaccard is equivalent to the un-weighted version.<br>
+ * For the weighted Jaccard version, all values less than or equal to zero will
+ * be treated as zero. For the unweighted versions, all non-zero values will
+ * behave as if their value is 1.0.
+ * <br>
+ * The Jaccard Distance and similarity are intertwined, and so this method is
+ * both a distance metric and kernel trick.
+ *
+ * @author Edward Raff
+ */
+public class JaccardDistance implements DistanceMetric, KernelTrick
+{
+    private boolean weighted;
+
+    /**
+     * Creates a new Jaccard similarity, which can be weighted or unweighted.
+     *
+     * @param weighted {@code true} to use the weighted Jaccard, {@code false}
+     * otherwise.
+     */
+    public JaccardDistance(boolean weighted)
+    {
+        this.weighted = weighted;
+    }
+
+    /**
+     * Creates a new Weighted Jaccard distance / similarity 
+     */
+    public JaccardDistance()
+    {
+        this(true);
+    }
+
+    @Override
+    public double dist(Vec a, Vec b)
+    {
+        return 1-eval(a, b);
+    }
+
+    @Override
+    public boolean isSymmetric()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isSubadditive()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isIndiscemible()
+    {
+        return true;
+    }
+
+    @Override
+    public double metricBound()
+    {
+        return 1.0;
+    }
+
+    @Override
+    public boolean supportsAcceleration()
+    {
+        return false;
+    }
+
+    @Override
+    public List<Double> getAccelerationCache(List<? extends Vec> vecs)
+    {
+        return null;
+    }
+
+    @Override
+    public List<Double> getAccelerationCache(List<? extends Vec> vecs, ExecutorService threadpool)
+    {
+        return null;
+    }
+
+    @Override
+    public double dist(int a, int b, List<? extends Vec> vecs, List<Double> cache)
+    {
+        return dist(vecs.get(a), vecs.get(b));
+    }
+
+    @Override
+    public double dist(int a, Vec b, List<? extends Vec> vecs, List<Double> cache)
+    {
+        return dist(vecs.get(a), b);
+    }
+
+    @Override
+    public List<Double> getQueryInfo(Vec q)
+    {
+        return null;
+    }
+
+    @Override
+    public double dist(int a, Vec b, List<Double> qi, List<? extends Vec> vecs, List<Double> cache)
+    {
+        return dist(vecs.get(a), b);
+    }
+
+    @Override
+    public double eval(Vec a, Vec b)
+    {
+        
+        double numer = 0, denom = 0;
+        Iterator<IndexValue> a_iter = a.getNonZeroIterator();
+        Iterator<IndexValue> b_iter = b.getNonZeroIterator();
+
+        IndexValue a_val = a_iter.hasNext() ? a_iter.next() : null;
+        IndexValue b_val = b_iter.hasNext() ? b_iter.next() : null;
+
+        while (a_val != null && b_val != null)
+        {
+            if (weighted)
+            {
+                if (a_val.getIndex() == b_val.getIndex())
+                {
+                    numer += max(min(a_val.getValue(), b_val.getValue()), 0.0);
+                    denom += max(max(a_val.getValue(), b_val.getValue()), 0.0);
+                    
+                    a_val = a_iter.hasNext() ? a_iter.next() : null;
+                    b_val = b_iter.hasNext() ? b_iter.next() : null;
+                }
+                else if(a_val.getIndex() < b_val.getIndex())
+                {
+                    denom += max(a_val.getValue(), 0.0);
+                    a_val = a_iter.hasNext() ? a_iter.next() : null;
+                }
+                else//b had a lower index
+                {
+                    denom += max(b_val.getValue(), 0.0);
+                    b_val = b_iter.hasNext() ? b_iter.next() : null;
+                }
+            }
+            else//unweighted variant
+            {
+                if (a_val.getIndex() == b_val.getIndex())
+                {
+                    numer++;
+                    denom++;
+                    
+                    a_val = a_iter.hasNext() ? a_iter.next() : null;
+                    b_val = b_iter.hasNext() ? b_iter.next() : null;
+                }
+                else if(a_val.getIndex() < b_val.getIndex())
+                {
+                    denom++;
+                    a_val = a_iter.hasNext() ? a_iter.next() : null;
+                }
+                else//b had a lower index
+                {
+                    denom++;
+                    b_val = b_iter.hasNext() ? b_iter.next() : null;
+                }
+            }
+        }
+        //catch straglers
+        Iterator<IndexValue> finalIter = a_val != null ? a_iter : b_iter;
+        IndexValue finalVal =            a_val != null ? a_val : b_val;
+        
+        while(finalVal != null)
+        {
+            if(weighted)
+                denom += max(finalVal.getValue(), 0.0);
+            else
+                denom++;
+            finalVal =  finalIter.hasNext() ?  finalIter.next() : null;
+        }       
+
+        return numer / denom;
+    }
+
+    @Override
+    public JaccardDistance clone()
+    {
+        return new JaccardDistance(weighted);
+    }
+
+    @Override
+    public void addToCache(Vec newVec, List<Double> cache)
+    {
+        //NOP, nothing to do 
+    }
+
+    @Override
+    public double eval(int a, Vec b, List<Double> qi, List<? extends Vec> vecs, List<Double> cache)
+    {
+        return eval(vecs.get(a), b);
+    }
+
+    @Override
+    public double eval(int a, int b, List<? extends Vec> trainingSet, List<Double> cache)
+    {
+        return eval(trainingSet.get(a), trainingSet.get(b));
+    }
+
+    @Override
+    public double evalSum(List<? extends Vec> finalSet, List<Double> cache, double[] alpha, Vec y, int start, int end)
+    {
+        return evalSum(finalSet, cache, alpha, y, getQueryInfo(y), start, end);
+    }
+
+    @Override
+    public double evalSum(List<? extends Vec> finalSet, List<Double> cache, double[] alpha, Vec y, List<Double> qi, int start, int end)
+    {
+        double sum = 0;
+        for(int i = start; i < end; i++)
+            if(alpha[i] != 0)
+                sum += alpha[i] * eval(i, y, qi, finalSet, cache);
+        return sum;
+    }
+
+    @Override
+    public boolean normalized()
+    {
+        return true;
+    }
+
+    @Override
+    public List<Parameter> getParameters()
+    {
+        return Collections.EMPTY_LIST;
+    }
+
+    @Override
+    public Parameter getParameter(String paramName)
+    {
+        return null;
+    }
+    
+}
---
+++ JSAT/test/jsat/linear/distancemetrics/JaccardDistanceTest.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2017 Edward Raff
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jsat.linear.distancemetrics;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import jsat.linear.DenseVector;
+import jsat.linear.Vec;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff
+ */
+public class JaccardDistanceTest
+{
+    
+    public JaccardDistanceTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of dist method, of class JaccardDistance.
+     */
+    @Test
+    public void testDist_Vec_Vec()
+    {
+        System.out.println("dist");
+        Vec a = DenseVector.toDenseVec(0.0, 1.0, 0.0, 2.0, 5.0);
+        Vec b = DenseVector.toDenseVec(1.0, 0.0, 0.0, 3.0, 0.0);
+        double unweightedSim = 1.0/4;
+        double weightedSim = 2.0/10;
+        JaccardDistance j = new JaccardDistance(false);
+        JaccardDistance wj = new JaccardDistance(true);
+        
+        assertEquals(unweightedSim, j.eval(a, b), 1e-12);
+        assertEquals(1-unweightedSim, j.dist(a, b), 1e-12);
+        assertEquals(unweightedSim, j.eval(b, a), 1e-12);
+        assertEquals(1-unweightedSim, j.dist(b, a), 1e-12);
+        
+        assertEquals(weightedSim, wj.eval(a, b), 1e-12);
+        assertEquals(1-weightedSim, wj.dist(a, b), 1e-12);
+        assertEquals(weightedSim, wj.eval(b, a), 1e-12);
+        assertEquals(1-weightedSim, wj.dist(b, a), 1e-12);
+        
+        assertEquals(1.0, j.eval(a, a), 1e-12);
+        assertEquals(0.0, j.dist(a, a), 1e-12);
+        assertEquals(1.0, j.eval(b, b), 1e-12);
+        assertEquals(0.0, j.dist(b, b), 1e-12);
+        
+        assertEquals(1.0, wj.eval(a, a), 1e-12);
+        assertEquals(0.0, wj.dist(a, a), 1e-12);
+        assertEquals(1.0, wj.eval(b, b), 1e-12);
+        assertEquals(0.0, wj.dist(b, b), 1e-12);
+    }
+
+    /**
+     * Test of isSymmetric method, of class JaccardDistance.
+     */
+    @Test
+    public void testIsSymmetric()
+    {
+        System.out.println("isSymmetric");
+        JaccardDistance instance = new JaccardDistance();
+        boolean expResult = true;
+        boolean result = instance.isSymmetric();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of isSubadditive method, of class JaccardDistance.
+     */
+    @Test
+    public void testIsSubadditive()
+    {
+        System.out.println("isSubadditive");
+        JaccardDistance instance = new JaccardDistance();
+        boolean expResult = true;
+        boolean result = instance.isSubadditive();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of isIndiscemible method, of class JaccardDistance.
+     */
+    @Test
+    public void testIsIndiscemible()
+    {
+        System.out.println("isIndiscemible");
+        JaccardDistance instance = new JaccardDistance();
+        boolean expResult = true;
+        boolean result = instance.isIndiscemible();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of metricBound method, of class JaccardDistance.
+     */
+    @Test
+    public void testMetricBound()
+    {
+        System.out.println("metricBound");
+        JaccardDistance instance = new JaccardDistance();
+        double expResult = 1.0;
+        double result = instance.metricBound();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of supportsAcceleration method, of class JaccardDistance.
+     */
+    @Test
+    public void testSupportsAcceleration()
+    {
+        System.out.println("supportsAcceleration");
+        JaccardDistance instance = new JaccardDistance();
+        boolean expResult = false;
+        boolean result = instance.supportsAcceleration();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getAccelerationCache method, of class JaccardDistance.
+     */
+    @Test
+    public void testGetAccelerationCache_List()
+    {
+        System.out.println("getAccelerationCache");
+        List<? extends Vec> vecs = null;
+        JaccardDistance instance = new JaccardDistance();
+        List<Double> expResult = null;
+        List<Double> result = instance.getAccelerationCache(vecs);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getAccelerationCache method, of class JaccardDistance.
+     */
+    @Test
+    public void testGetAccelerationCache_List_ExecutorService()
+    {
+        System.out.println("getAccelerationCache");
+        List<? extends Vec> vecs = null;
+        ExecutorService threadpool = null;
+        JaccardDistance instance = new JaccardDistance();
+        List<Double> expResult = null;
+        List<Double> result = instance.getAccelerationCache(vecs, threadpool);
+        assertEquals(expResult, result);
+    }
+
+
+    /**
+     * Test of getQueryInfo method, of class JaccardDistance.
+     */
+    @Test
+    public void testGetQueryInfo()
+    {
+        System.out.println("getQueryInfo");
+        Vec q = null;
+        JaccardDistance instance = new JaccardDistance();
+        List<Double> expResult = null;
+        List<Double> result = instance.getQueryInfo(q);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of normalized method, of class JaccardDistance.
+     */
+    @Test
+    public void testNormalized()
+    {
+        System.out.println("normalized");
+        JaccardDistance instance = new JaccardDistance();
+        boolean expResult = true;
+        boolean result = instance.normalized();
+        assertEquals(expResult, result);
+    }
+    
+}
