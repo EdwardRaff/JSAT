5ccc8f1e4360ba8cea69db2bc5615d2a798d5aaa
Edward Raff <Raff.Edward@gmail.com>
2017-08-13 19:44:19 -0400

    Making SDCA test run a bit faster by having better starting point for lambda in warm start tests

--- JSAT/test/jsat/classifiers/linear/SDCATest.java
+++ JSAT/test/jsat/classifiers/linear/SDCATest.java
@@ -193,8 +193,8 @@ public class SDCATest
                     sdca.setUseBias(false);
                     
                     sdca.setAlpha(1.0);
-                    //SDCA dosn't do fully L1 well, so skip to elastic and L2 tests
-                    sdca.setLambda(maxLam / 100);
+                    
+                    sdca.setLambda(maxLam);
                     double search_const = 0.025;
                     while(w.nnz() != 1)// I should be able to find a value of lambda that results in only 1 feature
                     {//SDCA requires a bit more searching b/c it behaved differently than normal coordinate descent solvers when selecting features
@@ -222,13 +222,12 @@ public class SDCATest
                     assertEquals(1, (int)Math.signum(w.get(nonZeroIndex)));
                     
                     //elastic case
-
-                    sdca.setLambda(maxLam / 1000);
+                    sdca.setLambda(maxLam / 10);
                     sdca.setAlpha(0.5);//now we should get the top 3 on
                     do
                     {
                         sdca.setLambda(sdca.getLambda() * 1.05);
-                        sdca.trainC(data);
+                        sdca.trainC(data, sdca);
                         w = sdca.getRawWeight(0);
                     }
                     while (w.nnz() > 3);//we should be able to find this pretty easily
@@ -294,10 +293,11 @@ public class SDCATest
         train.applyTransform(new LinearTransform(train));
         
         SDCA truth = new SDCA();
-        truth.setMaxIters(1000);
+        truth.setMaxIters(10000);
         truth.setAlpha(0.5);
         truth.setLoss(new LogisticLoss());
         truth.setTolerance(1e-10);
+        truth.setLambda(1.0/train.getSampleSize());
         truth.trainC(train);
         
         SDCA warm = new SDCA();
@@ -305,6 +305,7 @@ public class SDCATest
         warm.setLoss(new LogisticLoss());
         warm.setAlpha(0.5);
         warm.setTolerance(1e-7);
+        warm.setLambda(1.0/train.getSampleSize());
         warm.trainC(train, truth);
         
         assertEquals(0, warm.getRawWeight(0).subtract(truth.getRawWeight(0)).pNorm(2), 1e-4);
