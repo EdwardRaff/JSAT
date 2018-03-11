4c47dd0b7eb1f18dd2e77290d36ae5e09a153b68
EdwardRaff <Raff.Edward@gmail.com>
2018-03-10 19:42:46 -0500

    cache common value to improve speed

--- JSAT/src/jsat/distributions/discrete/Zipf.java
+++ JSAT/src/jsat/distributions/discrete/Zipf.java
@@ -18,6 +18,7 @@ package jsat.distributions.discrete;
 
 import static jsat.math.SpecialMath.*;
 import static java.lang.Math.*;
+import java.util.Random;
 
 
 /**
@@ -30,6 +31,10 @@ public class Zipf extends DiscreteDistribution
 {
     private double cardinality;
     private double skew;
+    /**
+     * Both variants of the Zipf distribution re-use a term of Zeta(1+skew) or Harmonic(cardinality, 1+skew) for most calculations. This value caches this commonly used constant
+     */
+    private double denomCache;
 
     /**
      * Creates a new Zipf distribution
@@ -68,6 +73,7 @@ public class Zipf extends DiscreteDistribution
     {
         this.cardinality = toCopy.cardinality;
         this.skew = toCopy.skew;
+        this.denomCache = toCopy.denomCache;
     }
     
     /**
@@ -82,6 +88,7 @@ public class Zipf extends DiscreteDistribution
         if (cardinality < 0 || Double.isNaN(cardinality))
             throw new IllegalArgumentException("Cardinality must be a positive integer or infinity, not " + cardinality);
         this.cardinality = Math.ceil(cardinality);
+        fixCache();
     }
 
     /**
@@ -105,6 +112,15 @@ public class Zipf extends DiscreteDistribution
         if(skew <= 0 || Double.isNaN(skew) || Double.isInfinite(skew))
             throw new IllegalArgumentException("Skew must be a positive value, not " + skew);
         this.skew = skew;
+        fixCache();
+    }
+
+    private void fixCache()
+    {
+        if (Double.isInfinite(cardinality))
+            denomCache = zeta(1 + skew);
+        else
+            denomCache = harmonic(cardinality, 1 + skew);
     }
 
     /**
@@ -126,14 +142,14 @@ public class Zipf extends DiscreteDistribution
         if(Double.isInfinite(cardinality))
         {
             //x^(-1 - skew)/Zeta[1 + skew]
-            return pow(x, -skew-1)/zeta(1+skew);
+            return pow(x, -skew-1)/denomCache;
         }
         else
         {
             if(x > cardinality)
                 return 0;
             //x^(-1-skew)/HarmonicNumber[cardinality,1+skew]
-            return pow(x, -skew-1)/harmonic(cardinality, 1+skew);
+            return pow(x, -skew-1)/denomCache;
         }
     }
 
@@ -148,12 +164,12 @@ public class Zipf extends DiscreteDistribution
         if(Double.isInfinite(cardinality))
         {
             //HarmonicNumber[x,1+skew]/Zeta[1+skew]
-            return harmonic(x, 1+skew)/zeta(1+skew);
+            return harmonic(x, 1+skew)/denomCache;
         }
         else
         {
             //HarmonicNumber[x,1+skew]/HarmonicNumber[cardinality,1+skew]
-            return harmonic(x, 1+skew)/harmonic(cardinality, 1+skew);
+            return harmonic(x, 1+skew)/denomCache;
         }
     }
 
@@ -177,12 +193,12 @@ public class Zipf extends DiscreteDistribution
             if(skew <= 1)
                 return Double.POSITIVE_INFINITY;
             //Zeta[skew]/Zeta[1+skew]
-            return zeta(skew)/zeta(1+skew);
+            return zeta(skew)/denomCache;
         }
         else
         {
             //HarmonicNumber[cardinality, skew]/HarmonicNumber[cardinality, 1 + skew]
-            return harmonic(cardinality, skew)/harmonic(cardinality, 1+skew);
+            return harmonic(cardinality, skew)/denomCache;
         }
     }
 
@@ -200,7 +216,7 @@ public class Zipf extends DiscreteDistribution
             if(skew <= 2)
                 return Double.POSITIVE_INFINITY;
             //-(Zeta[skew]^2/Zeta[1+skew]^2)+Zeta[-1+skew]/Zeta[1+skew]
-            double zSkewP1 = zeta(skew+1);
+            double zSkewP1 = denomCache;
             double zSkewM1 = zeta(skew-1);
             return zSkewM1/zSkewP1 - pow(zeta(skew), 2)/(zSkewP1*zSkewP1);
         }
@@ -221,7 +237,7 @@ public class Zipf extends DiscreteDistribution
                 return Double.POSITIVE_INFINITY;
             //(2 Zeta[skew]^3-3 Zeta[-1+skew] Zeta[skew] Zeta[1+skew]+Zeta[-2+skew] Zeta[1+skew]^2)/(-Zeta[skew]^2+Zeta[-1+skew] Zeta[1+skew])^(3/2)
             double zSkew = zeta(skew);
-            double zSkewP1 = zeta(skew + 1);
+            double zSkewP1 = denomCache;
             double zSkewM1 = zeta(skew - 1);
             return (2 * pow(zSkew, 3) - 3 * zSkewM1 * zSkew * zSkewP1 + zeta(-2 + skew) * pow(zSkewP1, 2)) / pow(-pow(zSkew, 2) + zSkewM1 * zSkewP1, 3.0 / 2.0);
         }
@@ -230,7 +246,7 @@ public class Zipf extends DiscreteDistribution
             //(2 HarmonicNumber[cardinality,skew]^3-3 HarmonicNumber[cardinality,-1+skew] HarmonicNumber[cardinality,skew] HarmonicNumber[cardinality,1+skew]+HarmonicNumber[cardinality,-2+skew] HarmonicNumber[cardinality,1+skew]^2)/(HarmonicNumber[cardinality,1+skew]^3 ((-HarmonicNumber[cardinality,skew]^2+HarmonicNumber[cardinality,-1+skew] HarmonicNumber[cardinality,1+skew])/HarmonicNumber[cardinality,1+skew]^2)^(3/2))
             double hSkewM1 = harmonic(cardinality, skew-1);
             double hSkew = harmonic(cardinality, skew);
-            double hSkewP1 = harmonic(cardinality, 1+skew);
+            double hSkewP1 = denomCache;
             //numerator is (2 HarmonicNumber[cardinality,skew]^3-3 HarmonicNumber[cardinality,-1+skew] HarmonicNumber[cardinality,skew] HarmonicNumber[cardinality,1+skew]+HarmonicNumber[cardinality,-2+skew] HarmonicNumber[cardinality,1+skew]^2)
             
             double numer = (2*pow(hSkew, 3)-3*hSkewM1*hSkew*hSkewP1+harmonic(cardinality, skew-2)*pow(hSkewP1, 2));
