f773037ee91522dd8cc391e7d2ba3a4542aa9c5c
EdwardRaff <Raff.Edward@gmail.com>
2018-04-17 00:03:38 -0400

    Initial outlier detection interface implemented. Added Linear OC-SVM as first outlier detection algorithm. Includes test case

---
+++ JSAT/src/jsat/outlire/LinearOCSVM.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2018 Edward Raff <Raff.Edward@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jsat.outlire;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import jsat.DataSet;
+import jsat.classifiers.DataPoint;
+import jsat.linear.DenseVector;
+import jsat.linear.ScaledVector;
+import jsat.linear.Vec;
+import jsat.math.optimization.stochastic.AdaGrad;
+import jsat.math.optimization.stochastic.GradientUpdater;
+import jsat.utils.random.RandomUtil;
+
+/**
+ * This class implements the One-Class SVM (OC-SVM) algorithm for outlier
+ * detection. This implementation works only in the primal or "linear" space. As
+ * such it works best when the data is sparse and high dimensional. If your data
+ * is dense and low dimensional, you may get better results by first applying a
+ * non-linear transformation to the data.
+ *
+ * 
+ * See: 
+ * <ul>
+ * <li>Schölkopf, B., Williamson, R., Smola, A., Shawe-Taylor, J., & Platt, J.
+ * (1999). <i>Support Vector Method for Novelty Detection</i>. In Advances in
+ * Neural Information Processing Systems 12 (pp. 582–588). Denver, CO.</li>
+ * <li>Manevitz, L. M., & Yousef, M. (2001). <i>One-class Svms for Document
+ * Classification</i>. J. Mach. Learn. Res., 2, 139–154. Retrieved from
+ * http://dl.acm.org/citation.cfm?id=944790.944808</li>
+ * </ul>
+ * 
+ * @author Edward Raff <Raff.Edward@gmail.com>
+ */
+public class LinearOCSVM implements Outlire
+{
+    
+    private Vec w;
+    private double p;
+    private int max_epochs = 100;
+    private double learningRate = 0.01;
+    private double v = 0.05;
+
+    public void setV(double v)
+    {
+        this.v = v;
+    }
+
+    public double getV()
+    {
+        return v;
+    }
+    
+    
+    @Override
+    public void fit(DataSet d)
+    {
+        
+        Random rand = RandomUtil.getRandom();
+        List<Vec> X = d.getDataVectors();
+        
+        int N = X.size();
+        w = new ScaledVector(new DenseVector(X.get(0).length()));
+        p = 0;
+        
+        
+        GradientUpdater gu = new AdaGrad();
+        gu.setup(w.length());
+        double cnt = 1/(v);
+        
+        double prevLoss = Double.POSITIVE_INFINITY;
+        double curLoss = Double.POSITIVE_INFINITY;
+        for(int epoch = 0; epoch < max_epochs; epoch++)
+        {
+            Collections.shuffle(X, rand);
+            
+            prevLoss = curLoss;
+            curLoss = 0;
+            
+            for(int i = 0; i < X.size(); i++)
+            {
+                Vec x = X.get(i);
+                double loss = p - w.dot(x);
+                
+                double p_delta = -1;
+                double x_mul = 0;
+                
+                if(loss > 0)
+                {
+                    p_delta += 1*cnt;
+                    x_mul = -1*cnt;
+                }
+                
+                curLoss += Math.max(0, loss);
+                
+                p -= gu.update(w, new ScaledVector(x_mul, x), learningRate, p, p_delta);
+                w.mutableMultiply(1-learningRate);
+            }
+            
+//            System.out.println("Epoch " + epoch + " " + curLoss + " " + (curLoss-prevLoss)/N);
+            if(Math.abs((curLoss-prevLoss)/N) < 1e-6*v)
+                break;//Convergence check
+            
+        }
+    }
+
+    @Override
+    public double score(DataPoint x)
+    {
+        return w.dot(x.getNumericalValues())-p;
+    }
+    
+}
---
+++ JSAT/test/jsat/outlire/LinearOCSVMTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2018 Edward Raff <Raff.Edward@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jsat.outlire;
+
+import jsat.DataSet;
+import jsat.SimpleDataSet;
+import jsat.classifiers.DataPoint;
+import jsat.datatransform.kernel.RFF_RBF;
+import jsat.distributions.Normal;
+import jsat.distributions.kernels.RBFKernel;
+import jsat.utils.GridDataGenerator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff <Raff.Edward@gmail.com>
+ */
+public class LinearOCSVMTest
+{
+    
+    public LinearOCSVMTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of fit method, of class LinearOCSVM.
+     */
+    @Test
+    public void testFit()
+    {
+        System.out.println("fit");
+        int N = 5000;
+        SimpleDataSet trainData = new GridDataGenerator(new Normal(), 1,1,1).generateData(N);
+        RFF_RBF rff = new RFF_RBF(RBFKernel.guessSigma(trainData).median(), 64);
+        rff.fit(trainData);
+        trainData.applyTransform(rff);
+        
+        SimpleDataSet outlierData = new GridDataGenerator(new Normal(10, 1.0), 1,1,1).generateData(N);
+        outlierData.applyTransform(rff);
+        
+        LinearOCSVM instance = new LinearOCSVM();
+        
+        for(double v : new double[]{0.01, 0.05, 0.1})
+        {
+            instance.setV(v);
+            instance.fit(trainData);
+            
+            double numOutliersInTrain = trainData.getDataPoints().stream().mapToDouble(instance::score).filter(x->x<0).count();
+//            System.out.println(v + " " + numOutliersInTrain + " " + v*N);
+            assertEquals(0, numOutliersInTrain/trainData.getSampleSize(), v*3);//Better say something like v% or less of training data is an outlier!
+            
+            double numOutliersInOutliers = outlierData.getDataPoints().stream().mapToDouble(instance::score).filter(x->x<0).count();
+//            System.out.println("Outliers: " + numOutliersInOutliers/outlierData.getSampleSize());
+            assertEquals(1.0, numOutliersInOutliers/outlierData.getSampleSize(), 0.1);//Better say 90% are outliers!
+        }
+    }
+    
+}
