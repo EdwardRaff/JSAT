fd615c1e9a5f3533960f12ae50dbc5b56d096adb
Edward Raff <Raff.Edward@gmail.com>
2018-04-24 16:28:08 -0400

    Implementing Density based outlier detection, including test case

--- JSAT/src/jsat/outlier/DensityOutlier.java
+++ JSAT/src/jsat/outlier/DensityOutlier.java
@@ -16,31 +16,97 @@
  */
 package jsat.outlier;
 
+import java.util.Arrays;
 import jsat.DataSet;
 import jsat.classifiers.DataPoint;
 import jsat.distributions.multivariate.MultivariateDistribution;
+import jsat.distributions.multivariate.NormalMR;
+import jsat.utils.concurrent.ParallelUtils;
 
 /**
- * This class provides an outlier detector based upon density estimation. 
+ * This class provides an outlier detector based upon density estimation.
+ *
  * @author Edward Raff
  */
 public class DensityOutlier implements Outlier
 {
-    double outlireFraction;
-    MultivariateDistribution density;
+    private double outlierFraction;
+    private MultivariateDistribution density;
+    /**
+     * Threshold for what counts as an outlier. Any value less than or equal to
+     * this threshold will be considered an outlier.
+     */
+    private double threshold;
     
+    public DensityOutlier()
+    {
+        this(0.05);
+    }
     
+    public DensityOutlier(double outlierFraction)
+    {
+        this(outlierFraction, new NormalMR());
+    }
 
+    public DensityOutlier(double outlierFraction, MultivariateDistribution density)
+    {
+        this.outlierFraction = outlierFraction;
+        this.density = density;
+    }
+    
+    public DensityOutlier(DensityOutlier toCopy)
+    {
+        this(toCopy.outlierFraction, toCopy.density.clone());
+        this.threshold = toCopy.threshold;
+    }
+
+    public void setOutlierFraction(double outlierFraction)
+    {
+        this.outlierFraction = outlierFraction;
+    }
+
+    public double getOutlierFraction()
+    {
+        return outlierFraction;
+    }
+
+    public void setDensityDistribution(MultivariateDistribution density)
+    {
+        this.density = density;
+    }
+
+    public MultivariateDistribution getDensityDistribution()
+    {
+        return density;
+    }
+    
+    
+    
     @Override
     public void fit(DataSet d, boolean parallel)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        density.setUsingData(d, parallel);
+        double[] scores = new double[d.getSampleSize()];
+        ParallelUtils.run(parallel, scores.length, (start, end)->
+        {
+            for(int i = start; i < end; i++)
+                scores[i] = density.logPdf(d.getDataPoint(i).getNumericalValues());
+        });
+        Arrays.sort(scores);
+        threshold = scores[(int)(scores.length*outlierFraction)];
     }
 
     @Override
     public double score(DataPoint x)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double logPDF = density.logPdf(x.getNumericalValues());
+        return logPDF - threshold;
+    }
+
+    @Override
+    protected DensityOutlier clone() throws CloneNotSupportedException
+    {
+        return new DensityOutlier(this);
     }
     
 }
---
+++ JSAT/test/jsat/outlier/DensityOutlierTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2018 Edward Raff <Raff.Edward@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jsat.outlier;
+
+import jsat.SimpleDataSet;
+import jsat.distributions.Normal;
+import jsat.distributions.multivariate.NormalM;
+import jsat.utils.GridDataGenerator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edward Raff <Raff.Edward@gmail.com>
+ */
+public class DensityOutlierTest
+{
+    
+    public DensityOutlierTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of fit method, of class LinearOCSVM.
+     */
+    @Test
+    public void testFit()
+    {
+        System.out.println("fit");
+        int N = 5000;
+        SimpleDataSet trainData = new GridDataGenerator(new Normal(), 1,1,1).generateData(N);
+        SimpleDataSet outlierData = new GridDataGenerator(new Normal(10, 1.0), 1,1,1).generateData(N);
+        
+        DensityOutlier instance = new DensityOutlier();
+        instance.setDensityDistribution(new NormalM());
+        
+        for(boolean parallel : new boolean[]{false, true})
+            for(double v : new double[]{0.01, 0.05, 0.1})
+            {
+                instance.setOutlierFraction(v);
+                instance.fit(trainData, parallel);
+
+                double numOutliersInTrain = trainData.getDataPoints().stream().mapToDouble(instance::score).filter(x->x<0).count();
+                assertEquals(0, numOutliersInTrain/trainData.getSampleSize(), v*3);//Better say something like v% or less of training data is an outlier!
+
+                double numOutliersInOutliers = outlierData.getDataPoints().stream().mapToDouble(instance::score).filter(x->x<0).count();
+                assertEquals(1.0, numOutliersInOutliers/outlierData.getSampleSize(), 0.1);//Better say 90% are outliers!
+            }
+    }
+    
+}
