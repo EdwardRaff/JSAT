aa0c32b4d5aa86a9462b3aaceb1a28b1b47d0779
Edward Raff <Raff.Edward@gmail.com>
2019-09-26 16:08:52 -0400

    Adding more code to handle case where we don't know how many bi-clusters should exist. Also allow user to change what base clustering algo is used

--- JSAT/src/jsat/clustering/biclustering/SpectralCoClustering.java
+++ JSAT/src/jsat/clustering/biclustering/SpectralCoClustering.java
@@ -8,7 +8,9 @@ import jsat.DataSet;
 import jsat.SimpleDataSet;
 import jsat.classifiers.CategoricalData;
 import jsat.classifiers.DataPoint;
+import jsat.clustering.Clusterer;
 import jsat.clustering.KClusterer;
+import jsat.clustering.kmeans.GMeans;
 import jsat.clustering.kmeans.HamerlyKMeans;
 import jsat.linear.DenseVector;
 import jsat.linear.IndexValue;
@@ -20,10 +22,19 @@ import jsat.utils.IntList;
 
 /**
  *
- * @author edwardraff
+ * @author Edward Raff
  */
 public class SpectralCoClustering implements Bicluster
 {
+    Clusterer baseClusterAlgo = new GMeans();
+
+    public void setBaseClusterAlgo(Clusterer baseClusterAlgo) {
+        this.baseClusterAlgo = baseClusterAlgo;
+    }
+
+    public Clusterer getBaseClusterAlgo() {
+        return baseClusterAlgo;
+    }
 
     @Override
     public void bicluster(DataSet dataSet, int clusters, boolean parallel, List<List<Integer>> row_assignments, List<List<Integer>> col_assignments) 
@@ -42,27 +53,70 @@ public class SpectralCoClustering implements Bicluster
         
         //A_n has r rows and c columns. We are going to make a new data matrix Z
         //Z will have (r+c) rows, and l columns. 
+        SimpleDataSet Z = create_Z_dataset(A_n, l, R, C);//+1 b/c we are going to skip the first SV
+        
+        KClusterer to_use;
+        if(baseClusterAlgo instanceof KClusterer)
+            to_use = (KClusterer) baseClusterAlgo;
+        else
+            to_use = new HamerlyKMeans();
+        int[] joint_designations = to_use.cluster(Z, clusters, parallel, null);
+        
+        createAssignments(row_assignments, col_assignments, clusters, A, joint_designations);
+        
+    }
+
+    public void bicluster(DataSet dataSet, boolean parallel, List<List<Integer>> row_assignments, List<List<Integer>> col_assignments)
+    {
+        //﻿1. Given A, form An = D_1^{−1/2} A D_2^{−1/2}
+        Matrix A = dataSet.getDataMatrix();
+                        
+        DenseVector R = new DenseVector(A.rows());
+        DenseVector C = new DenseVector(A.cols());
+        
+        Matrix A_n = row_col_normalize(A, R, C);
+        
+        //﻿2. Compute l = ceil(log2 k) singular vectors of A_n, u2, . . . u_l+1 and v2, . . . v_l+1, and form the matrix Z as in (12)
+        int k_max = Math.min(A.rows(), A.cols());
+        int l = (int) Math.ceil(Math.log(k_max)/Math.log(2.0));
+        
+        
+        SimpleDataSet Z = create_Z_dataset(A_n, l, R, C);
+        
+        
+        int[] joint_designations = baseClusterAlgo.cluster(Z, parallel, null);
+        int clusters = 0;
+        for(int i : joint_designations)
+            clusters = Math.max(clusters, i+1);
+        
+        //prep label outputs
+        createAssignments(row_assignments, col_assignments, clusters, A, joint_designations);
+        //the bicluster labels for the rows
+        //the bicluter labels for the columns
+        //Now we need to prune potential false bi-clusterings that have only features or only rows
+        
+    }
+
+    private SimpleDataSet create_Z_dataset(Matrix A_n, int l, DenseVector R, DenseVector C) {
+        //A_n has r rows and c columns. We are going to make a new data matrix Z
+        //Z will have (r+c) rows, and l columns.
         TruncatedSVD svd = new TruncatedSVD(A_n, l+1);//+1 b/c we are going to skip the first SV
         Matrix U = svd.getU();
         Matrix V = svd.getV().transpose();
-        
         //Drop the first column, which corresponds to the first SV we don't want
         U = new SubMatrix(U, 0, 1, U.rows(), l+1);
         V = new SubMatrix(V, 0, 1, V.rows(), l+1);
-        
         Matrix.diagMult(R, U);
         Matrix.diagMult(C, V);
-        
-        
         SimpleDataSet Z = new SimpleDataSet(l, new CategoricalData[0]);
         for(int i = 0; i < U.rows(); i++)
             Z.add(new DataPoint(U.getRow(i)));
         for(int i = 0; i < V.rows(); i++)
             Z.add(new DataPoint(V.getRow(i)));
-        
-        KClusterer kMeans = new HamerlyKMeans();
-        int[] joint_designations = kMeans.cluster(Z, clusters, parallel, null);
-        
+        return Z;
+    }
+    
+    private void createAssignments(List<List<Integer>> row_assignments, List<List<Integer>> col_assignments, int clusters, Matrix A, int[] joint_designations) {
         //prep label outputs
         row_assignments.clear();
         col_assignments.clear();
@@ -78,11 +132,17 @@ public class SpectralCoClustering implements Bicluster
         for(int j = 0; j < A.cols(); j++)//the bicluter labels for the columns
             if(joint_designations[j+A.rows()] >= 0)
                 col_assignments.get(joint_designations[j+A.rows()]).add(j);
-        
-        
+        //Now we need to prune potential false bi-clusterings that have only features or only rows
+        for(int j = row_assignments.size()-1; j >= 0; j--)
+        {
+            if(row_assignments.get(j).isEmpty() || col_assignments.get(j).isEmpty())
+            {
+                row_assignments.remove(j);
+                col_assignments.remove(j);
+            }
+        }
     }
 
-    
 
     @Override
     public SpectralCoClustering clone() 
--- JSAT/test/jsat/clustering/biclustering/SpectralCoClusteringTest.java
+++ JSAT/test/jsat/clustering/biclustering/SpectralCoClusteringTest.java
@@ -5,13 +5,10 @@ package jsat.clustering.biclustering;
 
 import java.util.ArrayList;
 import java.util.List;
-import jsat.DataSet;
-import jsat.SimpleDataSet;
 import jsat.classifiers.CategoricalData;
 import jsat.classifiers.ClassificationDataSet;
-import jsat.clustering.KClusterer;
+import jsat.clustering.HDBSCAN;
 import jsat.linear.DenseVector;
-import jsat.linear.Matrix;
 import jsat.linear.Vec;
 import jsat.utils.IntList;
 import org.junit.After;
@@ -59,7 +56,8 @@ public class SpectralCoClusteringTest {
         int true_k = 4;
         int features_per = 3;
         int n_c = 5;
-        int d = features_per*true_k;
+        int noisy_features = 0;
+        int d = features_per*true_k + noisy_features;
         
         List<List<Integer>> true_row_assingments = new ArrayList<>();
         List<List<Integer>> true_col_assingments = new ArrayList<>();
@@ -103,11 +101,8 @@ public class SpectralCoClusteringTest {
                 true_row_assingments, true_col_assingments, 
                 row_assignments, col_assignments);
         
-        //Should be able to get a perfect score
-        assertEquals(1.0, score, 0.0);
         
-//        System.out.println("Score: " + score);
-//        
+        
 //        for(int c = 0; c < true_k; c++)
 //        {
 //            System.out.println(c);
@@ -115,8 +110,84 @@ public class SpectralCoClusteringTest {
 //            System.out.println(col_assignments.get(c));
 //            System.out.println("\n\n");
 //        }
+//        
+//        System.out.println("Score: " + score);
+        
+        //Should be able to get a perfect score
+        assertEquals(1.0, score, 0.0);
+        
+    }
+    
+    @Test
+    public void testCluster_UnkK() 
+    {
+        System.out.println("cluster");
+        
+        IntList labels = new IntList();
+        List<Vec> data = new ArrayList<>();
+        int true_k = 4;
+        int features_per = 3;
+        int n_c = 15;
+        int noisy_features = 20;
+        int d = features_per*true_k + noisy_features;
+        
+        List<List<Integer>> true_row_assingments = new ArrayList<>();
+        List<List<Integer>> true_col_assingments = new ArrayList<>();
+        
+        for(int y = 0; y < true_k; y++)
+        {
+            IntList row_asign = new IntList();
+            IntList col_asign = new IntList();
+            for(int j = y*features_per; j < (y+1)*features_per; j++)
+                col_asign.add(j);
+            
+            
+            for(int i = 0; i < n_c; i++)
+            {
+                row_asign.add(data.size());
+                labels.add(y);
+//                DenseVector x = new DenseVector(d);
+                Vec x = DenseVector.random(d).multiply(0.01);
+                for(int j = y*features_per; j < (y+1)*features_per; j++)
+                    x.increment(j, 1.0);
+                data.add(x);
+            }
+            
+            true_row_assingments.add(row_asign);
+            true_col_assingments.add(col_asign);
+        }
+        
+        ClassificationDataSet dataSet = new ClassificationDataSet(d, new CategoricalData[0], new CategoricalData(true_k));
+        for(int i = 0; i < labels.size(); i++)
+            dataSet.addDataPoint(data.get(i), labels.get(i));
         
         
+        boolean parallel = false;
+        SpectralCoClustering instance = new SpectralCoClustering();
+        
+        instance.setBaseClusterAlgo(new HDBSCAN(5));
+        
+        List<List<Integer>> row_assignments = new ArrayList<>();
+        List<List<Integer>> col_assignments = new ArrayList<>();
+        instance.bicluster(dataSet, parallel, row_assignments, col_assignments);
+        
+        
+        double score = ConsensusScore.score(parallel, 
+                true_row_assingments, true_col_assingments, 
+                row_assignments, col_assignments);
+                
+//        for(int c = 0; c < row_assignments.size(); c++)
+//        {
+//            System.out.println(c);
+//            System.out.println(row_assignments.get(c));
+//            System.out.println(col_assignments.get(c));
+//            System.out.println("\n\n");
+//        }
+//        
+//        System.out.println("Score: " + score);
+        
+        //Should be able to do pretty well
+        assertEquals(1.0, score, 0.1);
         
     }
 
